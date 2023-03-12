48786cfc88fb09fb2db6ab407b546676d0a584f1
Qi Chen <Qi.Chen@man.com>
2023-03-12 13:42:33 +0000

    CIBuildWheel-based build

---
+++ build_tooling/vcpkg_caching.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $(realpath $(dirname $BASH_SOURCE))/../cpp/vcpkg
+
+[[ -x vcpkg ]] || ./bootstrap-vcpkg.sh -disableMetrics
+nuget="`which mono` `./vcpkg fetch nuget | tail -n 1`"  # which mono will return empty on windows
+echo "Using nuget=$nuget"
+url="https://nuget.pkg.github.com/$nuget_user/index.json"
+$nuget sources add -source $url -storepasswordincleartext -name github \
+    -username "${nuget_user:?nuget_user environment variable is not set}" \
+    -password "${nuget_token:?nuget_token environment variable is not set}"
+$nuget setapikey "$nuget_token" -source $url
