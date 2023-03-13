17046596ec5e1820a5dcc12942f3e889e7126898
Dealtry, William (London) <William.Dealtry@man.com>
2023-03-13 09:47:27 +0000

    Update license text

--- cpp/arcticdb/entity/types-inl.hpp
+++ cpp/arcticdb/entity/types-inl.hpp
@@ -1,7 +1,9 @@
-/*
-* Copyright 2023 Man Group Operations Ltd.
-* NO WARRANTY, EXPRESSED OR IMPLIED
-*/
+/* Copyright 2023 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+ */
 
 #ifndef ARCTICDB_TYPES_H_
 #error "This should only be included by types.hpp"
--- cpp/arcticdb/storage/mongo/mongo_client.hpp
+++ cpp/arcticdb/storage/mongo/mongo_client.hpp
@@ -1,7 +1,9 @@
-/*
-* Copyright 2023 Man Group Operations Ltd.
-* NO WARRANTY, EXPRESSED OR IMPLIED
-*/
+/* Copyright 2023 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+ */
 
 #pragma once
 #include <fmt/format.h>
