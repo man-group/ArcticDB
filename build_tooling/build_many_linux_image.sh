8583eb2599a10bd1994f5a7cbd3125ac7a8808e1
Qi Chen <Qi.Chen@man.com>
2023-03-24 17:51:50 +0000

    Revision in Manylinux image tag
    * To avoid confusion when we change how the image is built
    * Also tag "latest" to make running cibuildwheel easier locally
    * Ability to override the image used by a build

--- build_tooling/build_many_linux_image.sh
+++ build_tooling/build_many_linux_image.sh
@@ -16,7 +16,7 @@ fi
 
 if [[ -z "$sccache_ver" ]] ; then
     sccache_ver=`curl -sL -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
-        https://api.github.com/repos/mozilla/sccache/releases/latest | jq ".tag_name"`
+        https://api.github.com/repos/mozilla/sccache/releases/latest | jq -r ".tag_name"`
 fi
 
 echo "Building:
@@ -28,8 +28,8 @@ echo "Building:
 cd `mktemp -d`
 trap "rm -rf $PWD" EXIT
 
-curl -o sccache.tar.gz -L https://github.com/mozilla/sccache/releases/download/$sccache_ver/sccache-$sccache_ver-x86_64-unknown-linux-musl.tar.gz
-tar xvf sccache.tar.gz
+wget -nv https://github.com/mozilla/sccache/releases/download/$sccache_ver/sccache-$sccache_ver-x86_64-unknown-linux-musl.tar.gz
+tar xvf sccache*.tar.gz
 mv sccache-*/sccache .
 chmod 555 sccache
 
@@ -37,11 +37,15 @@ chmod 555 sccache
 
 echo "
 FROM $manylinux_image
+RUN cp \$(which bash) /usr/local/bin/suid_bash && chown 0:0 /usr/local/bin/suid_bash && chmod u+s /usr/local/bin/suid_bash
 RUN rpmkeys --import 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF' && \
-    curl https://download.mono-project.com/repo/centos7-stable.repo > /etc/yum.repos.d/mono-centos7-stable.repo
+    curl https://download.mono-project.com/repo/centos7-stable.repo > /etc/yum.repos.d/mono-centos7-stable.repo && \
+    sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://olcentgbl.trafficmanager.net|' /etc/yum.repos.d/CentOS-Base.repo && \
+    sed -ir 's/socket_timeout=3/socket_timeout=1/ ; s/maxhostfileage.*/maxhostfileage=1/ ;\
+            s/#?exclude.*/exclude=.edu/' /etc/yum/pluginconf.d/fastestmirror.conf
 ADD sccache /usr/local/bin/
 RUN yum update -y && \
-    yum install -y zip openssl-devel cyrus-sasl-devel devtoolset-10-libatomic-devel libcurl-devel && \
+    yum install -y zip jq openssl-devel cyrus-sasl-devel devtoolset-10-libatomic-devel libcurl-devel python3-devel && \
     rpm -Uvh --nodeps \$(repoquery --location mono-{core,web,devel,data,wcf,winfx}) && \
     yum clean all && touch /etc/arcticdb_deps_installed
 LABEL io.arcticdb.cibw_ver=\"${cibuildwheel_ver}\" io.arcticdb.base=\"${manylinux_image}\"
