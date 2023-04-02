820aaae9f98e17033409b0224c43105cadc12951
Qi <1662412+qc00@users.noreply.github.com>
2023-04-02 22:59:32 +0100

    Improve VCPKG caching
    * Supports reading man-group repo packages from a fork build
    * Supports Windows CIBuildWheel

--- build_tooling/vcpkg_caching.sh
+++ build_tooling/vcpkg_caching.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-cd $(realpath $(dirname $BASH_SOURCE))/../cpp/vcpkg
+pushd $(realpath $(dirname $BASH_SOURCE))/../cpp/vcpkg
 
 [[ -x vcpkg ]] || ./bootstrap-vcpkg.sh -disableMetrics
 nuget="`which mono` `./vcpkg fetch nuget | tail -n 1`"  # which mono will return empty on windows
 echo "Using nuget=$nuget"
-url="https://nuget.pkg.github.com/$nuget_user/index.json"
+
+VCPKG_BINARY_SOURCES="clear;nuget,github,readwrite"
+url="https://nuget.pkg.github.com/$VCPKG_NUGET_USER/index.json"
 $nuget sources add -source $url -storepasswordincleartext -name github \
-    -username "${nuget_user:?nuget_user environment variable is not set}" \
-    -password "${nuget_token:?nuget_token environment variable is not set}"
-$nuget setapikey "$nuget_token" -source $url
+    -username "${VCPKG_NUGET_USER:?environment variable is not set}" \
+    -password "${VCPKG_NUGET_TOKEN:?environment variable is not set}"
+$nuget setapikey "$VCPKG_NUGET_TOKEN" -source $url
+
+if [[ -n "$VCPKG_MAN_NUGET_TOKEN" ]] ; then
+    $nuget sources add -source "https://nuget.pkg.github.com/man-group/index.json" -storepasswordincleartext -name man \
+    -username "${VCPKG_MAN_NUGET_USER:-$VCPKG_NUGET_USER}" \
+    -password "$VCPKG_MAN_NUGET_TOKEN"
+    VCPKG_BINARY_SOURCES="$VCPKG_BINARY_SOURCES;nuget,man,read"
+fi
+
+popd
