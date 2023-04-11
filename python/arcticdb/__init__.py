43f33b234e3a70f286009c63182c65a792b64af3
Alex Seaton <alex.seaton@man.com>
2023-04-11 17:40:54 +0100

    Dynamically choose protobuf version without relying on arcticdb import
    Also make arcticc namespace package pkgutil-style for compat with `setup.py` installation.

--- python/arcticdb/__init__.py
+++ python/arcticdb/__init__.py
@@ -1,14 +1,6 @@
 import arcticdb_ext as _ext
-import os
+import os as _os
 import sys as _sys
-import google.protobuf as _protobuf
-
-_proto_ver = _protobuf.__version__.split(".")[0]
-if _proto_ver in "34":
-    # Use the namespace package (https://peps.python.org/pep-0420) feature to select the pb2 files matching the protobuf
-    _sys.path.append(os.path.join(os.path.dirname(__file__), "proto", _proto_ver))
-else:
-    raise NotImplementedError(f"We only support protobuf versions 3 & 4. You have {_protobuf.__version__}")
 
 from arcticdb.arctic import Arctic
 from arcticdb.options import LibraryOptions
@@ -17,6 +9,5 @@ import arcticdb.version_store.library as library
 from arcticdb.options import LibraryOptions
 from arcticdb.tools import set_config_from_env_vars
 
-set_config_from_env_vars(os.environ)
+set_config_from_env_vars(_os.environ)
 
-del os
