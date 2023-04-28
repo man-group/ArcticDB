f21cd946625cc9fdf237bb13abc0e457406e3971
DerThorsten <derthorstenbeier@gmail.com>
2023-04-28 14:46:09 +0200

    debugging

--- cpp/arcticdb/util/third_party/robin_hood.hpp
+++ cpp/arcticdb/util/third_party/robin_hood.hpp
@@ -206,7 +206,7 @@ return ROBIN_HOOD(BITSCANFORWARD)(&index, mask) ? static_cast<int>(index) \
 
 // workaround missing "is_trivially_copyable" in g++ < 5.0
 // See https://stackoverflow.com/a/31798726/48181
-#if defined(__GNUC__) && __GNUC__ < 5
+#if defined(__GNUC__) && __GNUC__ < 5 && !defined(__clang__)
 #    define ROBIN_HOOD_IS_TRIVIALLY_COPYABLE(...) __has_trivial_copy(__VA_ARGS__)
 #else
 #    define ROBIN_HOOD_IS_TRIVIALLY_COPYABLE(...) std::is_trivially_copyable<__VA_ARGS__>::value
