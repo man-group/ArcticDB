929ea68a80ef1ad9cec43c8a54ad7b8155c4c3b6
Julien Jerphanion <git@jjerphan.xyz>
2023-05-05 15:22:46 +0200

    build: Resolve Libevent via FindLibevent.cmake
    Extracted from https://github.com/man-group/ArcticDB/pull/318/
    
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>
    
    Co-authored-by: Thorsten Beier <derthorstenbeier@gmail.com>

---
+++ cpp/CMake/FindLibevent.cmake
@@ -0,0 +1,52 @@
+# The MIT License
+
+# Copyright (c) 2012, 2014, 2015, 2016 Tatsuhiro Tsujikawa
+# Copyright (c) 2012, 2014, 2015, 2016 nghttp2 contributors
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+# - Try to find the Libevent config processing library
+# Once done this will define
+#
+# LIBEVENT_FOUND - System has Libevent
+# LIBEVENT_INCLUDE_DIR - the Libevent include directory
+# LIBEVENT_LIBRARIES 0 The libraries needed to use LibEvLibeventent
+find_path     (LIBEVENT_INCLUDE_DIR NAMES event.h)
+find_library  (LIBEVENT_LIBRARY     NAMES event)
+find_library  (LIBEVENT_CORE        NAMES event_core)
+find_library  (LIBEVENT_EXTRA       NAMES event_extra)
+if (NOT EVHTP_DISABLE_EVTHR)
+    find_library (LIBEVENT_THREAD   NAMES event_pthreads)
+endif()
+if (NOT EVHTP_DISABLE_SSL)
+    find_library (LIBEVENT_SSL      NAMES event_openssl)
+endif()
+include (FindPackageHandleStandardArgs)
+set (LIBEVENT_INCLUDE_DIRS ${LIBEVENT_INCLUDE_DIR})
+set (LIBEVENT_LIBRARIES
+        ${LIBEVENT_LIBRARY}
+        ${LIBEVENT_SSL}
+        ${LIBEVENT_CORE}
+        ${LIBEVENT_EXTRA}
+        ${LIBEVENT_THREAD}
+        ${LIBEVENT_EXTRA})
+    find_package_handle_standard_args (Libevent DEFAULT_MSG LIBEVENT_LIBRARIES LIBEVENT_INCLUDE_DIR)
+mark_as_advanced(LIBEVENT_INCLUDE_DIRS LIBEVENT_LIBRARIES)
