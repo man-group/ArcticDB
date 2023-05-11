ac6baa23ad8430963a6c79d0ac2ebbc525122c8a
Vasil Danielov Pashov <vasil.pashov1@gmail.com>
2023-05-11 15:04:38 +0300

    Implementation for Decimal type with memory layout compatible with pyarrow's memory layout. (#341)
    * Load decimal from string
    * Convert decimal to string
    * Check for equality
    * Add unit tests
    
    Implementation is for 128 bit fixed point decimal. The number is stored
    as two little endian uint64_t variables. The format does not store the
    position of the decimal point.
    
    TODO:
    * Add arithmetic
    * Add comparison operators
    
    Co-authored-by: Vasil Pashov <Vasil.Pashov@man.com>

---
+++ cpp/arcticdb/util/decimal.hpp
@@ -0,0 +1,55 @@
+/* Copyright 2023 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+ */
+
+#pragma once
+#include <array>
+#include <string_view>
+#include <arcticdb/util/constructors.hpp>
+
+namespace arcticdb {
+
+    namespace util {
+        /**
+         * The decimal class is binary compatible with pyarrow's layout for 128 bit decimals. The format keeps
+         * the number as 128 bit integer in two's complement. The scale is not part of the format.
+         * https://github.com/apache/arrow/blob/45918a90a6ca1cf3fd67c256a7d6a240249e555a/cpp/src/arrow/util/decimal.h
+         * https://arrow.apache.org/docs/python/generated/pyarrow.decimal128.html
+         * Little endian order is used for the words and the bits inside each word.
+         */
+        class Decimal {
+        public:
+            Decimal();
+            explicit Decimal(std::string_view number);
+            ARCTICDB_MOVE_COPY_DEFAULT(Decimal);
+
+            /**
+             * Convert decimal to string
+             * @param scale Positive scale means the numbers after the decimal point.
+             *  Negative scale acts as multiplying by 10^(abs(scale))
+             * @example
+             *  Decimal("123").to_string(1) = "12.3"
+             *  Decimal("123").to_string(-1) = "1230"
+             *  Decimal("123").to_string(0) = "123"
+             */
+            [[nodiscard]] std::string to_string(int scale) const;
+            [[nodiscard]] bool is_negative() const;
+            [[nodiscard]] Decimal operator-() const;
+            [[nodiscard]] bool operator==(const Decimal&) const;
+            void negate();
+
+            constexpr static int max_scale = 38;
+        private:
+            void push_chunk(std::string_view);
+
+            enum {
+                LEAST_SIGNIFICANT_WORD_INDEX = 0,
+                MOST_SIGNIFICANT_WORD_INDEX = 1
+            };
+            std::array<uint64_t, 2> data_;
+        };
+    }
+}
