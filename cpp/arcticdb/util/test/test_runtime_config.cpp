0f3456869466975af8517d84dbc83ff54aa42522
Matthew Hertz <m@mehertz.com>
2023-05-11 10:48:05 +0100

    Add test to test case-insensitivity of ConfigsMap

--- cpp/arcticdb/util/test/test_runtime_config.cpp
+++ cpp/arcticdb/util/test/test_runtime_config.cpp
@@ -23,3 +23,18 @@ TEST(RuntimeConfig, PersistToProtobuf) {
     ASSERT_EQ(read_map.get_string("string_value"), "value");
     ASSERT_EQ(read_map.get_double("double_value"), 1.3);
 }
+
+TEST(RuntimeConfig, PersistToProtobufCaseInsensitive) {
+    using namespace arcticdb;
+
+    ConfigsMap test_map;
+    test_map.set_int("INT_value", 4);
+    test_map.set_string("STRING_value", "value");
+    test_map.set_double("DOUBLE_value", 1.3);
+
+    auto proto = test_map.to_proto();
+    auto read_map = ConfigsMap::from_proto(proto);
+    ASSERT_EQ(read_map.get_int("int_VALUE"), 4);
+    ASSERT_EQ(read_map.get_string("string_VALUE"), "value");
+    ASSERT_EQ(read_map.get_double("double_VALUE"), 1.3);
+}
