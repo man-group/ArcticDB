8647f6a4da5a29b2cf2741b5a65cf564270b10a2
Julien Jerphanion <git@jjerphan.xyz>
2023-05-15 10:13:56 +0200

    Use a temporary directory
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- python/tests/nonreg/arcticdb/adapters/test_lmdb_library_adapter.py
+++ python/tests/nonreg/arcticdb/adapters/test_lmdb_library_adapter.py
@@ -5,14 +5,16 @@ Use of this software is governed by the Business Source License 1.1 included in 
 
 As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
 """
+import tempfile
 
 from arcticdb import Arctic
 
 
 def test_creation_deletion_lmdb():
     # Non-regression test for #345
-    store = Arctic("lmdb://localhost:27017/historical_data")
-    store.create_library("option.1day")
-    assert store.list_libraries() == ["option.1day"]
-    store.delete_library("option.1day")
-    del store
+    with tempfile.TemporaryDirectory() as lmdb_temp_dirname:
+        store = Arctic(f"lmdb://{lmdb_temp_dirname}")
+        store.create_library("option.1day")
+        assert store.list_libraries() == ["option.1day"]
+        store.delete_library("option.1day")
+        del store
