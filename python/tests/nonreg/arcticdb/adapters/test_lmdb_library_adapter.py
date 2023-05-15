28b26fb9ff743f3b66353093ad6fd27764dac910
Julien Jerphanion <git@jjerphan.xyz>
2023-05-15 10:19:03 +0200

    Complete non-regression test Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- python/tests/nonreg/arcticdb/adapters/test_lmdb_library_adapter.py
+++ python/tests/nonreg/arcticdb/adapters/test_lmdb_library_adapter.py
@@ -12,9 +12,13 @@ from arcticdb import Arctic
 
 def test_creation_deletion_lmdb():
     # Non-regression test for #345
+
     with tempfile.TemporaryDirectory() as lmdb_temp_dirname:
+        # The following instructions must complete without failure.
         store = Arctic(f"lmdb://{lmdb_temp_dirname}")
+        assert store.list_libraries() == []
         store.create_library("option.1day")
         assert store.list_libraries() == ["option.1day"]
         store.delete_library("option.1day")
+        assert store.list_libraries() == []
         del store
