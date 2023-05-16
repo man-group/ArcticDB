4327099f9dfb7f12003b84ae54340844d020531c
DerThorsten <derthorstenbeier@gmail.com>
2023-05-16 14:40:16 +0200

    skip on mac

--- python/tests/integration/arcticdb/version_store/test_basic_version_store.py
+++ python/tests/integration/arcticdb/version_store/test_basic_version_store.py
@@ -40,10 +40,11 @@ from arcticdb.util.test import (
 from tests.util.date import DateRange
 
 
-if sys.platform == "win32":
+if sys.platform == "win32" or sys.platform == "darwin":
     # leave out Mongo as spinning up a Mongo instance in Windows CI is fiddly, and Mongo support is only
     # currently required for Linux for internal use.
-    SMOKE_TEST_VERSION_STORES = ["lmdb_version_store", "s3_version_store"]  # SKIP_WIN
+    # We also skip it on Mac as github actions containers don't work with macos
+    SMOKE_TEST_VERSION_STORES = ["lmdb_version_store", "s3_version_store"]  # SKIP_WIN and SKIP_MAC
 else:
     SMOKE_TEST_VERSION_STORES = ["lmdb_version_store", "s3_version_store", "mongo_version_store"]
 
