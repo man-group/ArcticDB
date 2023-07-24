d422bd5cdfba24bdce5d360bb9174c56c6daabc7
Georgi Petrov <Georgi.Petrov@man.com>
2023-07-24 13:43:35 +0000

    Fix formatting with black

--- python/tests/integration/arcticdb/version_store/test_basic_version_store.py
+++ python/tests/integration/arcticdb/version_store/test_basic_version_store.py
@@ -130,6 +130,7 @@ def test_unhandled_chars_default(s3_version_store, unhandled_char):
     syms = s3_version_store.list_symbols()
     assert sym not in syms
 
+
 @pytest.mark.parametrize("unhandled_char", [chr(0)])
 @mock.patch.dict(os.environ, {"ARCTICDB_NO_STRICT_SYMBOL_CHECK": "0"})
 def test_unhandled_chars_no_strict_check(s3_version_store, unhandled_char):
@@ -140,6 +141,7 @@ def test_unhandled_chars_no_strict_check(s3_version_store, unhandled_char):
     with pytest.raises(InternalException):
         s3_version_store.list_symbols()
 
+
 @pytest.mark.parametrize("unhandled_char", [chr(128)])
 @mock.patch.dict(os.environ, {"ARCTICDB_NO_STRICT_SYMBOL_CHECK": "0"})
 def test_unhandled_chars_already_present(s3_version_store, unhandled_char):
@@ -149,6 +151,7 @@ def test_unhandled_chars_already_present(s3_version_store, unhandled_char):
 
     with mock.patch.dict(os.environ, clear=True):
         s3_version_store.write(sym, df)
+
 
 @pytest.mark.parametrize("version_store", SMOKE_TEST_VERSION_STORES)
 def test_with_prune(request, version_store, symbol):
