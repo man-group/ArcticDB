4201a28435bf5062ca7169c2b68aa61656c98c9c
Georgi Petrov <gpetrov@man.com>
2023-08-02 16:36:22 +0300

    Testing with parametried lib names

--- python/tests/integration/test_real_storage.py
+++ python/tests/integration/test_real_storage.py
@@ -2,18 +2,28 @@ import pytest
 from arcticdb.arctic import Arctic
 
 
+def list_libraries():
+    OSes = ["linux"]
+    py_vers = ["3_6", "3_7", "3_8", "3_9", "3_10", "3_11"]
+    libs = list()
+    for os in OSes:
+        for ver in py_vers:
+            libs.append(f"{os}_{ver}")
+    return libs
+
+
+LIBRARIES = list_libraries()
+
+
 # TODO: Add a check if the real storage tests are enabled
-def test_real_s3_storage_read(real_s3_credentials):
+@pytest.mark.parametrize("library", LIBRARIES)
+def test_real_s3_storage_read(real_s3_credentials, library):
     endpoint, bucket, region, access_key, secret_key, clear = real_s3_credentials
     uri = f"s3s://{endpoint}:{bucket}?access={access_key}&secret={secret_key}&region={region}"
-
     ac = Arctic(uri)
-    libs = ac.list_libraries()
-    assert len(libs) > 0
-
-    for lib in libs:
-        symbols = lib.list_symbols()
-        assert len(symbols) > 0
-        for sym in symbols:
-            print(sym)
-            print(lib.read(sym).data)
+    lib = ac[library]
+    symbols = lib.list_symbols()
+    assert len(symbols) > 0
+    for sym in symbols:
+        print(sym)
+        print(lib.read(sym).data)
