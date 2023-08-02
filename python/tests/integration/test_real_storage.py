1039df49e235d8e47c70b5fe8f92c9810e8aca53
Georgi Petrov <gpetrov@man.com>
2023-08-02 15:18:49 +0300

    Test new seed job

--- python/tests/integration/test_real_storage.py
+++ python/tests/integration/test_real_storage.py
@@ -4,7 +4,7 @@ from arcticdb.arctic import Arctic
 
 # TODO: Add a check if the real storage tests are enabled
 def test_real_s3_storage_read(real_s3_credentials):
-    endpoint, bucket, region, access_key, secret_key, clear = real_s3_credentials()
+    endpoint, bucket, region, access_key, secret_key, clear = real_s3_credentials
     uri = f"s3s://{endpoint}:{bucket}?access={access_key}&secret={secret_key}&region={region}"
 
     ac = Arctic(uri)
