a9038798795fd0c50f796815c5e5a8801146e105
Georgi Petrov <gpetrov@man.com>
2023-08-23 10:50:59 +0300

    Patch an issue in test_symbol_list_read_only_compaction_needed when using lmdb

--- python/tests/integration/arcticdb/version_store/test_symbol_list.py
+++ python/tests/integration/arcticdb/version_store/test_symbol_list.py
@@ -97,7 +97,12 @@ def test_interleaved_store_read(version_store_factory):
 # Using S3 because LMDB does not allow OpenMode to be changed
 def test_symbol_list_read_only_compaction_needed(small_max_delta, object_version_store, compact_first):
     lib_write = object_version_store
-    lib_read = make_read_only(lib_write)
+    try:
+        lib_read = make_read_only(lib_write)
+    except Exception as ex:
+        if "E_INVALID_ARGUMENT Flags 0 and operating mode r are conflicting" == str(ex):
+            return
+        raise ex
     lt = lib_write.library_tool()
     old_compaction = []
     if compact_first:
