7ebe6ecbf99e7af3b561903673d48984d8f1ec46
Georgi Petrov <gpetrov@man.com>
2023-08-23 16:41:54 +0300

    Fix some tests

--- python/tests/stress/arcticdb/version_store/test_stress_version_map_compact.py
+++ python/tests/stress/arcticdb/version_store/test_stress_version_map_compact.py
@@ -22,7 +22,7 @@ def write_data(lib, sym, done, error):
     try:
         for idx1 in range(10):
             print("Iteration {}/10".format(idx1))
-            for idx2 in range(40):
+            for idx2 in range(20):
                 if idx2 % 4 == 3:
                     lib.delete_version(sym, delete_version_id)
                     delete_version_id += 1
@@ -42,46 +42,46 @@ def write_data(lib, sym, done, error):
     done.value = 1
 
 
-def compact_data(lib, sym, done):
+def compact_data(lib, sym, done, error):
     set_config_int("VersionMap.MaxVersionBlocks", 1)
-    while not done.value:
+    while not done.value or error.value:
         lib.version_store._compact_version_map(sym)
         time.sleep(random.uniform(0, 0.05))
 
 
-def read_data(lib, sym, done):
-    while not done.value:
+def read_data(lib, sym, done, error):
+    while not done.value or error.value:
         vs = lib.list_versions(sym)
         for idx in range(len(vs) - 1):
             assert vs[idx]["version"] == vs[idx + 1]["version"] + 1
 
 
-# def test_stress_version_map_compact(object_version_store, sym, capsys):
-#     done = Value("b", 0)
-#     error = Value("b", 0)
-#     lib = object_version_store
-#     lib.version_store._set_validate_version_map()
-#     with capsys.disabled():
-#         try:
-#             log.version.warn("Starting writer")
-#             writer = Process(name="writer", target=write_data, args=(lib, sym, done, error))
-#             writer.start()
-#             log.version.info("Starting compacter")
-#             compacter = Process(name="compacter", target=compact_data, args=(lib, sym, done))
-#             compacter.start()
-#             log.version.info("Starting reader")
-#             reader = Process(name="reader", target=read_data, args=(lib, sym, done))
-#             reader.start()
+def test_stress_version_map_compact(object_version_store, sym, capsys):
+    done = Value("b", 0)
+    error = Value("b", 0)
+    lib = object_version_store
+    lib.version_store._set_validate_version_map()
+    with capsys.disabled():
+        try:
+            log.version.warn("Starting writer")
+            writer = Process(name="writer", target=write_data, args=(lib, sym, done, error))
+            writer.start()
+            log.version.info("Starting compacter")
+            compacter = Process(name="compacter", target=compact_data, args=(lib, sym, done, error))
+            compacter.start()
+            log.version.info("Starting reader")
+            reader = Process(name="reader", target=read_data, args=(lib, sym, done, error))
+            reader.start()
 
-#             log.version.info("Joining writer")
-#             writer.join()
-#             log.version.info("Joining compacter")
-#             compacter.join()
-#             log.version.info("Joining reader")
-#             reader.join()
-#             assert error.value == 0
-#             log.version.info("Done")
-#         finally:
-#             log.version.info("Clearing library")
-#             lib.version_store.clear()
-#             log.version.info("Finished")
+            log.version.info("Joining writer")
+            writer.join()
+            log.version.info("Joining compacter")
+            compacter.join()
+            log.version.info("Joining reader")
+            reader.join()
+            assert error.value == 0
+            log.version.info("Done")
+        finally:
+            log.version.info("Clearing library")
+            lib.version_store.clear()
+            log.version.info("Finished")
