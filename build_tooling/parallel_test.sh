477afeac74ab7283f7bdd928c1cbf3b72722d469
Phoebus Mak <Phoebus.Mak@man.com>
2023-08-30 12:55:50 +0000

    Update parallel test

--- build_tooling/parallel_test.sh
+++ build_tooling/parallel_test.sh
@@ -26,8 +26,11 @@ function worker() {
     mkdir -p $new_root
     MSYS=winsymlinks:nativestrict ln -s "$(realpath "$tooling_dir/../python/tests")" $new_root/
     cd $new_root
-    $catch python -m pytest -vs --log-file="$TEST_OUTPUT_DIR/pytest-logger.$group.log" --junitxml="$TEST_OUTPUT_DIR/pytest.$group.xml" --splits $splits --group $group --durations-path="$duration_file" --store-durations --basetemp="$new_root/temp-pytest-output" "$@" 2>&1 | sed -ur "s#^(tests/.*/([^/]+\.py))?#$group: \2#"
-}
+    $catch python -m pytest -v --show-capture=yes --log-file="$TEST_OUTPUT_DIR/pytest-logger.$group.log" \
+        --junitxml="$TEST_OUTPUT_DIR/pytest.$group.xml" \
+        --splits $splits --group $group --durations-path="$duration_file" --store-durations \
+        --basetemp="$new_root/temp-pytest-output" \
+        "$@" 2>&1 | sed -ur "s#^(tests/.*/([^/]+\.py))?#$group: \2#"
 
 for i in `seq $splits` ; do
     worker $i "$@" &
