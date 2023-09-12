a29ef2cdd140b90ca927452ab0c1a714cf55d11e
Julien Jerphanion <git@jjerphan.xyz>
2023-09-12 11:27:10 +0200

    test: Reintroduce type checks again and document them
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- python/tests/stress/arcticdb/version_store/test_stress_multicolumn.py
+++ python/tests/stress/arcticdb/version_store/test_stress_multicolumn.py
@@ -80,4 +80,7 @@ def test_stress_multicolumn(lib_type, request):
         output_df = lib.read(name).data
         print("reading from arctic native: {}".format(pd.Timestamp("now") - now))
 
-        assert_frame_equal(test_data, output_df, check_dtype=False, check_index_type=False)
+        # ArcticDB stores empty columns under a dedicated `EMPTYVAL` type, so the types are not going
+        # to match with pandas until the first append.
+        is_not_empty = not test_data.empty
+        assert_frame_equal(test_data, output_df, check_dtype=is_not_empty, check_index_type=is_not_empty)
