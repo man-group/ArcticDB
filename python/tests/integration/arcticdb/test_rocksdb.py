2b657baa485a78af1dc4ecd04b32400c35f0fad7
Joe Iddon <joe.iddon@man.com>
2023-10-24 13:20:39 +0000

    Added rocksdb to the arctic_client, and skips for conda builds.

--- python/tests/integration/arcticdb/test_rocksdb.py
+++ python/tests/integration/arcticdb/test_rocksdb.py
@@ -1,9 +1,16 @@
 from arcticdb import Arctic
 import pandas as pd
+import pytest
 
 from arcticdb.util.test import assert_frame_equal
 
+# TODO: When RocksDB is enabled on all platforms can remove this import
+from arcticdb_ext.storage import LibraryManager
 
+@pytest.mark.skipif(
+    not LibraryManager.rocksdb_support,
+    reason="RocksDB is not supported on conda builds"
+)
 def test_rocksdb(tmpdir):
     ac = Arctic(f"rocksdb://{tmpdir}/test_rocks")
     ac.create_library("test_lib")
