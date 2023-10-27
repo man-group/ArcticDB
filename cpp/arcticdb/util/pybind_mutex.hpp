8b7b61a7e0bc7a2e0dbb800c4ba57064eb112aa3
Phoebus Mak <Phoebus.Mak@man.com>
2023-10-27 12:48:59 +0000

    Add multiprocess handling

--- cpp/arcticdb/util/pybind_mutex.hpp
+++ cpp/arcticdb/util/pybind_mutex.hpp
@@ -17,11 +17,27 @@ task runners can acquire the thread.
 */
 class SingleThreadMutexHolder {
 private:
+    inline static std::unique_ptr<std::mutex> single_thread_mutex = std::make_unique<std::mutex>();
+
     [[nodiscard]] static std::lock_guard<std::mutex> ensure_single_thread_cpp_pybind_entry() {
         py::gil_scoped_release release;
-        static std::mutex single_thread_mutex;
-        single_thread_mutex.lock(); //This is a hack for the mandatory std::adopt_lock below
-        return {single_thread_mutex, std::adopt_lock}; //Copy list-initialization will be used if the list is incomplete. 
+        single_thread_mutex->lock(); //This is a hack for the mandatory std::adopt_lock below
+        return {*single_thread_mutex, std::adopt_lock}; //Copy list-initialization will be used if the list is incomplete. 
     };
     std::lock_guard<std::mutex> single_thread_lck = ensure_single_thread_cpp_pybind_entry();
+public:
+/*
+    https://man7.org/linux/man-pages/man3/pthread_atfork.3.html
+    When fork is called in a multithreaded process, only the calling thread is duplicated in the child process. So locked mutex will stay locked in the 
+    child process. So special handling is required.
+    The thread being forked must not have a running task. And the parent process may have another threads running task, locking the mutex. As other threads
+    in the parent process won't be forked, at fork, it is safe to reset the mutex memory. The mutex cannot be simply unlocked as unlock() should only be
+    called in the thread called lock() previously, which cannot be the thread being forked.
+    Note: According to test, below mutex is automataically unlocked during fork. The observation deviates from the manual. So to play safe, mutex will
+    manually unlocked anyway, if it is locked.
+*/
+    static void reset_mutex() {
+        single_thread_mutex.release();
+        single_thread_mutex = std::make_unique<std::mutex>();
+    }
 };
