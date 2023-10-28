0b2f33940c9fda668e629677e5dcf9174f6411e0
Vasil Pashov <Vasil.Pashov@man.com>
2023-10-28 10:48:54 +0000

    Add options to use asan, tsan, and ubsan

--- python/tests/unit/arcticdb/version_store/test_array_column_type.py
+++ python/tests/unit/arcticdb/version_store/test_array_column_type.py
@@ -18,30 +18,30 @@ def array_type(request):
     yield request.param
 
 
-def assert_db_in_out_match(version_store, df_in):
-    version_store.write("test", df_in)
-    df_out = version_store.read("test")
+def assert_db_in_out_match(version_store, df_in, symbol):
+    version_store.write(symbol, df_in)
+    df_out = version_store.read(symbol)
     assert_frame_equal(df_in, df_out.data)
 
 
 class TestEmptyArrays:
     def test_single_empty_array(self, lmdb_version_store):
         df_in = pd.DataFrame({"col1": [np.array([])]})
-        assert_db_in_out_match(lmdb_version_store, df_in)
+        assert_db_in_out_match(lmdb_version_store, df_in, "test_single_empty_array")
 
     def test_multiple_empty_arrays(self, lmdb_version_store):
         df_in = pd.DataFrame({"col1": [np.array([]), np.array([]), np.array([])]})
-        assert_db_in_out_match(lmdb_version_store, df_in)
+        assert_db_in_out_match(lmdb_version_store, df_in, "test_multiple_empty_arrays")
 
     def test_empty_array_can_coexist_with_nonempty_arrays(self, lmdb_version_store, array_type):
         df_in = pd.DataFrame({"col1": [np.array([]), np.array([1, 2, 3]).astype(array_type), np.array([])]})
-        assert_db_in_out_match(lmdb_version_store, df_in)
+        assert_db_in_out_match(lmdb_version_store, df_in, "test_empty_array_can_coexist_with_nonempty_arrays")
 
 
 class TestNonEmptyArrays:
     def test_single_array(self, lmdb_version_store, array_type):
         df_in = pd.DataFrame({"col1": [np.array([1, 2, 3]).astype(array_type)]})
-        assert_db_in_out_match(lmdb_version_store, df_in)
+        assert_db_in_out_match(lmdb_version_store, df_in, "test_single_array")
 
     def test_differently_shaped_rows(self, lmdb_version_store, array_type):
         df_in = pd.DataFrame(
@@ -54,13 +54,13 @@ class TestNonEmptyArrays:
                 ]
             }
         )
-        assert_db_in_out_match(lmdb_version_store, df_in)
+        assert_db_in_out_match(lmdb_version_store, df_in, "test_differently_shaped_rows")
 
     def test_rows_can_be_none(self, lmdb_version_store, array_type):
         df_in = pd.DataFrame({"col1": [None, np.array([1, 2]).astype(array_type), None]})
-        assert_db_in_out_match(lmdb_version_store, df_in)
+        assert_db_in_out_match(lmdb_version_store, df_in, "test_rows_can_be_none")
 
     def test_can_contain_nan(self, lmdb_version_store):
         # Note integers don't have a NaN value
         df_in = pd.DataFrame({"col1": [np.array([np.NAN, float("NaN")])]})
-        assert_db_in_out_match(lmdb_version_store, df_in)
+        assert_db_in_out_match(lmdb_version_store, df_in, "test_can_contain_nan")
