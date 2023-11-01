e3ecdf3c63bddbe2ddd7ab4ac3e04833cec66aae
Qi <1662412+qc00@users.noreply.github.com>
2023-11-01 19:09:46 +0000

    Centralise the pytest marks
    + Replaced the manual fixture parameterization in test_deletion.py

--- python/tests/util/mark.py
+++ python/tests/util/mark.py
@@ -5,10 +5,44 @@ Use of this software is governed by the Business Source License 1.1 included in 
 
 As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
 """
-
+import os
+import sys
+import pytest
 from typing import Union
 from datetime import date
 from numpy import datetime64
+
+# TODO: Some tests are either segfaulting or failing on MacOS with conda builds.
+# This is meant to be used as a temporary flag to skip/xfail those tests.
+MACOS_CONDA_BUILD = sys.platform == "darwin" and os.getenv("ARCTICDB_USING_CONDA", "0") == "1"
+_MACOS_CONDA_BUILD_SKIP_REASON = (
+    "Tests fail for macOS conda builds, either because Azurite is improperly configured"
+    "on the CI or because there's problem with Azure SDK for C++ in this configuration."
+)
+
+# These two should become pytest marks as opposed to variables feeding into skipif
+PERSISTENT_STORAGE_TESTS_ENABLED = os.getenv("ARCTICDB_PERSISTENT_STORAGE_TESTS") == "1"
+FAST_TESTS_ONLY = os.getenv("ARCTICDB_FAST_TESTS_ONLY") == "1"
+
+
+# !!!!!!!!!!!!!!!!!!!!!! Below mark (variable) names should reflect where they will be used, not what they do.
+# This is to avoid the risk of the name becoming out of sync with the actual condition.
+
+AZURE_TESTS_MARK = pytest.mark.skipif(FAST_TESTS_ONLY or MACOS_CONDA_BUILD, reason=_MACOS_CONDA_BUILD_SKIP_REASON)
+"""Mark to skip all Azure tests when MACOS_CONDA_BUILD or ARCTICDB_FAST_TESTS_ONLY is set."""
+
+MONGO_TESTS_MARK = pytest.mark.skipif(
+    FAST_TESTS_ONLY or sys.platform != "linux",
+    reason="Skipping mongo tests under ARCTICDB_FAST_TESTS_ONLY",
+)
+"""Mark on tests using the mongo storage fixtures. Currently skips if ARCTICDB_FAST_TESTS_ONLY."""
+
+REAL_S3_TESTS_MARK = pytest.mark.skipif(
+    FAST_TESTS_ONLY or not PERSISTENT_STORAGE_TESTS_ENABLED,
+    reason="Can be used only when persistent storage is enabled",
+)
+"""Mark on tests using the real (i.e. hosted by AWS as opposed to moto) S3.
+Currently controlled by the ARCTICDB_PERSISTENT_STORAGE_TESTS and ARCTICDB_FAST_TESTS_ONLY env vars."""
 
 
 def _no_op_decorator(fun):
