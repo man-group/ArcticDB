801cf4b6f0f9ec0a997311fbdc14639537d3bbb6
Hind-M <70631848+Hind-M@users.noreply.github.com>
2023-11-30 11:02:57 +0100

    [Column] Move some definitions to cpp file (#1100)
    With an aim of reducing build time and improve code readability, moving
    some definitions from header to cpp file.

--- cpp/arcticdb/column_store/column.cpp
+++ cpp/arcticdb/column_store/column.cpp
@@ -6,9 +6,155 @@
  */
 
 #include <arcticdb/column_store/column.hpp>
-#include <arcticdb/util/sparse_utils.hpp>
 
 namespace arcticdb {
+
+// Column operators
+bool operator==(const Column& left, const Column& right) {
+    if (left.row_count() != right.row_count())
+        return false;
+
+    if (left.type_ != right.type_)
+        return false;
+
+    return left.type_.visit_tag([&left, &right](auto l_impl) {
+        using LeftType= std::decay_t<decltype(l_impl)>;
+        using LeftRawType = typename LeftType::DataTypeTag::raw_type;
+
+        return right.type_.visit_tag([&left, &right](auto r_impl) {
+            using RightType= std::decay_t<decltype(r_impl)>;
+            using RightRawType = typename RightType::DataTypeTag::raw_type;
+
+            if constexpr(std::is_same_v < LeftRawType, RightRawType>) {
+                for (auto i = 0u; i < left.row_count(); ++i) {
+                    auto left_val =left.scalar_at<LeftRawType>(i);
+                    auto right_val =right.scalar_at<RightRawType>(i);
+                    if (left_val != right_val)
+                        return false;
+                }
+                return true;
+            } else {
+                return false;
+            }
+        });
+    });
+}
+
+bool operator!=(const Column& left, const Column& right) {
+    return !(left == right);
+}
+
+// Column public methods
+Column Column::clone() const {
+    Column output;
+
+    output.data_ = data_.clone();
+    output.shapes_ = shapes_.clone();
+
+    output.offsets_ = offsets_;
+    output.type_ = type_;
+    output.orig_type_ = orig_type_;
+    output.last_logical_row_ = last_logical_row_;
+    output.last_physical_row_ = last_physical_row_;
+    output.inflated_ = inflated_;
+    output.allow_sparse_ = allow_sparse_;
+    output.sparse_map_ = sparse_map_;
+
+    return output;
+}
+
+bool Column::is_sparse() const {
+    if(last_logical_row_ != last_physical_row_) {
+        util::check(static_cast<bool>(sparse_map_), "Expected sparse map in column with logical row {} and physical row {}", last_logical_row_, last_physical_row_);
+        return true;
+    }
+    return false;
+}
+
+bool Column::sparse_permitted() const {
+    return allow_sparse_;
+}
+
+ssize_t Column::last_row() const {
+    return last_logical_row_;
+}
+
+void Column::check_magic() const {
+    magic_.check();
+}
+
+void Column::unsparsify(size_t num_rows) {
+    if(!sparse_map_)
+        return;
+
+    type_.visit_tag([that=this, num_rows] (const auto tdt) {
+        using TagType = decltype(tdt);
+        using RawType = typename TagType::DataTypeTag::raw_type;
+        const auto dest_bytes = num_rows * sizeof(RawType);
+        auto dest = ChunkedBuffer::presized(dest_bytes);
+        util::default_initialize<TagType>(dest.data(), dest_bytes);
+        util::expand_dense_buffer_using_bitmap<RawType>(that->sparse_map_.value(), that->data_.buffer().data(), dest.data());
+        std::swap(dest, that->data_.buffer());
+    });
+    sparse_map_ = std::nullopt;
+    last_logical_row_ = last_physical_row_ = static_cast<ssize_t>(num_rows) - 1;
+
+    ARCTICDB_DEBUG(log::version(), "Unsparsify: last_logical_row_: {} last_physical_row_: {}", last_logical_row_, last_physical_row_);
+}
+
+void Column::sparsify() {
+    type().visit_tag([that=this](auto type_desc_tag) {
+        using TDT = decltype(type_desc_tag);
+        using RawType = typename TDT::DataTypeTag::raw_type;
+        if constexpr(is_floating_point_type(TDT::DataTypeTag::data_type)) {
+            auto raw_ptr =reinterpret_cast<const RawType*>(that->ptr());
+            auto buffer = util::scan_floating_point_to_sparse(raw_ptr, that->row_count(), that->sparse_map());
+            std::swap(that->data().buffer(), buffer);
+            that->last_physical_row_ = that->sparse_map().count() - 1;
+        }
+    });
+}
+
+void Column::string_array_prologue(ssize_t row_offset, size_t num_strings) {
+    util::check_arg(last_logical_row_ + 1 == row_offset, "string_array_prologue expected row {}, actual {} ", last_logical_row_ + 1, row_offset);
+    shapes_.ensure<shape_t>();
+    auto shape_cursor = reinterpret_cast<shape_t *>(shapes_.ptr());
+    *shape_cursor = shape_t(num_strings);
+    data_.ensure<StringPool::offset_t>(num_strings);
+}
+
+void Column::string_array_epilogue(size_t num_strings) {
+    data_.commit();
+    shapes_.commit();
+    update_offsets(num_strings * sizeof(StringPool::offset_t));
+    ++last_logical_row_;
+}
+
+void Column::set_string_array(ssize_t row_offset,
+                        size_t string_size,
+                        size_t num_strings,
+                        char *input,
+                        StringPool &string_pool) {
+    string_array_prologue(row_offset, num_strings);
+    auto data_ptr = reinterpret_cast<StringPool::offset_t*>(data_.ptr());
+    for (size_t i = 0; i < num_strings; ++i) {
+        auto off = string_pool.get(std::string_view(input, string_size));
+        *data_ptr++ = off.offset();
+        input += string_size;
+    }
+    string_array_epilogue(num_strings);
+}
+
+void Column::set_string_list(ssize_t row_offset, const std::vector<std::string> &input, StringPool &string_pool) {
+    string_array_prologue(row_offset, input.size());
+    auto data_ptr = reinterpret_cast<StringPool::offset_t *>(data_.ptr());
+    for (const auto &str : input) {
+        auto off = string_pool.get(str.data());
+        *data_ptr++ = off.offset();
+    }
+    string_array_epilogue(input.size());
+}
+
 void Column::append_sparse_map(const util::BitMagic& bv, position_t at_row) {
     auto& sm = sparse_map();
 
@@ -137,6 +283,207 @@ void Column::sort_external(const JiveTable& jive_table) {
     }
 }
 
+void Column::mark_absent_rows(size_t num_rows) {
+    if(sparse_permitted()) {
+        if(!sparse_map_){
+            if (last_physical_row_ != -1)
+                backfill_sparse_map(last_physical_row_);
+            else
+                (void)sparse_map();
+        }
+        last_logical_row_ += static_cast<ssize_t>(num_rows);
+    } else {
+        util::check(last_logical_row_ == last_physical_row_, "Expected logical and physical rows to be equal in non-sparse column");
+        default_initialize_rows(last_logical_row_ + 1, num_rows, true);
+    }
+}
+
+void Column::default_initialize_rows(size_t start_pos, size_t num_rows, bool ensure_alloc) {
+    type_.visit_tag([that=this, start_pos, num_rows, ensure_alloc](auto tag) {
+        using T= std::decay_t<decltype(tag)>;
+        using RawType = typename T::DataTypeTag::raw_type;
+        const auto bytes = (num_rows * sizeof(RawType));
+
+        if(ensure_alloc)
+            that->data_.ensure<uint8_t>(bytes);
+
+        auto type_ptr = that->data_.ptr_cast<RawType>(start_pos, bytes);
+        util::default_initialize<T>(reinterpret_cast<uint8_t*>(type_ptr), bytes);
+
+        if(ensure_alloc)
+            that->data_.commit();
+
+        that->last_logical_row_ += static_cast<ssize_t>(num_rows);
+        that->last_physical_row_ += static_cast<ssize_t>(num_rows);
+    });
+}
+
+void Column::set_row_data(size_t row_id) {
+    last_logical_row_ = row_id;
+    const auto last_stored_row = row_count() - 1;
+    if(sparse_map_) {
+        last_physical_row_ = sparse_map_->count() - 1;
+    }
+    else if (last_logical_row_ != last_stored_row) {
+        last_physical_row_ = last_stored_row;
+        backfill_sparse_map(last_stored_row);
+    } else {
+        last_physical_row_ = last_logical_row_;
+    }
+    ARCTICDB_TRACE(log::version(), "Set row data: last_logical_row_: {}, last_physical_row_: {}", last_logical_row_, last_physical_row_);
+}
+
+size_t Column::get_physical_offset(size_t row) const {
+    if(!is_sparse())
+        return row;
+
+    if(row == 0u)
+        return 0u;
+
+    // TODO: cache index
+    auto rs = std::make_unique<bm::bvector<>::rs_index_type>();
+    sparse_map().build_rs_index(rs.get());
+    return sparse_map().count_to(bv_size(row - 1), *rs);
+}
+
+void Column::set_sparse_map(util::BitSet&& bitset) {
+    sparse_map_ = std::move(bitset);
+}
+
+std::optional<position_t> Column::get_physical_row(position_t row) const {
+    if(row > last_logical_row_) {
+        if(sparse_permitted())
+            return std::nullopt;
+        else
+            util::raise_rte("Scalar index {} out of bounds in column of size {}", row, row_count());
+    }
+
+    util::check_arg(is_scalar(), "get_scalar requested on non-scalar column");
+    if(is_sparse() && !sparse_map().get_bit(bv_size(row)))
+        return std::nullopt;
+
+    return get_physical_offset(row);
+}
+
+bool Column::has_value_at(position_t row) const {
+    return !is_sparse() || sparse_map().get_bit(bv_size(row));
+}
+
+void Column::set_allow_sparse(bool value) {
+    allow_sparse_ = value;
+}
+
+void Column::set_shapes_buffer(size_t row_count) {
+    CursoredBuffer<Buffer> shapes;
+    shapes.ensure<shape_t>();
+    shape_t rc(row_count);
+    memcpy(shapes.ptr(), &rc, sizeof(shape_t));
+    shapes.commit();
+    swap(shapes_, shapes);
+}
+
+// The following two methods inflate (reduplicate) numpy string arrays that are potentially multi-dimensional,
+// i.e where the value is not a string but an array of strings
+void Column::inflate_string_array(
+        const TensorType<OffsetString::offset_t> &string_refs,
+        CursoredBuffer<ChunkedBuffer> &data,
+        CursoredBuffer<Buffer> &shapes,
+        std::vector<position_t> &offsets,
+        const StringPool &string_pool) {
+    ssize_t max_size = 0;
+    for (int i = 0; i < string_refs.size(); ++i)
+        max_size = std::max(max_size, static_cast<ssize_t>(string_pool.get_const_view(string_refs.at(i)).size()));
+
+    size_t data_size = static_cast<size_t>(max_size) * string_refs.size();
+    data.ensure<uint8_t >(data_size);
+    shapes.ensure<shape_t>();
+    auto str_data = data.cursor();
+    memset(str_data, 0, data_size);
+    for (int i = 0; i < string_refs.size(); ++i) {
+        auto str = string_pool.get_const_view(string_refs.at(i));
+        memcpy(&str_data[i * max_size], str.data(), str.size());
+    }
+
+    offsets.push_back(data_size);
+    shape_t s = string_refs.size();
+    memcpy(shapes.cursor(), &s, sizeof(shape_t));
+    data.commit();
+    shapes.commit();
+}
+
+void Column::inflate_string_arrays(const StringPool &string_pool) {
+    util::check_arg(is_fixed_string_type(type().data_type()), "Can only inflate fixed string array types");
+    util::check_arg(type().dimension() == Dimension::Dim1, "Fixed string inflation is for array types only");
+
+    CursoredBuffer<ChunkedBuffer> data;
+    CursoredBuffer<Buffer> shapes;
+    std::vector<position_t> offsets;
+    for (position_t row = 0; row < row_count(); ++row) {
+        auto string_refs = tensor_at<OffsetString::offset_t>(row).value();
+        inflate_string_array(string_refs, data, shapes, offsets, string_pool);
+    }
+
+    using std::swap;
+    swap(shapes_, shapes);
+    swap(offsets_, offsets);
+    swap(data_, data);
+    inflated_ = true;
+}
+
+// Used when the column has been inflated externally, i.e. because it has be done
+// in a pipeline of tiled sub-segments
+void Column::set_inflated(size_t inflated_count) {
+    set_shapes_buffer(inflated_count);
+    inflated_ = true;
+}
+
+bool Column::is_inflated() const {
+    return inflated_;
+}
+
+void Column::change_type(DataType target_type) {
+    util::check(shapes_.empty(), "Can't change type on multi-dimensional column with type {}", type_);
+    if(type_.data_type() == target_type)
+        return;
+
+    CursoredBuffer<ChunkedBuffer> buf;
+    for(const auto& block : data_.buffer().blocks()) {
+        details::visit_type(type_.data_type(), [&buf, &block, type=type_, target_type] (auto&& source_dtt) {
+            using source_raw_type = typename std::decay_t<decltype(source_dtt)>::raw_type;
+            details::visit_type(target_type, [&buf, &block, &type, target_type] (auto&& target_dtt) {
+                using target_raw_type = typename std::decay_t<decltype(target_dtt)>::raw_type;
+
+                if constexpr (!is_narrowing_conversion<source_raw_type, target_raw_type>() &&
+                        !std::is_same_v<source_raw_type, bool>) {
+                    auto num_values = block->bytes() / sizeof(source_raw_type);
+                    buf.ensure<target_raw_type>(num_values);
+                    auto src = reinterpret_cast<const source_raw_type *>(block->data());
+                    auto dest = reinterpret_cast<target_raw_type *>(buf.cursor());
+                    for (auto i = 0u; i < num_values; ++i)
+                        dest[i] = target_raw_type(src[i]);
+                }
+                else {
+                    util::raise_rte("Cannot narrow column type from {} to {}", type, target_type);
+                }
+            });
+        });
+    }
+    type_ = TypeDescriptor{target_type, type_.dimension()};
+    std::swap(data_, buf);
+}
+
+position_t Column::row_count() const {
+    if(!is_scalar()) {
+        // TODO check with strings as well
+        return num_shapes() / shape_t(type_.dimension());
+    }
+
+    if(is_sequence_type(type().data_type()) && inflated_ && is_fixed_string_type(type().data_type()))
+        return inflated_row_count();
+
+    return data_.bytes() / size_t(item_size());
+}
+
 std::vector<std::shared_ptr<Column>> Column::split(const std::shared_ptr<Column>& column, size_t rows) {
     // TODO: Doesn't work the way you would expect for sparse columns - the bytes for each buffer won't be uniform
     const auto bytes = rows * get_type_size(column->type().data_type());
@@ -224,6 +571,100 @@ std::shared_ptr<Column> Column::truncate(const std::shared_ptr<Column>& column, 
     }
     res->set_row_data(end_row - (start_row + 1));
     return res;
+}
+
+// Column private methods
+position_t Column::last_offset() const {
+    return offsets_.empty() ? 0 : *offsets_.rbegin();
+}
+
+void Column::update_offsets(size_t nbytes) {
+    offsets_.push_back(last_offset() + nbytes);
+}
+
+bool Column::is_scalar() const {
+    return type().dimension() == Dimension(0);
+}
+
+const shape_t* Column::shape_index(position_t idx) const {
+    if (is_scalar())
+        return nullptr;
+
+    return shapes_.buffer().ptr_cast<shape_t>(idx * size_t(type_.dimension()) * sizeof(shape_t), sizeof(shape_t));
+}
+
+position_t Column::bytes_offset(position_t idx) const {
+    regenerate_offsets();
+    util::check_arg(idx < row_count(),
+                    "bytes_offset index {} out of bounds in column of size {}",
+                    idx,
+                    row_count());
+
+    if (idx == 0)
+        return 0;
+
+    if (is_scalar())
+        return scalar_offset(idx);
+
+    util::check(size_t(idx - 1) < offsets_.size(), "Offset {} out of range, only have {}", idx - 1, offsets_.size());
+    return offsets_[idx - 1];
+}
+
+position_t Column::scalar_offset(position_t idx) const {
+    return idx * item_size();
+}
+
+size_t Column::item_size() const {
+    if(is_sequence_type(type().data_type()) && inflated_ && is_fixed_string_type(type().data_type())) {
+        return data_.bytes() / inflated_row_count();
+    }
+
+    return get_type_size(type().data_type());
+}
+
+size_t Column::inflated_row_count() const {
+    // using the content of shapes since the item size
+    // from the datatype is no longer reliable
+    return *reinterpret_cast<const size_t*>(shapes_.data());
+}
+
+size_t Column::num_shapes() const  {
+    return shapes_.bytes() / sizeof(shape_t);
+}
+
+void Column::set_sparse_bit_for_row(size_t sparse_location) {
+    sparse_map()[bv_size(sparse_location)] = true;
+}
+
+bool Column::empty() const {
+    return row_count() == 0;
+}
+
+void Column::regenerate_offsets() const {
+    if (ARCTICDB_LIKELY(is_scalar() || !offsets_.empty()))
+        return;
+
+    position_t pos = 0;
+    for (position_t i = 0, j = i + position_t(type_.dimension()); j < position_t(num_shapes());
+            i = j, j += position_t(type_.dimension())) {
+        auto num_elements =
+                position_t(std::accumulate(shape_index(i), shape_index(j), shape_t(1), std::multiplies<>()));
+        auto offset = num_elements * get_type_size(type_.data_type());
+        offsets_.push_back(pos + offset);
+        pos += offset;
+    }
+}
+
+[[nodiscard]] util::BitMagic& Column::sparse_map() {
+    if(!sparse_map_)
+        sparse_map_ = std::make_optional<util::BitMagic>(0);
+
+    return sparse_map_.value();
+}
+
+const util::BitMagic& Column::sparse_map() const {
+    util::check(static_cast<bool>(sparse_map_), "Expected sparse map when it was not set");
+    return sparse_map_.value();
 }
 
 } //namespace arcticdb
