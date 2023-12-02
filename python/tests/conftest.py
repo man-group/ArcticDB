4c1fce84f5740098ecee10d5f88b81a98e2a70d8
William Dealtry <william.dealtry@gmail.com>
2023-12-02 15:25:34 +0000

    Fix conftest

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -49,59 +49,9 @@ def sym(request):
 
 
 @pytest.fixture()
-def sym():
-    return "test" + datetime.utcnow().strftime("%Y-%m-%dT%H_%M_%S_%f")
-
-
-@pytest.fixture()
-def lib_name():
-    return f"local.test.{random.randint(0, 999)}_{datetime.utcnow().strftime('%Y-%m-%dT%H_%M_%S_%f')}"
-
-
-@pytest.fixture
-def arcticdb_test_s3_config(moto_s3_endpoint_and_credentials):
-    endpoint, port, bucket, aws_access_key, aws_secret_key = moto_s3_endpoint_and_credentials
-
-    def create(lib_name):
-        return create_test_s3_cfg(lib_name, aws_access_key, aws_secret_key, bucket, endpoint)
-
-    return create
-
-
-@pytest.fixture
-def arcticdb_test_azure_config(azurite_azure_test_connection_setting, azurite_azure_uri):
-    def create(lib_name):
-        (endpoint, container, credential_name, credential_key, ca_cert_path) = azurite_azure_test_connection_setting
-        return create_test_azure_cfg(
-            lib_name=lib_name,
-            credential_name=credential_name,
-            credential_key=credential_key,
-            container_name=container,
-            endpoint=azurite_azure_uri,
-            ca_cert_path=ca_cert_path,
-        )
-
-    return create
-
-
-def _version_store_factory_impl(
-        used, make_cfg, default_name, *, name: str = None, reuse_name=False, **kwargs
-) -> NativeVersionStore:
-    """Common logic behind all the factory fixtures"""
-    name = name or default_name
-    if name == "_unique_":
-        name = name + str(len(used))
-
-    assert (name not in used) or reuse_name, f"{name} is already in use"
-    cfg = make_cfg(name)
-    lib = cfg.env_by_id[Defaults.ENV].lib_by_path[name]
-    # Use symbol list by default (can still be overridden by kwargs)
-    lib.version.symbol_list = True
-    apply_lib_cfg(lib, kwargs)
-    out = ArcticMemoryConfig(cfg, Defaults.ENV)[name]
-    used[name] = out
-    return out
->>>>>>> 53b03983 (Batch read versions with snapshots)
+def lib_name(request):
+    name = re.sub(r"[^\w]", "_", request.node.name)[:30]
+    return f"{name}.{random.randint(0, 999)}_{datetime.utcnow().strftime('%Y-%m-%dT%H_%M_%S_%f')}"
 
 
 @enum.unique
@@ -136,64 +86,9 @@ def s3_storage_factory():
 
 
 @pytest.fixture
-<<<<<<< HEAD
 def s3_storage(s3_storage_factory):
     with s3_storage_factory.create_fixture() as f:
         yield f
-=======
-def version_store_factory(lib_name, tmpdir):
-    """Factory to create any number of distinct LMDB libs with the given WriteOptions or VersionStoreConfig.
-
-    Accepts legacy options col_per_group and row_per_segment.
-    `name` can be a magical value "_unique_" which will create libs with unique names.
-    The values in `lmdb_config` populates the `LmdbConfig` Protobuf that creates the `Library` in C++. On Windows, it
-    can be used to override the `map_size`.
-    """
-    used: Dict[str, NativeVersionStore] = {}
-    def create_version_store(
-            col_per_group: Optional[int] = None,
-            row_per_segment: Optional[int] = None,
-            lmdb_config: Dict[str, Any] = None,
-            override_name: str = None,
-            **kwargs,
-    ) -> NativeVersionStore:
-        if col_per_group is not None and "column_group_size" not in kwargs:
-            kwargs["column_group_size"] = col_per_group
-        if row_per_segment is not None and "segment_row_size" not in kwargs:
-            kwargs["segment_row_size"] = row_per_segment
-        if lmdb_config is None:
-            # 128 MiB - needs to be reasonably small else Windows build runs out of disk
-            lmdb_config = {"map_size": 128 * (1 << 20)}
-
-        if override_name is not None:
-            library_name = override_name
-        else:
-            library_name = lib_name
-
-        cfg_factory = functools.partial(create_test_lmdb_cfg, db_dir=str(tmpdir), lmdb_config=lmdb_config)
-        return _version_store_factory_impl(used, cfg_factory, library_name, **kwargs)
-
-    try:
-        yield create_version_store
-    except RuntimeError as e:
-        if "mdb_" in str(e):  # Dump keys when we get uncaught exception from LMDB:
-            for store in used.values():
-                print(store)
-                lt = store.library_tool()
-                for kt in lt.key_types():
-                    for key in lt.find_keys(kt):
-                        print(key)
-        raise
-    finally:
-        for result in used.values():
-            #  pytest holds a member variable `cached_result` equal to `result` above which keeps the storage alive and
-            #  locked. See https://github.com/pytest-dev/pytest/issues/5642 . So we need to decref the C++ objects keeping
-            #  the LMDB env open before they will release the lock and allow Windows to delete the LMDB files.
-            result.version_store = None
-            result._library = None
-
-        shutil.rmtree(tmpdir, ignore_errors=True)
->>>>>>> 53b03983 (Batch read versions with snapshots)
 
 
 @pytest.fixture(scope="session")
@@ -201,22 +96,9 @@ def real_s3_storage_factory():
     return real_s3_from_environment_variables()
 
 
-<<<<<<< HEAD
 @pytest.fixture(scope="session")
 def real_s3_storage_without_clean_up(real_s3_storage_factory):
     return real_s3_storage_factory.create_fixture()
-=======
-@pytest.fixture(scope="function")
-def library_factory(arctic_client, lib_name):
-    used: Dict[str, Library] = {}
-
-    def create_library(
-            library_options: Optional[LibraryOptions] = None,
-    ) -> Library:
-        return _arctic_library_factory_impl(used, lib_name, arctic_client, library_options)
-
-    return create_library
->>>>>>> 53b03983 (Batch read versions with snapshots)
 
 
 @pytest.fixture
@@ -232,61 +114,9 @@ def azurite_storage_factory():
 
 
 @pytest.fixture
-<<<<<<< HEAD
 def azurite_storage(azurite_storage_factory: AzuriteStorageFixtureFactory):
     with azurite_storage_factory.create_fixture() as f:
         yield f
-=======
-def real_s3_store_factory(lib_name, **kwargs):
-    endpoint, bucket, region, aws_access_key, aws_secret_key, path_prefix, clear = real_s3_credentials(
-        shared_path=False
-    )
-
-    # Not exposing the config factory to discourage people from creating libs that won't get cleaned up
-    def make_cfg(name):
-        # with_prefix=False to allow reuse_name to work correctly
-        return create_test_s3_cfg(
-            name,
-            aws_access_key,
-            aws_secret_key,
-            bucket,
-            endpoint,
-            with_prefix=path_prefix,
-            is_https=True,
-            region=region,
-        )
-
-    used = {}
-
-    def create_version_store(
-            col_per_group: Optional[int] = None,
-            row_per_segment: Optional[int] = None,
-            # lmdb_config: Dict[str, Any] = {},
-            override_name: str = None,
-            **kwargs,
-    ) -> NativeVersionStore:
-        if col_per_group is not None and "column_group_size" not in kwargs:
-            kwargs["column_group_size"] = col_per_group
-        if row_per_segment is not None and "segment_row_size" not in kwargs:
-            kwargs["segment_row_size"] = row_per_segment
-
-        if "lmdb_config" in kwargs:
-            del kwargs["lmdb_config"]
-
-        if override_name is not None:
-            library_name = override_name
-        else:
-            library_name = lib_name
-
-        return _version_store_factory_impl(used, make_cfg, library_name, **kwargs)
-
-    try:
-        yield create_version_store
-    finally:
-        if clear:
-            for lib in used.values():
-                lib.version_store.clear()
->>>>>>> 53b03983 (Batch read versions with snapshots)
 
 
 @pytest.fixture(scope="session")
@@ -377,73 +207,6 @@ def real_s3_version_store_dynamic_schema(real_s3_store_factory):
 
 
 @pytest.fixture
-<<<<<<< HEAD
-=======
-def azure_store_factory(lib_name, arcticdb_test_azure_config, azure_client_and_create_container):
-    """Factory to create any number of Azure libs with the given WriteOptions or VersionStoreConfig.
-
-    `name` can be a magical value "_unique_" which will create libs with unique names.
-    This factory will clean up any libraries requested
-    """
-    used = {}
-    try:
-        yield functools.partial(_version_store_factory_impl, used, arcticdb_test_azure_config, lib_name)
-    finally:
-        for lib in used.values():
-            lib.version_store.clear()
-
-
-@pytest.fixture
-def mongo_test_uri(request):
-    """Similar capability to `s3_store_factory`, but uses a mongo store."""
-    # Use MongoDB if it's running (useful in CI), otherwise spin one up with pytest-server-fixtures.
-    # mongodb does not support prefix to differentiate different tests and the server is session-scoped
-    # therefore lib_name is needed to be used to avoid reusing library name or list_versions check will fail
-
-    if (
-            "--group" not in sys.argv or sys.argv[sys.argv.index("--group") + 1] == "1"
-    ):  # To detect pytest-split parallelization group in use
-        mongo_host = os.getenv("CI_MONGO_HOST", "localhost")
-        mongo_port = 27017
-    else:
-        mongo_host = os.getenv("CI_MONGO_HOST_2", "localhost")
-        mongo_port = 27017
-
-    mongo_path = f"{mongo_host}:{mongo_port}"
-    try:
-        res = requests.get(f"http://{mongo_path}")
-        have_running_mongo = res.status_code == 200 and "mongodb" in res.text.lower()
-    except requests.exceptions.ConnectionError:
-        have_running_mongo = False
-
-    if have_running_mongo:
-        uri = f"mongodb://{mongo_path}"
-    else:
-        mongo_server = request.getfixturevalue("mongo_server")
-        uri = f"mongodb://{mongo_server.hostname}:{mongo_server.port}"
-
-    yield uri
-
-    # mongodb does not support prefix to differentiate different tests and the server is session-scoped
-    mongo_client = pymongo.MongoClient(uri)
-    for db_name in mongo_client.list_database_names():
-        if db_name not in ["local", "admin", "apiomat", "config"]:
-            mongo_client.drop_database(db_name)
-
-
-@pytest.fixture
-def mongo_store_factory(request, lib_name, mongo_test_uri):
-    cfg_maker = functools.partial(create_test_mongo_cfg, uri=mongo_test_uri)
-    used = {}
-    try:
-        yield functools.partial(_version_store_factory_impl, used, cfg_maker, lib_name)
-    finally:
-        for lib in used.values():
-            lib.version_store.clear()
-
-
-@pytest.fixture
->>>>>>> 53b03983 (Batch read versions with snapshots)
 def s3_version_store_v1(s3_store_factory):
     return s3_store_factory(dynamic_strings=True)
 
@@ -888,57 +651,6 @@ def get_wide_df():
     return get_df
 
 
-<<<<<<< HEAD
-=======
-@pytest.fixture(scope="session")
-def azurite_port():
-    return _get_ephemeral_port()
-
-
-@pytest.fixture
-def azurite_container():
-    global bucket_id
-    container = f"testbucket{bucket_id}"
-    bucket_id = bucket_id + 1
-    return container
-
-
-@pytest.fixture(scope="session")
-def spawn_azurite(azurite_port):
-    temp_folder = tempfile.TemporaryDirectory()
-    try:  # Awaiting fix for cleanup in windows file-in-use problem
-        p = subprocess.Popen(
-            f"azurite --silent --blobPort {azurite_port} --blobHost 127.0.0.1 --queuePort 0 --tablePort 0",
-            cwd=temp_folder.name,
-            shell=True,
-        )
-        time.sleep(2)  # Wait for Azurite to start up
-        yield
-    finally:
-        print("Killing Azurite")
-        if sys.platform == "win32":  # different way of killing process on Windows
-            os.system(f"taskkill /F /PID {p.pid}")
-        else:
-            p.kill()
-        temp_folder = None  # For cleanup; For an unknown reason somehow using with syntax causes error
-
-
-@pytest.fixture(
-    scope="function",
-    params=(
-            [
-                "moto_s3_uri_incl_bucket",
-                pytest.param("azurite_azure_uri_incl_bucket", marks=AZURE_TESTS_MARK),
-                pytest.param("mongo_test_uri", marks=MONGO_TESTS_MARK),
-                pytest.param("unique_real_s3_uri", marks=REAL_S3_TESTS_MARK),
-            ]
-    ),
-)
-def object_storage_uri_incl_bucket(request):
-    yield request.getfixturevalue(request.param)
-
-
->>>>>>> 53b03983 (Batch read versions with snapshots)
 @pytest.fixture(
     scope="function",
     params=(
@@ -979,42 +691,6 @@ def object_and_mem_and_lmdb_version_store_dynamic_schema(request):
 
 
 @pytest.fixture
-<<<<<<< HEAD
-=======
-def in_memory_store_factory(lib_name):
-    cfg_maker = functools.partial(create_test_memory_cfg)
-    used = {}
-
-    def create_version_store(
-            col_per_group: Optional[int] = None,
-            row_per_segment: Optional[int] = None,
-            override_name: str = None,
-            **kwargs,
-    ) -> NativeVersionStore:
-        if col_per_group is not None and "column_group_size" not in kwargs:
-            kwargs["column_group_size"] = col_per_group
-        if row_per_segment is not None and "segment_row_size" not in kwargs:
-            kwargs["segment_row_size"] = row_per_segment
-
-        if "lmdb_config" in kwargs:
-            del kwargs["lmdb_config"]
-
-        if override_name is not None:
-            library_name = override_name
-        else:
-            library_name = lib_name
-
-        return _version_store_factory_impl(used, cfg_maker, library_name, **kwargs)
-
-    try:
-        yield create_version_store
-    finally:
-        for lib in used.values():
-            lib.version_store.clear()
-
-
-@pytest.fixture
->>>>>>> 53b03983 (Batch read versions with snapshots)
 def in_memory_version_store(in_memory_store_factory):
     return in_memory_store_factory()
 
