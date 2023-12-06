Fixed a lot of off-by-one errors

python/tests/unit/arcticdb/version_store/test_resample.py
@@ -1,24 +1,57 @@
 import numpy as np
 import pandas as pd
-import pytest
 
 from arcticdb import QueryBuilder
 from arcticdb.util.test import assert_frame_equal
 
-@pytest.mark.skip
+def generic_resample_test(lib, sym, rule, aggregations, date_range=None, closed=None, label=None):
+    read_df = lib.read(sym, date_range=date_range).data
+    pandas_resampled_df = read_df.resample(rule, closed=closed, label=label).agg(aggregations)
+
+    q = QueryBuilder()
+    q = q.resample(rule, closed=closed, label=label).agg(aggregations)
+    arcticdb_resampled_df = lib.read(sym, date_range=date_range, query_builder=q).data
+    print(f"Raw:\n{read_df}")
+    print(f"Pandas resampled:\n{pandas_resampled_df}")
+    print(f"ArcticDB resampled:\n{arcticdb_resampled_df}")
+    assert_frame_equal(pandas_resampled_df, arcticdb_resampled_df)
+
+
 def test_resampling(lmdb_version_store_v1):
     lib = lmdb_version_store_v1
     sym = "test_resampling"
-    index=pd.date_range("1970-01-01", periods=120, freq="S")
+    index=pd.date_range("2023-01-01", "2023-12-31T23:59:59", freq="S")
+    rng = np.random.default_rng()
     df = pd.DataFrame(
         {
-            "col_1": np.arange(len(index), dtype=np.uint64),
+            "ints": rng.integers(0, 100, len(index)),
+            "floats": rng.random(len(index)),
         },
         index=index,
     )
-    expected = df.resample("T").sum()
     lib.write(sym, df)
-    q = QueryBuilder()
-    q = q.resample("T").agg({"col_1": "sum",})
-    received = lib.read(sym, date_range=(index[0] + pd.Timedelta(30, unit="S"), index[len(index) - 1] - pd.Timedelta(30, unit="S")), query_builder=q).data
-    assert_frame_equal(expected, received)
+
+    print("MINUTE BARS")
+    generic_resample_test(lib, sym, "T", {"ints": "sum", "floats": "sum"})
+
+    print("15 MINUTE BARS")
+    generic_resample_test(lib, sym, "15T", {"ints": "sum", "floats": "sum"})
+
+    print("HOURLY BARS")
+    generic_resample_test(lib, sym, "H", {"ints": "sum", "floats": "sum"})
+
+    print("DATE RANGE SUBSET")
+    generic_resample_test(lib, sym, "T", {"ints": "sum", "floats": "sum"},
+                          date_range=(pd.Timestamp("2023-02-01"), pd.Timestamp("2023-02-28T23:59:59")))
+
+    print("CLOSED RIGHT BOUNDARIES")
+    generic_resample_test(lib, sym, "T", {"ints": "sum", "floats": "sum"},
+                          date_range=(pd.Timestamp("2023-02-01"), pd.Timestamp("2023-02-28T23:59:59")),
+                          closed="right")
+
+    print("LABELLED BY RIGHT BOUNDARIES")
+    generic_resample_test(lib, sym, "T", {"ints": "sum", "floats": "sum"},
+                          date_range=(pd.Timestamp("2023-02-01"), pd.Timestamp("2023-02-28T23:59:59")),
+                          closed="right",
+                          label="right")
+