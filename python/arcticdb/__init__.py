ac81ed546af3e2d0763c8bfaca82a119be85665f
Muhammad Hamza Sajjad <muhammadhamzasajjad@gmail.com>
2024-01-11 11:24:12 +0000

    Remove duplicate import and remove the version

--- python/arcticdb/__init__.py
+++ python/arcticdb/__init__.py
@@ -7,7 +7,6 @@ from arcticdb.options import LibraryOptions
 from arcticdb.version_store.processing import QueryBuilder
 from arcticdb.version_store._store import VersionedItem
 import arcticdb.version_store.library as library
-from arcticdb.options import LibraryOptions
 from arcticdb.tools import set_config_from_env_vars
 from arcticdb_ext.version_store import DataError, VersionRequestType
 from arcticdb_ext.exceptions import ErrorCode, ErrorCategory
@@ -16,4 +15,4 @@ from arcticdb.version_store.library import StagedDataFinalizeMethod, WriteMetada
 
 set_config_from_env_vars(_os.environ)
 
-__version__ = "1.4.1.dev0"
+__version__ = "dev"
