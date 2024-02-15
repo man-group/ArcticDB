e0ce1b4b53c3a53b6e368047b5a005b9a99f6c6c
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-02-15 18:19:35 +0100

    Fixed: function marked constexpr but definition is not available at call point

--- cpp/arcticdb/entity/types.hpp
+++ cpp/arcticdb/entity/types.hpp
@@ -236,7 +236,7 @@ enum class DataType : uint8_t {
     UNKNOWN = 0,
 };
 
-constexpr std::string_view datatype_to_str(DataType dt);
+std::string_view datatype_to_str(DataType dt);
 
 constexpr DataType combine_data_type(ValueType v, SizeBits b = SizeBits::UNKNOWN_SIZE_BITS) {
     return static_cast<DataType>(detail::combine_val_bits(v, b));
