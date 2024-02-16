e59e18a9bcdc1f41575cb311ccb1dda3140222fd
Julien Jerphanion <git@jjerphan.xyz>
2024-02-16 11:49:48 +0100

    Cosmit
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- cpp/arcticdb/entity/types.hpp
+++ cpp/arcticdb/entity/types.hpp
@@ -236,7 +236,6 @@ enum class DataType : uint8_t {
     UNKNOWN = 0,
 };
 
-
 std::string_view datatype_to_str(const DataType dt);
 
 constexpr DataType combine_data_type(ValueType v, SizeBits b = SizeBits::UNKNOWN_SIZE_BITS) {
