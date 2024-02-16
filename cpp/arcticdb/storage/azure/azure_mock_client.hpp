3b7199e3d680be8fa3118caf458265a3e08f9655
Muhammad Hamza Sajjad <45758804+muhammadhamzasajjad@users.noreply.github.com>
2024-02-16 11:22:39 +0000

    Add a `MockAzureClient` which can simulate azure failures (#1331)
    This is similar to #1281.
    - Adds a config option to use the `MockAzureClient` instead of the
    `RealAzureClient`
    - If a `MockAzureClient` is created, one can simulate failures by
    passing the appropriate symbol name
    - Creates tests to ensure that `MockAzureClient` works properly for
    `read`, `write`, `update`, `delete` and `list` functions.
    
    To do: Test exceptions thrown by `MockAzureClient`. This will be done in
    the next PR where we normalize Azure exception.
    
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>

---
+++ cpp/arcticdb/storage/azure/azure_mock_client.hpp
@@ -0,0 +1,62 @@
+/* Copyright 2023 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+ */
+
+#pragma once
+
+#include <azure/core/http/curl_transport.hpp>
+#include <azure/core/http/http_status_code.hpp>
+#include <azure/core.hpp>
+#include <azure/storage/blobs.hpp>
+
+#include <arcticdb/storage/storage_utils.hpp>
+#include <arcticdb/storage/azure/azure_client_wrapper.hpp>
+
+namespace arcticdb::storage::azure {
+
+enum class AzureOperation{
+    READ,
+    WRITE,
+    DELETE, // delete_blobs will fail for all objects if one of them triggers a delete failure
+    LIST,
+    EXISTS,
+};
+
+class MockAzureClient : public AzureClientWrapper {
+
+public:
+
+    void write_blob(
+            const std::string& blob_name,
+            Segment&& segment,
+            const Azure::Storage::Blobs::UploadBlockBlobFromOptions& upload_option,
+            unsigned int request_timeout) override;
+
+    Segment read_blob(
+            const std::string& blob_name,
+            const Azure::Storage::Blobs::DownloadBlobToOptions& download_option,
+            unsigned int request_timeout) override;
+
+    void delete_blobs(
+            const std::vector<std::string>& blob_names,
+            unsigned int request_timeout) override;
+
+    bool blob_exists(const std::string& blob_name) override;
+
+    Azure::Storage::Blobs::ListBlobsPagedResponse list_blobs(const std::string& prefix) override;
+
+    static std::string get_failure_trigger(
+            const std::string& blob_name,
+            AzureOperation operation_to_fail,
+            Azure::Core::Http::HttpStatusCode error_to_fail_with);
+
+private:
+    // Stores a mapping from blob_name to a Segment.
+    std::map<std::string, Segment> azure_contents;
+
+};
+
+}
---
+++ cpp/arcticdb/storage/test/common.hpp
@@ -0,0 +1,63 @@
+/* Copyright 2023 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+ */
+
+#pragma once
+
+#include <arcticdb/codec/codec.hpp>
+#include <arcticdb/entity/variant_key.hpp>
+#include <arcticdb/storage/storage.hpp>
+#include <arcticdb/stream/test/stream_test_common.hpp>
+
+namespace arcticdb::storage {
+
+inline VariantKey get_test_key(std::string name) {
+    auto builder = arcticdb::atom_key_builder();
+    return builder.build<arcticdb::entity::KeyType::TABLE_DATA>(name);
+}
+
+inline Segment get_test_segment() {
+    auto segment_in_memory = get_test_frame<arcticdb::stream::TimeseriesIndex>("symbol", {}, 10, 0).segment_;
+    auto codec_opts = proto::encoding::VariantCodec();
+    return encode_dispatch(std::move(segment_in_memory), codec_opts, arcticdb::EncodingVersion::V2);
+}
+
+inline void write_in_store(Storage &store, std::string symbol) {
+    auto variant_key = get_test_key(symbol);
+    store.write(KeySegmentPair(std::move(variant_key), get_test_segment()));
+}
+
+inline bool exists_in_store(Storage &store, std::string symbol) {
+    auto variant_key = get_test_key(symbol);
+    return store.key_exists(variant_key);
+}
+
+inline std::string read_in_store(Storage &store, std::string symbol) {
+    auto variant_key = get_test_key(symbol);
+    auto opts = ReadKeyOpts{};
+    auto result = store.read(std::move(variant_key), opts);
+    return std::get<StringId>(result.atom_key().id());
+}
+
+inline void remove_in_store(Storage &store, std::vector<std::string> symbols) {
+    auto to_remove = std::vector<VariantKey>();
+    for (auto &symbol: symbols) {
+        to_remove.emplace_back(get_test_key(symbol));
+    }
+    auto opts = RemoveOpts();
+    store.remove(Composite(std::move(to_remove)), opts);
+}
+
+inline std::set<std::string> list_in_store(Storage &store) {
+    auto keys = std::set<std::string>();
+    store.iterate_type(KeyType::TABLE_DATA, [&keys](VariantKey &&key) {
+        auto atom_key = std::get<AtomKey>(key);
+        keys.emplace(std::get<StringId>(atom_key.id()));
+    });
+    return keys;
+}
+
+}
