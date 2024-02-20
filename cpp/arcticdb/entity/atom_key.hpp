99b810a0bab6219c08cd92745007f2a2537287b4
Julien Jerphanion <git@jjerphan.xyz>
2024-02-20 11:34:49 +0100

    Minify changes
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- cpp/arcticdb/entity/atom_key.hpp
+++ cpp/arcticdb/entity/atom_key.hpp
@@ -27,7 +27,16 @@ class AtomKeyImpl {
         ContentHash content_hash,
         IndexValueType start_index,
         IndexValueType end_index,
-        KeyType key_type);
+        KeyType key_type)
+        :
+        id_(std::move(id)),
+        version_id_(version_id),
+        creation_ts_(creation_ts),
+        content_hash_(content_hash),
+        key_type_(key_type),
+        index_start_(std::move(start_index)),
+        index_end_(std::move(end_index)),
+        str_() { }
 
     AtomKeyImpl() = default;
     AtomKeyImpl(const AtomKeyImpl &other) = default;
@@ -35,42 +44,74 @@ class AtomKeyImpl {
     AtomKeyImpl(AtomKeyImpl &&other) = default;
     AtomKeyImpl &operator=(AtomKeyImpl &&other) = default;
 
-    const StreamId& id() const;
-    const VersionId& version_id() const;
-    const VersionId& gen_id() const;
-    const timestamp& creation_ts() const;
-    TimestampRange time_range() const;
-    timestamp start_time() const;
-    timestamp end_time() const;
-    const ContentHash& content_hash() const;
-    const KeyType& type() const;
-    KeyType& type();
-    const IndexValue &start_index() const;
-    const IndexValue &end_index() const;
-    IndexRange index_range() const;
+    const auto& id() const { return id_; }
+    const auto& version_id() const { return version_id_; }
+    const auto& gen_id() const { return version_id_; }
+    const auto& creation_ts() const { return creation_ts_; }
+    TimestampRange time_range() const { return {start_time(), end_time()}; }
+    timestamp start_time() const { if (std::holds_alternative<timestamp>(index_start_)) return std::get<timestamp>(index_start_); else return 0LL; }
+    timestamp end_time() const { if (std::holds_alternative<timestamp>(index_end_)) return std::get<timestamp>(index_end_); else return 0LL; }
+    const auto& content_hash() const { return content_hash_; }
+    const auto& type() const { return key_type_; }
+    auto& type() { return key_type_; }
+    const IndexValue &start_index() const { return index_start_; }
+    const IndexValue &end_index() const { return index_end_; }
+    IndexRange index_range() const { IndexRange ir = {index_start_, index_end_}; ir.end_closed_ = false; return ir;}
 
-    auto change_type(KeyType new_type);
+    auto change_type(KeyType new_type) {
+        key_type_ = new_type;
+        reset_cached();
+    }
 
     /**
      * Useful for caching/replacing the ID with an existing shared instance.
      * @param id Will be moved.
      * @return The old id moved out.
      */
-    StreamId change_id(StreamId id);
+    StreamId change_id(StreamId id) {
+        auto out = std::move(id_);
+        id_ = std::move(id);
+        reset_cached();
+        return out;
+    }
 
-    friend bool operator==(const AtomKeyImpl &l, const AtomKeyImpl &r);
+    friend bool operator==(const AtomKeyImpl &l, const AtomKeyImpl &r) {
+        return l.version_id() == r.version_id()
+            && l.creation_ts() == r.creation_ts()
+            && l.content_hash() == r.content_hash()
+            && l.start_index() == r.start_index()
+            && l.end_index() == r.end_index()
+            && l.type() == r.type()
+            && l.id() == r.id();
+    }
 
-    friend bool operator!=(const AtomKeyImpl &l, const AtomKeyImpl &r);
+    friend bool operator!=(const AtomKeyImpl &l, const AtomKeyImpl &r) {
+        return !(l == r);
+    }
 
-    friend bool operator<(const AtomKeyImpl &l, const AtomKeyImpl &r);
+    friend bool operator<(const AtomKeyImpl &l, const AtomKeyImpl &r) {
+        auto lt = std::tie(l.id_, l.version_id_, l.index_start_, l.index_end_, l.creation_ts_);
+        auto rt = std::tie(r.id_, r.version_id_, r.index_start_, r.index_end_, r.creation_ts_);
+        return lt < rt;
+    }
 
-    friend bool operator>(const AtomKeyImpl &l, const AtomKeyImpl &r);
+    friend bool operator>(const AtomKeyImpl &l, const AtomKeyImpl &r) {
+        return !(l < r) && (l != r);
+    }
 
-    size_t get_cached_hash() const;
+    size_t get_cached_hash() const {
+        if (!hash_) {
+            // arcticdb::commutative_hash_combine needs extra template specialisations for our variant types, folly's
+            // built-in variant forwards to std::hash which should be good enough for these simple types
+            hash_ = folly::hash::hash_combine(id_, version_id_, creation_ts_, content_hash_, key_type_, index_start_,
+                    index_end_);
+        }
+        return *hash_;
+    }
 
     void set_string() const;
 
-    std::string_view view() const;
+    std::string_view view() const { if(str_.empty()) set_string(); return {str_}; }
 
 private:
     StreamId id_;
@@ -83,7 +124,10 @@ private:
     mutable std::string str_; //TODO internalized string
     mutable std::optional<size_t> hash_;
 
-    void reset_cached();
+    void reset_cached() {
+        str_.clear();
+        hash_.reset();
+    }
 };
 
 /**
@@ -95,19 +139,40 @@ private:
  */
 class AtomKeyBuilder {
   public:
-    AtomKeyBuilder &version_id(VersionId v);
+    auto &version_id(VersionId v) {
+        version_id_ = v;
+        return *this;
+    }
 
-    AtomKeyBuilder &gen_id(VersionId v);
+    auto &gen_id(VersionId v) {
+        util::check_arg(version_id_ == 0, "Should not set both version_id and version id on a key");
+        version_id_ = v;
+        return *this;
+    }
 
-    AtomKeyBuilder &creation_ts(timestamp v);
+    auto &creation_ts(timestamp v) {
+        creation_ts_ = v;
+        return *this;
+    }
 
-    AtomKeyBuilder &string_index(const std::string &s);
+    auto &string_index(const std::string &s) {
+        index_start_ = s;
+        return *this;
+    }
+    auto &start_index(const IndexValue &iv) {
+        index_start_ = iv;
+        return *this;
+    }
 
-    AtomKeyBuilder &start_index(const IndexValue &iv);
+    auto &end_index(const IndexValue &iv) {
+        index_end_ = iv;
+        return *this;
+    }
 
-    AtomKeyBuilder &end_index(const IndexValue &iv);
-
-    AtomKeyBuilder &content_hash(ContentHash v);
+    auto &content_hash(ContentHash v) {
+        content_hash_ = v;
+        return *this;
+    }
 
     template<KeyType KT>
     AtomKeyImpl build(StreamId id) {
@@ -116,7 +181,11 @@ class AtomKeyBuilder {
         };
     }
 
-    AtomKeyImpl build(StreamId id, KeyType key_type);
+    AtomKeyImpl build(StreamId id, KeyType key_type) {
+        return {
+            std::move(id), version_id_, creation_ts_, content_hash_, index_start_, index_end_, key_type
+        };
+    }
 
   private:
     VersionId version_id_ = 0;
@@ -134,7 +203,7 @@ using AtomKey = AtomKeyImpl;
  */
 using IndexTypeKey = AtomKey;
 
-inline AtomKeyBuilder atom_key_builder() {
+inline auto atom_key_builder() {
     return AtomKeyBuilder{};
 }
 
@@ -149,36 +218,35 @@ inline AtomKey null_key() {
 // format. Transformation of keys for persistence is handled elsewhere.
 namespace fmt {
 
+using namespace arcticdb::entity;
+
 template<class FormatTag>
-struct formatter<arcticdb::entity::FormattableRef< arcticdb::entity::AtomKeyImpl, FormatTag>> {
+struct formatter<FormattableRef < AtomKey, FormatTag>> {
 template<typename ParseContext>
 constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
 
 template<typename FormatContext>
-auto format(const arcticdb::entity::FormattableRef<arcticdb::entity::AtomKeyImpl, FormatTag> &f,
-            FormatContext &ctx) const {
+auto format(const FormattableRef <arcticdb::entity::AtomKey, FormatTag> &f, FormatContext &ctx) const {
     const auto &key = f.ref;
     return fmt::format_to(ctx.out(), FMT_STRING(FormatTag::format),
-                          key.type(), key.id(), key.version_id(),
-                          key.content_hash(), key.creation_ts(), tokenized_index(key.start_index()),
-                          tokenized_index(key.end_index()));
+                    key.type(), key.id(), key.version_id(),
+                     key.content_hash(), key.creation_ts(), tokenized_index(key.start_index()), tokenized_index(key.end_index()));
 }
+
 };
 
 template<>
-struct formatter<arcticdb::entity::AtomKeyImpl> {
+struct formatter<AtomKey> {
     template<typename ParseContext>
-    constexpr auto parse(ParseContext& ctx) { return ctx.begin(); }
+    constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
 
     template<typename FormatContext>
-    auto format(const arcticdb::entity::AtomKeyImpl& value, FormatContext& ctx) const {
-        using RefType = arcticdb::entity::FormattableRef< arcticdb::entity::AtomKeyImpl >;
-        formatter<RefType> f;
-        return f.format(RefType{ value }, ctx);
+    auto format(const arcticdb::entity::AtomKey &key, FormatContext &ctx) const {
+        formatter<FormattableRef < arcticdb::entity::AtomKey, DefaultAtomKeyFormat>>
+        f;
+        return f.format(formattable(key), ctx);
     }
-
 };
-
 }
 
 namespace std {
@@ -188,4 +256,12 @@ struct hash<arcticdb::entity::AtomKeyImpl> {
         return k.get_cached_hash();
     }
 };
+}
+
+namespace arcticdb::entity
+{
+    // This needs to be defined AFTER the formatter for AtomKeyImpl
+    inline void AtomKeyImpl::set_string() const {
+        str_ = fmt::format("{}", *this);
+    }
 }
--- cpp/arcticdb/entity/ref_key.hpp
+++ cpp/arcticdb/entity/ref_key.hpp
@@ -7,15 +7,25 @@
 
 #pragma once
 
-#include <fmt/format.h>
 #include <arcticdb/entity/types.hpp>
+#include <fmt/format.h>
 #include <arcticdb/entity/key.hpp>
 
 namespace arcticdb::entity {
     class RefKey {
     public:
 
-        RefKey(StreamId id, KeyType key_type, bool old_type = false);
+        RefKey(
+                StreamId id,
+                KeyType key_type,
+                bool old_type = false)
+                :
+                id_(std::move(id)),
+                key_type_(key_type),
+                old_type_(old_type) {
+            util::check(!std::holds_alternative<StringId>(id_) || !std::get<StringId>(id_).empty(), "Empty symbol in reference key");
+            util::check(old_type || is_ref_key_class(key_type), "Can't create ref key with non-ref key class keytype {}", key_type);
+        }
 
         RefKey() = default;
         RefKey(const RefKey &other) = default;
@@ -23,20 +33,29 @@ namespace arcticdb::entity {
         RefKey(RefKey &&other) = default;
         RefKey &operator=(RefKey &&other) = default;
 
-        const StreamId& id() const;
-        const KeyType& type() const;
-        KeyType& type();
-        bool is_old_type() const;
-        void change_type(KeyType new_type);
+        const StreamId& id() const { return id_; }
+        const auto& type() const { return key_type_; }
+        auto& type() { return key_type_; }
+        auto is_old_type() const { return old_type_; }
+        void change_type(KeyType new_type) {
+            key_type_ = new_type;
+        }
 
-        friend bool operator==(const RefKey &l, const RefKey &r);
+        friend bool operator==(const RefKey &l, const RefKey &r) {
+            return l.type() == r.type()
+                   && l.id() == r.id();
+        }
 
-        friend bool operator!=(const RefKey &l, const RefKey &r);
+        friend bool operator!=(const RefKey &l, const RefKey &r) {
+            return !(l == r);
+        }
 
         //TODO Neither key sorts by type
-        friend bool operator<(const RefKey &l, const RefKey &r);
+        friend bool operator<(const RefKey &l, const RefKey &r) {
+            return l.id() < r.id();
+        }
 
-        std::string_view view() const;
+        std::string_view view() const { if(str_.empty()) set_string(); return std::string_view{str_}; }
 
         void set_string() const;
     private:
@@ -49,17 +68,20 @@ namespace arcticdb::entity {
     };
 } // namespace arcticdb::entity
 
+
+namespace fmt {
 template<>
-struct fmt::formatter<arcticdb::entity::RefKey>
-{
+struct formatter<RefKey> {
     template<typename ParseContext>
-    constexpr auto parse(ParseContext& ctx) { return ctx.begin(); }
+    constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
 
     template<typename FormatContext>
-    auto format(const arcticdb::entity::RefKey& number, FormatContext& ctx) {
-        return fmt::format_to(ctx.out(), "{}:{}", number.type(), number.id());
+    auto format(const RefKey &k, FormatContext &ctx) const {
+        return fmt::format_to(ctx.out(), "{}:{}", k.type(), k.id());
     }
 };
+
+} //namespace fmt
 
 //TODO this is operating on the pretty-printed version and is needlessly inefficient
 namespace std {
@@ -70,4 +92,12 @@ struct hash<arcticdb::entity::RefKey> {
         return arcticdb::hash(const_cast<uint8_t * >(reinterpret_cast<const uint8_t *>(view.data())), view.size());
     }
 };
+}
+
+namespace arcticdb::entity
+{
+    // Note: this needs to be defined after formatters.
+    inline void RefKey::set_string() const {
+        str_ = fmt::format("{}", *this);
+    }
 }
