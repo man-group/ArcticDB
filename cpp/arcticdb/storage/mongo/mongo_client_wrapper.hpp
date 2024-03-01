4ddccbb07ac18ecb12e29e04d4fe45dfea140f61
Muhammad Hamza Sajjad <45758804+muhammadhamzasajjad@users.noreply.github.com>
2024-03-01 12:19:55 +0000

    Refactor: Move mongo client errors handling into mongo_storage.cpp (#1383)
    This is a refactor and it does not change the existing behaviour.
    Currently error handling is done in `mongo_client.cpp`. We want to avoid
    that because later a mock client shall be created which will be used
    instead of `MongoClient` to simulate mongo errors. For this reason the
    errors should be handled in `mongo_storage.cpp`. Error simulation via a
    mock client is needed to perform exception normlization similar to other
    storages such as S3 and Azure.
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>
    
    <!--
    Thanks for contributing a Pull Request to ArcticDB! Please ensure you
    have taken a look at:
    - ArcticDB's Code of Conduct:
    https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
    - ArcticDB's Contribution Licensing:
    https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
    -->

---
+++ cpp/arcticdb/storage/mongo/mongo_client_wrapper.hpp
@@ -0,0 +1,67 @@
+/* Copyright 2023 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+ */
+
+#pragma once
+#include <fmt/format.h>
+
+#include <arcticdb/storage/storage.hpp>
+#include <arcticdb/entity/protobufs.hpp>
+
+namespace arcticdb::storage::mongo {
+
+// modified_count set to null_opt signals update failed (mongo docs are unclear on what error causes this)
+struct UpdateResult { std::optional<int> modified_count; };
+
+// delete_count set to null_opt signals delete failed (mongo docs are unclear on what error causes this)
+struct DeleteResult { std::optional<int> delete_count; };
+
+class MongoClientWrapper {
+public:
+    virtual ~MongoClientWrapper() = default;
+
+    virtual bool write_segment(
+            const std::string &database_name,
+            const std::string &collection_name,
+            storage::KeySegmentPair&& kv) = 0;
+
+    virtual UpdateResult update_segment(
+            const std::string &database_name,
+            const std::string &collection_name,
+            storage::KeySegmentPair&& kv,
+            bool upsert) = 0;
+
+    virtual std::optional<KeySegmentPair> read_segment(
+            const std::string &database_name,
+            const std::string &collection_name,
+            const entity::VariantKey &key) = 0;
+
+    virtual DeleteResult remove_keyvalue(
+            const std::string &database_name,
+            const std::string &collection_name,
+            const entity::VariantKey &key) = 0;
+
+    virtual std::vector<VariantKey> list_keys(
+            const std::string &database_name,
+            const std::string &collection_name,
+            KeyType key_type,
+            const std::optional<std::string> &prefix) = 0;
+
+    virtual void ensure_collection(
+            std::string_view database_name,
+            std::string_view collection_name) = 0;
+
+    virtual void drop_collection(
+            std::string database_name,
+            std::string collection_name) = 0;
+
+    virtual bool key_exists(
+            const std::string &database_name,
+            const std::string &collection_name,
+            const  entity::VariantKey &key) = 0;
+};
+
+}
