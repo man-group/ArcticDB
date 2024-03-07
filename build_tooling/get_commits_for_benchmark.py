2ea640bf069a15f845907772c11b18e554f714c1
Georgi Petrov <32372905+G-D-Petrov@users.noreply.github.com>
2024-03-07 09:29:45 +0200

    Test benchmarking improvements (#1326)
    #### Reference Issues/PRs
    Fixes #1311 
    
    #### What does this implement or fix?
    TLDR: This PR aims to address some of the major bottlenecks that we have
    addressed when using ASV, namely:
    - reduce the time that it takes to run/regenerate all benchmarks -
    currently 17+ hours vs ~2 hours after this PR;
    - this is done by parallelizing the benchmarking of each commit on a
    separate GH **Free** Runner
    - eliminate the need to push the results to git - ASV tracks the results
    of each benchmark as a json file, but this results in many unneeded
    commits and potential clashes
    - this is fixed by transforming the jsons to pandas DFs and storing them
    in an ArcticDB database in AWS S3
      
      
    More detailed explanation of the various changes:
    1. Removed the [ASV results jsons from
    git](https://github.com/man-group/ArcticDB/pull/1326/files#diff-5fe96e5ac9998c3baa62242a849c12213c3a1519e2d6b28338e6f4cd8e61b78a),
    only the
    [benchmarks.json](https://github.com/man-group/ArcticDB/pull/1326/files#diff-789c26163b19ea0ebd9d1b4f211c2d2e31f166a8c848f1e762f28dbbc75737d8)
    has been kept as it is needed to generate the HTML and it is a bit hard
    to generate on-the-fly
    2.
    [build_tooling/transform_asv_results.py](https://github.com/man-group/ArcticDB/pull/1326/files#diff-e6cfb2b0545257e63d5b4d5b5d53af13b51a467c9d7b2ce2069c413955a834fa)
    file has been added which is responsible for the transformation between
    ASV jsons and DF and vice verse
    3.
    [build_tooling/get_commits_for_benchmark.py](https://github.com/man-group/ArcticDB/pull/1326/files#diff-1100dd99a115c31a48b4ea2cc167549ec0d9da872508dae86f665346c87f8f3a)
    file has been added which is encapsulates the logic for determining
    which commits should be used for benchmarking, currently supports 2
    modes:
    - run_all_benchmars - which select all commits from which we have
    created new versions (after version 2.0.0), this is mainly for
    regenerating the benchmarks
    - default - which just select the commit hash for the current HEAD, this
    is the incremental benchmark
    4. [ASV
    config](https://github.com/man-group/ArcticDB/pull/1326/files#diff-d18ceac2def9772dfdf24d7a83cbe688bf5717e334108eff2aeb581d9992d531)
    has been changed to use `conda `as env manager instead of `virtualenv `-
    this is because the free runners are not working well with `virtualenv`
    - I've kept the old config as
    [asv.conf_old.json](https://github.com/man-group/ArcticDB/pull/1326/files#diff-6052c98ef491566f9d9edea33859de3003f919e5e716c412797da56146401491)
    because that one is easier to use when testing locally
    5. I've made some changes to the benchmarks themselves to make them run
    a bit faster and more consistent

---
+++ build_tooling/get_commits_for_benchmark.py
@@ -0,0 +1,65 @@
+"""
+Copyright 2024 Man Group Operations Limited
+
+Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+
+As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+"""
+import os
+import re
+import subprocess
+from argparse import ArgumentParser
+
+
+def get_git_tags():
+    result = subprocess.run(["git", "tag", "--list"], capture_output=True, text=True)
+
+    # Filter the tags using a regular expression
+    pattern = r"^v[0-9]+\.[0-9]+\.[0-9]+$"
+    tags = [tag for tag in result.stdout.splitlines() if re.match(pattern, tag)]
+    # We are only interested in tags with version 3.0.0 or higher
+    # Because there are strange bugs with the lower versions
+    filtered_tags = [
+        tag for tag in tags if int(tag.split(".")[0].replace("v", "")) >= 3
+    ]
+    return filtered_tags
+
+
+def get_commit_from_tag(tag):
+    result = subprocess.run(
+        ["git", "rev-list", "-n", "1", tag], capture_output=True, text=True
+    )
+    return result.stdout.strip()
+
+
+parser = ArgumentParser(
+    description="Add a list of commits that need to be benchmarked to the GitHub output"
+)
+parser.add_argument(
+    "--run_all_benchmarks",
+    help="Should we get the tags and commits for all benchmarks, or just the current one",
+    action="store_true",
+)
+
+args = parser.parse_args()
+
+if args.run_all_benchmarks:
+    tags = get_git_tags()
+    commits = [get_commit_from_tag(tag) for tag in tags]
+else:
+    # Get the current git commit hash
+    tags = ["HEAD"]
+    result = subprocess.run(
+        ["git", "rev-parse", "HEAD"], capture_output=True, text=True
+    )
+    commits = [result.stdout.strip()]
+
+short_commits = [commit[:8] for commit in commits]
+
+print(
+    "Git tags and their corresponding commits: " + str(list(zip(tags, short_commits)))
+)
+
+if "GITHUB_OUTPUT" in os.environ:
+    with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+        print(f"commits={str(short_commits)}", file=fh)
