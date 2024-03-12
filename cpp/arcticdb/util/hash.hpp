8968cc9c396d356107e208e02aca58f9b107be20
Alex Owens <alexowens90@gmail.com>
2024-03-12 16:48:19 +0000

    Use std::hash

--- cpp/arcticdb/util/hash.hpp
+++ cpp/arcticdb/util/hash.hpp
@@ -16,57 +16,7 @@
 #include <xxhash.h>
 #undef XXH_STATIC_LINKING_ONLY
 
-//#include <highwayhash/highwayhash.h>
-
 namespace arcticdb {
-
-using HashedValue = XXH64_hash_t;
-constexpr std::size_t DEFAULT_SEED = 0x42;
-
-// xxhash
-//template<class T, std::size_t seed = DEFAULT_SEED>
-//HashedValue hash(T *d, std::size_t count) {
-//    return XXH64(reinterpret_cast<const void *>(d), count * sizeof(T), seed);
-//}
-//
-//// size argument to XXH64 being compile-time constant improves performance
-//template<class T, std::size_t seed = DEFAULT_SEED>
-//HashedValue hash(T *d) {
-//    return XXH64(reinterpret_cast<const void *>(d), sizeof(T), seed);
-//}
-//
-//inline HashedValue hash(std::string_view sv) {
-//    return hash(sv.data(), sv.size());
-//}
-
-// Highwayhash
-//using namespace highwayhash;
-//HH_ALIGNAS(32) const HHKey key = {1, 2, 3, 4};
-
-//template<class T>
-//inline HHResult64 hash(T *d, std::size_t count) {
-//    HHResult64 result;
-//    HHStateT<HH_TARGET> state(key);
-//    HighwayHashT(&state, reinterpret_cast<const char*>(d), count * sizeof(T), &result);
-//    return result;
-//}
-//
-//template<class T>
-//inline HHResult64 hash(T *d) {
-//    HHResult64 result;
-//    HHStateT<HH_TARGET> state(key);
-//    HighwayHashT(&state, reinterpret_cast<const char*>(d), sizeof(T), &result);
-//    return result;
-//}
-//
-//inline HHResult64 hash(std::string_view sv) {
-//    HHResult64 result;
-//    HHStateT<HH_TARGET> state(key);
-//    HighwayHashT(&state, reinterpret_cast<const char*>(sv.data()), sv.size(), &result);
-//    return result;
-//}
-
-// std::hash
 
 template<class T>
 inline size_t hash(T *d) {
@@ -76,6 +26,9 @@ inline size_t hash(T *d) {
 inline size_t hash(std::string_view sv) {
     return std::hash<std::string_view>{}(sv);
 }
+
+using HashedValue = XXH64_hash_t;
+constexpr std::size_t DEFAULT_SEED = 0x42;
 
 class HashAccum {
   public:
