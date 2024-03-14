671caf0ac09335d6f26b388810e69cee3e30f6d8
Alex Owens <alexowens90@gmail.com>
2024-03-14 16:09:57 +0000

    Change existing test

--- python/tests/unit/arcticdb/version_store/test_column_type_changes.py
+++ python/tests/unit/arcticdb/version_store/test_column_type_changes.py
@@ -21,9 +21,9 @@ def test_changing_numeric_type(version_store_factory, dynamic_schema):
     lib = version_store_factory(dynamic_schema=dynamic_schema)
     sym_append = "test_changing_numeric_type_append"
     sym_update = "test_changing_numeric_type_update"
-    df_write = pd.DataFrame({"col": np.arange(3, dtype=np.uint8)}, index=pd.date_range("2024-01-01", periods=3))
-    df_append = pd.DataFrame({"col": np.arange(1, dtype=np.uint16)}, index=pd.date_range("2024-01-04", periods=1))
-    df_update = pd.DataFrame({"col": np.arange(1, dtype=np.uint16)}, index=pd.date_range("2024-01-02", periods=1))
+    df_write = pd.DataFrame({"col": np.arange(3, dtype=np.uint32)}, index=pd.date_range("2024-01-01", periods=3))
+    df_append = pd.DataFrame({"col": np.arange(1, dtype=np.int32)}, index=pd.date_range("2024-01-04", periods=1))
+    df_update = pd.DataFrame({"col": np.arange(1, dtype=np.int32)}, index=pd.date_range("2024-01-02", periods=1))
 
     lib.write(sym_append, df_write)
     lib.write(sym_update, df_write)
@@ -41,50 +41,9 @@ def test_changing_numeric_type(version_store_factory, dynamic_schema):
         received_append = lib.read(sym_append).data
         assert_frame_equal(expected_append, received_append)
 
-        expected_update = pd.DataFrame({"col": np.array([0, 0, 2], dtype=np.uint16)}, index=pd.date_range("2024-01-01", periods=3))
+        expected_update = pd.DataFrame({"col": np.array([0, 0, 2], dtype=np.int64)}, index=pd.date_range("2024-01-01", periods=3))
         received_update = lib.read(sym_update).data
         assert_frame_equal(expected_update, received_update)
-
-
-# TODO: Test encoding v2 as well
-# TODO: test append, update, and all combinations of numeric types
-def test_changing_numeric_type_2(lmdb_version_store_dynamic_schema_v1):
-    lib = lmdb_version_store_dynamic_schema_v1
-    sym_append = "test_changing_numeric_type_append"
-    df_write = pd.DataFrame({"col": np.arange(3, dtype=np.uint8)}, index=pd.date_range("2024-01-01", periods=3))
-    df_append = pd.DataFrame({"col": np.arange(1, dtype=np.int8)}, index=pd.date_range("2024-01-04", periods=1))
-
-    lib.write(sym_append, df_write)
-    lib.append(sym_append, df_append)
-
-    expected_append = pd.concat([df_write, df_append])
-    received_append = lib.read(sym_append).data
-    assert_frame_equal(expected_append, received_append)
-
-
-@pytest.mark.parametrize("dynamic_schema", [True, False])
-@pytest.mark.parametrize("dynamic_strings_first", [True, False])
-def test_changing_string_type(version_store_factory, dynamic_schema, dynamic_strings_first):
-    lib = version_store_factory(dynamic_strings=True, dynamic_schema=dynamic_schema)
-    sym_append = "test_changing_string_type_append"
-    sym_update = "test_changing_string_type_update"
-    df_write = pd.DataFrame({"col": ["a", "bb", "ccc"]}, index=pd.date_range("2024-01-01", periods=3))
-    df_append = pd.DataFrame({"col": ["dddd"]}, index=pd.date_range("2024-01-04", periods=1))
-    df_update = pd.DataFrame({"col": ["dddd"]}, index=pd.date_range("2024-01-02", periods=1))
-
-    lib.write(sym_append, df_write, dynamic_strings=dynamic_strings_first)
-    lib.write(sym_update, df_write, dynamic_strings=dynamic_strings_first)
-
-    lib.append(sym_append, df_append, dynamic_strings=not dynamic_strings_first)
-    lib.update(sym_update, df_update, dynamic_strings=not dynamic_strings_first)
-
-    expected_append = pd.concat([df_write, df_append])
-    received_append = lib.read(sym_append).data
-    assert_frame_equal(expected_append, received_append)
-
-    expected_update = pd.DataFrame({"col": ["a", "dddd", "ccc"]}, index=pd.date_range("2024-01-01", periods=3))
-    received_update = lib.read(sym_update).data
-    assert_frame_equal(expected_update, received_update)
 
 
 @pytest.mark.parametrize("dynamic_schema", [True, False])
