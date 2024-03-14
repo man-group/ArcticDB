65d6b33cc784c3c5d59335a02d32adc21e38f029
Alex Owens <alexowens90@gmail.com>
2024-03-14 16:58:26 +0000

    Reinstate accidentally deleted test

--- python/tests/unit/arcticdb/version_store/test_column_type_changes.py
+++ python/tests/unit/arcticdb/version_store/test_column_type_changes.py
@@ -47,6 +47,31 @@ def test_changing_numeric_type(version_store_factory, dynamic_schema):
 
 
 @pytest.mark.parametrize("dynamic_schema", [True, False])
+@pytest.mark.parametrize("dynamic_strings_first", [True, False])
+def test_changing_string_type(version_store_factory, dynamic_schema, dynamic_strings_first):
+    lib = version_store_factory(dynamic_strings=True, dynamic_schema=dynamic_schema)
+    sym_append = "test_changing_string_type_append"
+    sym_update = "test_changing_string_type_update"
+    df_write = pd.DataFrame({"col": ["a", "bb", "ccc"]}, index=pd.date_range("2024-01-01", periods=3))
+    df_append = pd.DataFrame({"col": ["dddd"]}, index=pd.date_range("2024-01-04", periods=1))
+    df_update = pd.DataFrame({"col": ["dddd"]}, index=pd.date_range("2024-01-02", periods=1))
+
+    lib.write(sym_append, df_write, dynamic_strings=dynamic_strings_first)
+    lib.write(sym_update, df_write, dynamic_strings=dynamic_strings_first)
+
+    lib.append(sym_append, df_append, dynamic_strings=not dynamic_strings_first)
+    lib.update(sym_update, df_update, dynamic_strings=not dynamic_strings_first)
+
+    expected_append = pd.concat([df_write, df_append])
+    received_append = lib.read(sym_append).data
+    assert_frame_equal(expected_append, received_append)
+
+    expected_update = pd.DataFrame({"col": ["a", "dddd", "ccc"]}, index=pd.date_range("2024-01-01", periods=3))
+    received_update = lib.read(sym_update).data
+    assert_frame_equal(expected_update, received_update)
+
+
+@pytest.mark.parametrize("dynamic_schema", [True, False])
 @pytest.mark.parametrize("wider_strings_first", [True, False])
 def test_changing_fixed_string_width(version_store_factory, dynamic_schema, wider_strings_first):
     lib = version_store_factory(dynamic_strings=False, dynamic_schema=dynamic_schema)
