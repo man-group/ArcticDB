d05cfb1ce2715b7a84a4fc3b27ce90440929f06f
James Munro <jamesjulianmunro@gmail.com>
2024-03-17 22:37:11 +0000

    add _msgpack_compact tests, add test for metadata warn size

---
+++ python/tests/unit/arcticdb/test_msgpack_compact.py
@@ -0,0 +1,50 @@
+import pytest
+import msgpack
+from arcticdb._msgpack_compat import packb, padded_packb, unpackb
+
+
+def test_packb():
+    assert packb(None) == msgpack.packb(None, use_bin_type=True, strict_types=True)
+    assert packb("a") == msgpack.packb("a", use_bin_type=True, strict_types=True)
+    assert packb(b"12345") == msgpack.packb(b"12345", use_bin_type=True, strict_types=True)
+    assert packb(1) == msgpack.packb(1, use_bin_type=True, strict_types=True)
+    assert packb([1, 2, 3, 4]) == msgpack.packb([1, 2, 3, 4], use_bin_type=True, strict_types=True)
+
+
+def test_packb_raises_on_tuple():
+    with pytest.raises(TypeError):
+        packb((1, 2))
+
+
+def test_padded_packb_small():
+    packed, nbytes = padded_packb(1)
+    assert len(packed) % 8 == 0
+    assert len(packed) > nbytes
+    assert len(packed) - nbytes < 8
+    assert packed[:nbytes] == msgpack.packb(1, use_bin_type=True, strict_types=True)
+
+
+def test_padded_packb_list():
+    ints = list(range(10_000_000))
+    packed, nbytes = padded_packb(ints)
+    assert len(packed) % 8 == 0
+    assert len(packed) >= nbytes
+    assert len(packed) - nbytes < 8
+    assert packed[:nbytes] == msgpack.packb(ints, use_bin_type=True, strict_types=True)
+
+
+def test_padded_packb_string():
+    aas = 'A' * 1_000_005 # not dividisble by 8
+    packed, nbytes = padded_packb(aas)
+    assert len(packed) % 8 == 0
+    assert len(packed) >= nbytes
+    assert len(packed) - nbytes < 8
+    assert packed[:nbytes] == msgpack.packb(aas, use_bin_type=True, strict_types=True)
+
+
+def test_unpackb():
+    # serializes without strict_types
+    packed = msgpack.packb({(1, 2): "a"})
+    with pytest.raises(TypeError):
+        assert unpackb(packed)
+
