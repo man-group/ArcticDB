12d767123fea2f605c5adc57ffe80d40c6d11449
Julien Jerphanion <git@jjerphan.xyz>
2024-03-20 17:24:39 +0100

    maint: Replace usage of folly in `clock.hpp`
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- cpp/arcticdb/util/clock.hpp
+++ cpp/arcticdb/util/clock.hpp
@@ -9,29 +9,20 @@
 
 #include <arcticdb/entity/types.hpp>
 
-#include <folly/ClockGettimeWrappers.h>
-#include <folly/portability/Time.h>
-#ifndef CLOCK_REALTIME_COARSE
 #include <chrono>
-#endif
 
 namespace arcticdb::util {
 
 class SysClock {
   public:
     static entity::timestamp nanos_since_epoch() {
-        return (*folly::chrono::clock_gettime_ns)(CLOCK_REALTIME);
+        auto t = std::chrono::system_clock::now();
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(t.time_since_epoch()).count();
     }
-#if defined(_WIN32) || defined(__APPLE__)
     static entity::timestamp coarse_nanos_since_epoch() {
         auto t = std::chrono::system_clock::now();
         return std::chrono::duration_cast<std::chrono::nanoseconds>(t.time_since_epoch()).count();
     }
-#else
-    static entity::timestamp coarse_nanos_since_epoch() {
-        return (*folly::chrono::clock_gettime_ns)(CLOCK_REALTIME_COARSE);
-    }
-#endif
 };
 
 
