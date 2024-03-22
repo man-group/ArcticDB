a2a1af26c857af6d248b4c7329999ccb5affaa13
phoebusm <phoebusmak@gmail.com>
2024-03-22 17:48:06 +0000

    Exclude libssl

--- cpp/third_party/vcpkg_overlays/triplets/x64-linux-mix.cmake
+++ cpp/third_party/vcpkg_overlays/triplets/x64-linux-mix.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-if(${PORT} MATCHES "curl")
+if(${PORT} MATCHES "openssl")
 	set(VCPKG_LIBRARY_LINKAGE dynamic)
 else()
     set(VCPKG_LIBRARY_LINKAGE static)
