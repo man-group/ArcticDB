570d9eec1f219b3035d91bc61bdd5a70e59bb8c0
Muhammad Hamza Sajjad <45758804+muhammadhamzasajjad@users.noreply.github.com>
2024-04-19 17:08:09 +0100

    bugfix/1336: on s3 and azure add delimiter in iterate_type when prefix is empty to avoid KeyType::LOG_COMPACTED when KeyType::LOG is queried (#1511)
    #### Reference Issues/PRs
    Fixes #1336 
    <!--Example: Fixes #1234. See also #3456.-->
    
    #### What does this implement or fix?
    
    #### Any other comments?
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>
    
    <!--
    Thanks for contributing a Pull Request to ArcticDB! Please ensure you
    have taken a look at:
    - ArcticDB's Code of Conduct:
    https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
    - ArcticDB's Contribution Licensing:
    https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
    -->

--- cpp/arcticdb/storage/test/common.hpp
+++ cpp/arcticdb/storage/test/common.hpp
@@ -14,9 +14,9 @@
 
 namespace arcticdb::storage {
 
-inline VariantKey get_test_key(std::string name) {
+inline VariantKey get_test_key(std::string name, entity::KeyType key_type = entity::KeyType::TABLE_DATA) {
     auto builder = arcticdb::atom_key_builder();
-    return builder.build<arcticdb::entity::KeyType::TABLE_DATA>(name);
+    return builder.build(name, key_type);
 }
 
 inline Segment get_test_segment() {
@@ -25,44 +25,55 @@ inline Segment get_test_segment() {
     return encode_dispatch(std::move(segment_in_memory), codec_opts, arcticdb::EncodingVersion::V2);
 }
 
-inline void write_in_store(Storage &store, std::string symbol) {
-    auto variant_key = get_test_key(symbol);
+inline void write_in_store(Storage &store, std::string symbol, entity::KeyType key_type = entity::KeyType::TABLE_DATA) {
+    auto variant_key = get_test_key(symbol, key_type);
     store.write(KeySegmentPair(std::move(variant_key), get_test_segment()));
 }
 
-inline void update_in_store(Storage &store, std::string symbol) {
-    auto variant_key = get_test_key(symbol);
+inline void update_in_store(Storage &store, std::string symbol, entity::KeyType key_type = entity::KeyType::TABLE_DATA) {
+    auto variant_key = get_test_key(symbol, key_type);
     store.update(KeySegmentPair(std::move(variant_key), get_test_segment()), arcticdb::storage::UpdateOpts{});
 }
 
-inline bool exists_in_store(Storage &store, std::string symbol) {
-    auto variant_key = get_test_key(symbol);
+inline bool exists_in_store(Storage &store, std::string symbol, entity::KeyType key_type = entity::KeyType::TABLE_DATA) {
+    auto variant_key = get_test_key(symbol, key_type);
     return store.key_exists(variant_key);
 }
 
-inline std::string read_in_store(Storage &store, std::string symbol) {
-    auto variant_key = get_test_key(symbol);
+inline std::string read_in_store(Storage &store, std::string symbol, entity::KeyType key_type = entity::KeyType::TABLE_DATA) {
+    auto variant_key = get_test_key(symbol, key_type);
     auto opts = ReadKeyOpts{};
     auto result = store.read(std::move(variant_key), opts);
     return std::get<StringId>(result.atom_key().id());
 }
 
-inline void remove_in_store(Storage &store, std::vector<std::string> symbols) {
+inline void remove_in_store(Storage &store, std::vector<std::string> symbols, entity::KeyType key_type = entity::KeyType::TABLE_DATA) {
     auto to_remove = std::vector<VariantKey>();
     for (auto &symbol: symbols) {
-        to_remove.emplace_back(get_test_key(symbol));
+        to_remove.emplace_back(get_test_key(symbol, key_type));
     }
     auto opts = RemoveOpts();
     store.remove(Composite(std::move(to_remove)), opts);
 }
 
-inline std::set<std::string> list_in_store(Storage &store) {
+inline std::set<std::string> list_in_store(Storage &store, entity::KeyType key_type = entity::KeyType::TABLE_DATA) {
     auto keys = std::set<std::string>();
-    store.iterate_type(KeyType::TABLE_DATA, [&keys](VariantKey &&key) {
+    store.iterate_type(key_type, [&keys](VariantKey &&key) {
         auto atom_key = std::get<AtomKey>(key);
         keys.emplace(std::get<StringId>(atom_key.id()));
     });
     return keys;
+}
+
+inline std::set<std::string> populate_store(Storage &store, std::string_view symbol_prefix, int start, int end,
+                                            entity::KeyType key_type = entity::KeyType::TABLE_DATA) {
+    auto symbols = std::set<std::string>();
+    for (int i = start; i < end; ++i) {
+        auto symbol = fmt::format("{}_{}", symbol_prefix, i);
+        write_in_store(store, symbol, key_type);
+        symbols.emplace(symbol);
+    }
+    return symbols;
 }
 
 }
