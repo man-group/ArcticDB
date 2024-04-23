194b3f12d1376cb73d3592f548a43d73c1271482
Alex Owens <alexowens90@gmail.com>
2024-04-23 16:46:36 +0100

    Fix bug

--- python/tests/integration/arcticdb/version_store/test_deletion.py
+++ python/tests/integration/arcticdb/version_store/test_deletion.py
@@ -571,17 +571,18 @@ def test_delete_date_range_remove_everything(version_store_factory, map_timeout)
 def test_delete_date_range_get_info(lmdb_version_store_v1):
     lib = lmdb_version_store_v1
     sym = "test_delete_date_range_get_info"
-    df = pd.DataFrame({"col": np.arange(5)}, index=pd.date_range(pd.Timestamp("2024-01-01"), periods=5))
+    df = pd.DataFrame({"col": np.arange(5)}, index=pd.date_range(pd.Timestamp(1000), freq="us", periods=5))
     lib.write(sym, df)
-    assert df.index[0] == lib.get_info(sym)["date_range"][0]
-    assert df.index[-1] == lib.get_info(sym)["date_range"][1]
+    date_range = lib.get_info(sym)["date_range"]
+    assert df.index[0] == date_range[0]
+    assert df.index[-1] == date_range[1]
 
-    lib.delete(sym, (pd.Timestamp("2024-01-04"), pd.Timestamp("2024-01-05")))
+    lib.delete(sym, (pd.Timestamp(4000), pd.Timestamp(5000)))
     received = lib.read(sym).data
     assert_frame_equal(df.iloc[:3], received)
     assert received.index[-1] == lib.get_info(sym)["date_range"][1]
 
-    lib.delete(sym, (pd.Timestamp("2024-01-01"), pd.Timestamp("2024-01-02")))
+    lib.delete(sym, (pd.Timestamp(1000), pd.Timestamp(2000)))
     received = lib.read(sym).data
     assert_frame_equal(df.iloc[2:3], received)
     assert received.index[0] == lib.get_info(sym)["date_range"][0]
