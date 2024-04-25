daadcdaa2a72bb4785a693df804b065a0a021877
Julien Jerphanion <git@jjerphan.xyz>
2024-04-25 09:40:15 +0200

    fixup! Also install CMake and Ninja

--- build_tooling/build_many_linux_image.sh
+++ build_tooling/build_many_linux_image.sh
@@ -39,10 +39,21 @@ echo "
 FROM $manylinux_image
 RUN rpm --import https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux
 RUN yum update -y && \
-    yum install -y zip jq less gcc-toolset-13-gdb \
-      openssl-devel cyrus-sasl-devel gcc-toolset-13-libatomic-devel \
-      libcurl-devel python3-devel flex cmake ninja && \
-    yum clean all && touch /etc/arcticdb_deps_installed
+    yum install -y \
+      zip \
+      jq \
+      less \
+      gcc-toolset-13-gdb \
+      openssl-devel \
+      cyrus-sasl-devel \
+      gcc-toolset-13-libatomic-devel \
+      libcurl-devel \
+      python3-devel \
+      flex \
+      cmake \
+      ninja-build && \
+    yum clean all &&\
+    touch /etc/arcticdb_deps_installed
 
 ENV CMAKE_C_COMPILER=/opt/rh/gcc-toolset-13/root/usr/bin/gcc
 ENV CMAKE_CXX_COMPILER=/opt/rh/gcc-toolset-13/root/usr/bin/g++
