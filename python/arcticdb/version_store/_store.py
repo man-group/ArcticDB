9858652d7a993cffedcb316b9654edff0c67b352
Muhammad Hamza Sajjad <45758804+muhammadhamzasajjad@users.noreply.github.com>
2024-04-26 17:10:42 +0100

    LibraryTool add read_index (#1533)
    #### Reference Issues/PRs
    Implements #1391 and fixes #1532.
    - Added read_index in LibraryTool (as required in #1391)
    - Allowed Library to get LibraryTool and call read_index (to fix #1532).
    Also updated the docs accordingly.
    
    #### What does this implement or fix?
    
    #### Any other comments?
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>
    
    <!--
    Thanks for contributing a Pull Request to ArcticDB! Please ensure you
    have taken a look at:
    - ArcticDB's Code of Conduct:
    https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
    - ArcticDB's Contribution Licensing:
    https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
    -->

--- python/arcticdb/version_store/_store.py
+++ python/arcticdb/version_store/_store.py
@@ -2819,4 +2819,4 @@ class NativeVersionStore:
         return self._library
 
     def library_tool(self) -> LibraryTool:
-        return LibraryTool(self.library())
+        return LibraryTool(self.library(), self)
