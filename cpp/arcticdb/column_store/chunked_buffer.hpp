91e02f4a8d5137da34791c5f15ee65f6188e062b
Alex Owens <alexowens90@gmail.com>
2024-05-14 15:21:17 +0100

    Comment changes

--- cpp/arcticdb/column_store/chunked_buffer.hpp
+++ cpp/arcticdb/column_store/chunked_buffer.hpp
@@ -216,10 +216,10 @@ class ChunkedBufferImpl {
 
     // Trim will reduce the size of the chunked buffer to the specified size by dropping blocks that are wholly unneeded
     // i.e. no allocation/memcpy is involved, only deallocation
-    // Use in performance critical code where a column is being created, but the final size is unknown at construction
-    // time by creating a Column using a chunked buffer that is presized in blocks. This unlocks ColumnDataIterator
-    // usage (more performant than repeated calls to Column::push_back). Once the column is created and the number of
-    // elements known, use this to drop unneeded blocks.
+    // Use in performance critical code where a column is being created, the final size is unknown at construction
+    // time but a maximum size is known, by creating a Column using a chunked buffer that is presized in blocks. This
+    // unlocks ColumnDataIterator usage (more performant than repeated calls to Column::push_back). Once the column is
+    // created and the number of elements known, use this to drop unneeded blocks.
     void trim(size_t requested_size) {
         internal::check<ErrorCode::E_ASSERTION_FAILURE>(requested_size <= bytes_,
                                                         "Cannot trim ChunkedBuffer with {} bytes to {} bytes",
