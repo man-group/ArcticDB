16070dc28750bf9492e7fb386525b0d0eb4c53c4
Alex Owens <73388657+alexowens90@users.noreply.github.com>
2024-06-11 13:25:17 +0100

    Refactor 1608: Re-introduce C++20 features (#1614)
    #### Reference Issues/PRs
    Closes #1608 
    
    Pure refactor to reintroduce some C++20 features (mostly `concepts`,
    `std::ranges::reverse_view`, and `std::erase_if`) that were removed when
    we emergency reverted to C++17 due to the `ray` import issue.
    
    There are more comments in the code base referencing changes that can be
    made after moving to C++20, particularly in `ranges_from_future.hpp`,
    but these require more intrusive changes throughout the code base.

--- cpp/arcticdb/async/tasks.hpp
+++ cpp/arcticdb/async/tasks.hpp
@@ -406,21 +406,13 @@ struct MemSegmentProcessingTask : BaseTask {
     ARCTICDB_MOVE_ONLY_DEFAULT(MemSegmentProcessingTask)
 
     Composite<EntityIds> operator()() {
-        // TODO: Replace with commented out code once C++20 is reinstated
-//        std::ranges::reverse_view reversed_clauses{clauses_};
-//        for (const auto& clause: reversed_clauses) {
-//            entity_ids_ = clause->process(std::move(entity_ids_));
-//
-//            if(clause->clause_info().requires_repartition_)
-//                break;
-//        }
-        for (auto clause = clauses_.crbegin(); clause != clauses_.crend(); ++clause) {
-            entity_ids_ = (*clause)->process(std::move(entity_ids_));
+        std::ranges::reverse_view reversed_clauses{clauses_};
+        for (const auto& clause: reversed_clauses) {
+            entity_ids_ = clause->process(std::move(entity_ids_));
 
-            if((*clause)->clause_info().requires_repartition_)
+            if(clause->clause_info().requires_repartition_)
                 break;
         }
-        // end TODO
         return std::move(entity_ids_);
     }
 
