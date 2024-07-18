d4f18ed395a4a32e802ef65341b280298bffad34
phoebusm <phoebusmak@gmail.com>
2024-07-18 12:19:52 +0100

    Port sparse data test from old repo

---
+++ python/tests/stress/arcticdb/version_store/test_sparse.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+
+
+def get_dataframe_dense_till(total_size=100, dense_till=50):
+    arr = []
+    for idx in range(total_size):
+        if idx < dense_till:
+            arr.append(float(idx))
+        else:
+            arr.append(np.nan)
+
+    return pd.DataFrame({"float": arr})
+
+
+def get_interleaved_dataframe(size=100):
+    arr = []
+    for idx in range(size):
+        if idx % 2 == 0:
+            arr.append(float(idx))
+        else:
+            arr.append(np.nan)
+
+    return pd.DataFrame({"float": arr})
+
+
+def test_sparse_interleaved(sym, lmdb_version_store):
+    lib = lmdb_version_store
+    df = get_interleaved_dataframe(100)
+    lib.write(sym, df, dynamic_schema=True)
+    dd = lib.read(sym, dynamic_schema=True).data
+
+    assert dd["float"][2] == df["float"][2]
+
+    # TODO: This should be NaN
+    assert np.isnan(dd["float"][1])
+
+
+def test_sparse_chunked(sym, lmdb_version_store):
+    lib = lmdb_version_store
+    df = get_dataframe_dense_till(100, 30)
+    lib.write(sym, df, allow_sparse=True)
+    dd = lib.read(sym, allow_sparse=True).data
+    assert dd["float"][2] == df["float"][2]
+
+    # TODO: This should be NaN
+    assert dd["float"][1] == 1.0
+    assert dd["float"][2] == 2.0
+
+
+def test_sparse_segmented(version_store_factory, sym):
+    lib = version_store_factory(column_group_size=100, segment_row_size=100)
+
+    df = get_interleaved_dataframe(100)
+    lib.write(sym, df, allow_sparse=True)
+    dd = lib.read(sym, allow_sparse=True).data
+    
+    assert dd["float"][2] == df["float"][2]
+    assert np.isnan(dd["float"][1])
