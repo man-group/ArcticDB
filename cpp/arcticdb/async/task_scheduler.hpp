c7046b1767151a585c50ba3c11c68b24eac1c3a4
Georgi Petrov <32372905+G-D-Petrov@users.noreply.github.com>
2024-07-23 10:39:23 +0300

    Add support for cgroup v2 cpu limits (#1696)
    #### Reference Issues/PRs
    Fixes #1695 
    
    #### What does this implement or fix?
    It extends the current cgroup handling logic to support the new
    interface.

--- cpp/arcticdb/async/task_scheduler.hpp
+++ cpp/arcticdb/async/task_scheduler.hpp
@@ -13,6 +13,8 @@
 #include <arcticdb/util/exponential_backoff.hpp>
 #include <arcticdb/util/configs_map.hpp>
 #include <arcticdb/util/home_directory.hpp>
+#include <arcticdb/util/string_utils.hpp>
+
 #include <arcticdb/async/base_task.hpp>
 #include <arcticdb/entity/performance_tracing.hpp>
 
@@ -67,8 +69,8 @@ public:
                 ARCTICDB_SAMPLE_THREAD();
               func();
             });
-
-    }
+    
+  }
 
     virtual const std::string& getNamePrefix() const override{
         return named_factory_.getNamePrefix();
@@ -101,31 +103,65 @@ struct SchedulerWrapper : public SchedulerType {
     }
 };
 
-inline int64_t get_cgroup_value(const std::string& cgroup_file) {
+struct CGroupValues {
+    std::optional<double> cpu_quota = std::nullopt;
+    std::optional<double> cpu_period = std::nullopt;
+};
+
+inline std::optional<double> get_cgroup_value_v1(const std::string& cgroup_file) {
     if(const auto path = std::filesystem::path{fmt::format("/sys/fs/cgroup/{}",cgroup_file)}; std::filesystem::exists(path)){
         std::ifstream strm(path.string());
-        util::check(static_cast<bool>(strm), "Failed to open cgroups cpu file for read at path '{}': {}", path.string(), std::strerror(errno));
+        util::check(static_cast<bool>(strm), "Failed to open cgroups v1 cpu file for read at path '{}': {}", path.string(), std::strerror(errno));
         std::string str;
         std::getline(strm, str);
-        return std::stol(str);
+        return std::stod(str);
     }
-    return static_cast<int64_t>(-1);
+    return std::nullopt;
+}
+
+inline CGroupValues get_cgroup_values_v1() {
+    return CGroupValues{get_cgroup_value_v1("cpu/cpu.cfs_quota_us"), get_cgroup_value_v1("cpu/cpu.cfs_period_us")};
+}
+
+// In cgroup v2, the /sys/fs/cgroup/cpu.max file is used and the format is $MAX $PERIOD
+// the default is max 100000
+inline CGroupValues get_cgroup_values_v2() {
+    if(const auto path = std::filesystem::path{"/sys/fs/cgroup/cpu.max"}; std::filesystem::exists(path)){
+        std::ifstream strm(path.string());
+        util::check(static_cast<bool>(strm), "Failed to open cgroups v2 cpu file for read at path '{}': {}", path.string(), std::strerror(errno));
+        std::string str;
+        std::getline(strm, str);
+        auto values = util::split_to_array<2>(str, ' ');
+
+        auto quota = std::string{values[0]};
+        auto period = std::string{values[1]};
+        if (quota == std::string("max"))
+            return CGroupValues{0, std::stod(period)};
+
+        return CGroupValues{std::stod(quota), std::stod(period)};
+    }
+
+    return CGroupValues{};
 }
 
 inline auto get_default_num_cpus() {
-    auto cpu_count = std::thread::hardware_concurrency() == 0 ? 16 : std::thread::hardware_concurrency();
+    int64_t cpu_count = std::thread::hardware_concurrency() == 0 ? 16 : std::thread::hardware_concurrency();
     #ifdef _WIN32
         return static_cast<int64_t>(cpu_count);
     #else
-        auto quota_count = 0UL;
-        auto quota = get_cgroup_value("cpu/cpu.cfs_quota_us");
-        auto period = get_cgroup_value("cpu/cpu.cfs_period_us");
+        int64_t quota_count = 0UL;
+        auto cgroup_val = get_cgroup_values_v1();
 
-        if (quota > -1 && period > 0)
-            quota_count = static_cast<int64_t>(ceil(static_cast<double>(quota) / static_cast<double>(period)));
+        // if cgroup v1 values are not found, try to get values from cgroup v2
+        if (!cgroup_val.cpu_quota.has_value() || !cgroup_val.cpu_period.has_value())
+            cgroup_val = get_cgroup_values_v2();
 
-        auto limit_count = quota_count != 0 ? quota_count : cpu_count;
-        return std::min(static_cast<int64_t>(cpu_count), static_cast<int64_t>(limit_count));
+        if ((cgroup_val.cpu_quota.has_value() && cgroup_val.cpu_period.has_value()) &&
+             (cgroup_val.cpu_quota.value() > -1 && cgroup_val.cpu_period.value() > 0))
+            quota_count = static_cast<int64_t>(ceil(cgroup_val.cpu_quota.value() / cgroup_val.cpu_period.value()));
+
+        int64_t limit_count = quota_count != 0 ? quota_count : cpu_count;
+        return std::min(cpu_count, limit_count);
     #endif
 }
 
@@ -148,7 +184,7 @@ class TaskScheduler {
     using CPUSchedulerType = folly::FutureExecutor<folly::CPUThreadPoolExecutor>;
     using IOSchedulerType = folly::FutureExecutor<folly::IOThreadPoolExecutor>;
 
-    explicit TaskScheduler(const std::optional<size_t>& cpu_thread_count = std::nullopt, const std::optional<size_t>& io_thread_count = std::nullopt) :
+     explicit TaskScheduler(const std::optional<size_t>& cpu_thread_count = std::nullopt, const std::optional<size_t>& io_thread_count = std::nullopt) :
         cpu_thread_count_(cpu_thread_count ? *cpu_thread_count : ConfigsMap::instance()->get_int("VersionStore.NumCPUThreads", get_default_num_cpus())),
         io_thread_count_(io_thread_count ? *io_thread_count : ConfigsMap::instance()->get_int("VersionStore.NumIOThreads", (int) (cpu_thread_count_ * 1.5))),
         cpu_exec_(cpu_thread_count_, std::make_shared<InstrumentedNamedFactory>("CPUPool")) ,
