0a33f5a213f1be566bb98e6e6f88891f7256997f
Alex Owens <73388657+alexowens90@users.noreply.github.com>
2024-07-26 12:04:38 +0300

    Bugfix 1692: fix batch read with row range no query builder (#1710)
    Fixes #1692

--- cpp/arcticdb/version/python_bindings.cpp
+++ cpp/arcticdb/version/python_bindings.cpp
@@ -321,11 +321,12 @@ void register_bindings(py::module &version, py::exception<arcticdb::ArcticExcept
             .def_property_readonly("end", &DateRangeClause::end)
             .def("__str__", &DateRangeClause::to_string);
 
-    py::class_<ReadQuery>(version, "PythonVersionStoreReadQuery")
+    py::class_<ReadQuery, std::shared_ptr<ReadQuery>>(version, "PythonVersionStoreReadQuery")
             .def(py::init())
             .def_readwrite("columns",&ReadQuery::columns)
             .def_readwrite("row_range",&ReadQuery::row_range)
             .def_readwrite("row_filter",&ReadQuery::row_filter)
+            .def_readonly("needs_post_processing",&ReadQuery::needs_post_processing)
             // Unsurprisingly, pybind11 doesn't understand folly::poly, so use vector of variants here
             .def("add_clauses",
                  [](ReadQuery& self,
@@ -341,6 +342,22 @@ void register_bindings(py::module &version, py::exception<arcticdb::ArcticExcept
                 for (auto&& clause: clauses) {
                     util::variant_match(
                         clause,
+                        [&](std::shared_ptr<RowRangeClause> clause) {
+                            self.needs_post_processing = false;
+                            _clauses.emplace_back(std::make_shared<Clause>(*clause));
+                        },
+                        [&](std::shared_ptr<DateRangeClause> clause) {
+                            self.needs_post_processing = false;
+                            _clauses.emplace_back(std::make_shared<Clause>(*clause));
+                        },
+                        [&](std::shared_ptr<ResampleClause<ResampleBoundary::LEFT>> clause) {
+                            self.needs_post_processing = false;
+                            _clauses.emplace_back(std::make_shared<Clause>(*clause));
+                        },
+                        [&](std::shared_ptr<ResampleClause<ResampleBoundary::RIGHT>> clause) {
+                            self.needs_post_processing = false;
+                            _clauses.emplace_back(std::make_shared<Clause>(*clause));
+                        },
                         [&](auto&& clause) {_clauses.emplace_back(std::make_shared<Clause>(*clause));}
                     );
                 }
@@ -670,13 +687,17 @@ void register_bindings(py::module &version, py::exception<arcticdb::ArcticExcept
              &PythonVersionStore::force_release_lock,
              py::call_guard<SingleThreadMutexHolder>(), "Force release a lock.")
         .def("batch_read",
-             [&](PythonVersionStore& v, const std::vector<StreamId> &stream_ids,  const std::vector<VersionQuery>& version_queries, std::vector<ReadQuery> read_queries, const ReadOptions& read_options){
+             [&](PythonVersionStore& v,
+                 const std::vector<StreamId> &stream_ids,
+                 const std::vector<VersionQuery>& version_queries,
+                 std::vector<std::shared_ptr<ReadQuery>>& read_queries,
+                 const ReadOptions& read_options){
                  return python_util::adapt_read_dfs(v.batch_read(stream_ids, version_queries, read_queries, read_options));
              },
              py::call_guard<SingleThreadMutexHolder>(), "Read a dataframe from the store")
         .def("batch_read_keys",
              [&](PythonVersionStore& v, std::vector<AtomKey> atom_keys) {
-                 return python_util::adapt_read_dfs(frame_to_read_result(v.batch_read_keys(atom_keys, {}, ReadOptions{})));
+                 return python_util::adapt_read_dfs(frame_to_read_result(v.batch_read_keys(atom_keys)));
              },
              py::call_guard<SingleThreadMutexHolder>(), "Read a specific version of a dataframe from the store")
         .def("batch_write",
