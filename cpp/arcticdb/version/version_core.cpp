8db2b785928ceaf830ffb87f1d37ea6ea182ba99
Vasil Pashov <vasil.pashov@man.com>
2024-08-20 16:26:51 +0300

    Add comments for unreachable code as per review request

--- cpp/arcticdb/version/version_core.cpp
+++ cpp/arcticdb/version/version_core.cpp
@@ -860,7 +860,8 @@ void check_incompletes_index_ranges_dont_overlap(const std::shared_ptr<PipelineC
                     !last_existing_index_value.has_value() || it->slice_and_key().key().start_time() >= *last_existing_index_value,
                     "Cannot append staged segments to existing data as incomplete segment contains index value < existing data (in UTC): {} <= {}",
                     date_and_time(it->slice_and_key().key().start_time()),
-                    last_existing_index_value ? date_and_time(*last_existing_index_value) : "NaT"
+                    // Should never reach "" but the standard mandates that all function arguments are evaluated
+                    last_existing_index_value ? date_and_time(*last_existing_index_value) : ""
             );
             auto [_, inserted] = unique_timestamp_ranges.insert({it->slice_and_key().key().start_time(), it->slice_and_key().key().end_time()});
             // This is correct because incomplete segments aren't column sliced
