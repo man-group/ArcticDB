4238faa7dcf3a6b24ad0ab8fb8f249049aaf9147
Vasil Pashov <vasil.pashov@man.com>
2024-09-04 10:33:09 +0300

    Add more unit tests

--- python/tests/unit/arcticdb/version_store/test_normalization.py
+++ python/tests/unit/arcticdb/version_store/test_normalization.py
@@ -635,6 +635,14 @@ def test_bools_are_pickled(lmdb_version_store_allows_pickling):
     lib.get_info(sym)['type'] == 'pickled'
     assert_frame_equal(df, lib.read(sym).data)
 
+def test_bools_with_nan_throw_without_pickling(lmdb_version_store_v1):
+    lib = lmdb_version_store_v1
+    sym = "test_bools_throw_without_pickling"
+
+    df = pd.DataFrame({"a": [True, False, np.nan]})
+    with pytest.raises(Exception):
+        lib.write(sym, df)
+
 def test_arrays_are_pickled(lmdb_version_store_allows_pickling):
     lib = lmdb_version_store_allows_pickling
     sym = "test_arrays_are_pickled"
@@ -648,3 +656,12 @@ def test_arrays_are_pickled(lmdb_version_store_allows_pickling):
     lib.write(sym, df)
     lib.get_info(sym)['type'] == 'pickled'
     assert_frame_equal(df, lib.read(sym).data)
+
+def test_arrays_throw_without_pickling(lmdb_version_store_v1):
+    lib = lmdb_version_store_v1
+    sym = "test_arrays_throw_without_pickling"
+
+    df = pd.DataFrame({"a": [np.array([1, 2])]})
+
+    with pytest.raises(Exception):
+        lib.write(sym, df)
