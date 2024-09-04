565255719476bd85ce08c4d41b85410f8e6caa08
Vasil Danielov Pashov <vasil.pashov1@gmail.com>
2024-09-04 15:05:31 +0300

    Fix regression in bool pickling [backport 4.4.x] (#1812)
    #### Reference Issues/PRs
    <!--Example: Fixes #1234. See also #3456.-->
    
    #### What does this implement or fix?
    
    #### Any other comments?
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>
    
    <!--
    Thanks for contributing a Pull Request to ArcticDB! Please ensure you
    have taken a look at:
    - ArcticDB's Code of Conduct:
    https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
    - ArcticDB's Contribution Licensing:
    https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
    -->
    
    Co-authored-by: Vasil Pashov <vasil.pashov@man.com>

--- python/arcticdb/version_store/_normalization.py
+++ python/arcticdb/version_store/_normalization.py
@@ -240,8 +240,6 @@ def _to_primitive(arr, arr_name, dynamic_strings, string_max_len=None, coerce_co
             casted_arr = coerce_string_column_to_fixed_length_array(arr, type(sample), string_max_len)
     elif dynamic_strings and sample is None:  # arr is entirely empty
         return arr
-    elif isinstance(sample, bool) or isinstance(sample, np.ndarray):
-        return arr
     else:
         raise ArcticDbNotYetImplemented(
             f"Failed to normalize column '{arr_name}' with dtype '{arr.dtype}'. Found first non-null value of type "
