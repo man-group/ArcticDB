0efb1b4cd01472381b36558412feb4faf4acf0a3
Ivo Dilov <iddilov@gmail.com>
2024-09-12 17:34:04 +0300

    Adds a type change utility for append data keys in lib_tool
    Does this by adding a cpp layer functionality to overwrite append data
    keys. And using a read + type change + overwrite in the python layer.
    
    More specifically this change involves:
    - Using a `LocalVersionEngine` instead of an `AsyncStore` as library
      tool state
    - Exposing some more python bindings to allow iteration over `APPEND_DATA`
      keys with the library tool
    - Allow using normalization in library tool to allow overwriting append
      data keys with a custom dataframe
    - Provide the type change functionality by reading a dataframe, changing
      its type with pandas and overwriting it
    - Adds an elaborate test to verify iterating and reading append data
      linked list is fine with various overwrites.

--- cpp/arcticdb/async/async_store.hpp
+++ cpp/arcticdb/async/async_store.hpp
@@ -18,6 +18,10 @@
 #include <arcticdb/processing/clause.hpp>
 #include <arcticdb/storage/key_segment_pair.hpp>
 
+namespace arcticdb::toolbox::apy{
+    class LibraryTool;
+}
+
 namespace arcticdb::async {
 
 std::pair<VariantKey, std::optional<Segment>> lookup_match_in_dedup_map(
@@ -395,6 +399,7 @@ folly::Future<SliceAndKey> async_write(
     }
 
 private:
+    friend class arcticdb::toolbox::apy::LibraryTool;
     std::shared_ptr<storage::Library> library_;
     std::shared_ptr<arcticdb::proto::encoding::VariantCodec> codec_;
     const EncodingVersion encoding_version_;
