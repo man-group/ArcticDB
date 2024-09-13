5ef9514f744a3f749ebece2abbdb92bf5e26b44d
Vasil Pashov <vasil.pashov@man.com>
2024-09-13 17:46:09 +0300

    Fix hypothesis tests

--- python/tests/hypothesis/arcticdb/test_sort_merge.py
+++ python/tests/hypothesis/arcticdb/test_sort_merge.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from hypothesis import assume, given
+from hypothesis import assume, given, settings
 import hypothesis.extra.pandas as hs_pd
 import hypothesis.strategies as st
 from hypothesis.stateful import RuleBasedStateMachine, rule, initialize, run_state_machine_as_test, precondition
@@ -13,7 +13,7 @@ import string
 from arcticdb.util._versions import IS_PANDAS_TWO
 from pandas.api.types import is_numeric_dtype, is_integer_dtype, is_float_dtype
 from arcticdb.util.hypothesis import use_of_function_scoped_fixtures_in_hypothesis_checked
-
+from arcticdb.toolbox.library_tool import KeyType
 
 ColumnInfo = namedtuple('ColumnInfo', ['name', 'dtype'])
 
@@ -32,7 +32,7 @@ def string_column_strategy(name):
     return hs_pd.column(name=name, elements=st.text(alphabet=string.ascii_letters))
 
 @st.composite
-def generate_single_dataframe(draw, column_list):
+def generate_single_dataframe(draw, column_list, min_size=0, allow_nat_in_index=True):
     column_infos = draw(st.lists(st.sampled_from(column_list), unique_by=lambda x: x.name, min_size=1))
     columns = [hs_pd.column(name=ci.name, dtype=ci.dtype) if ci.dtype != 'object' else string_column_strategy(ci.name) for ci in column_infos]
     if not IS_PANDAS_TWO:
@@ -40,9 +40,9 @@ def generate_single_dataframe(draw, column_list):
         # we change the dtype of empty float columns. This makes hypothesis tests extremely hard to write as we must
         # keep addional state about is there a mix of empty/non-empty float columns in the staging area, did we write
         # empty float column (if so it's type would be object). These edge cases are covered in the unit tests.
-        index = hs_pd.indexes(dtype="datetime64[ns]", min_size=1)
+        index = hs_pd.indexes(dtype="datetime64[ns]", min_size=1 if min_size <= 0 else min_size).filter(lambda x: allow_nat_in_index or not pd.NaT in x)
     else:
-        index = hs_pd.indexes(dtype="datetime64[ns]")
+        index = hs_pd.indexes(dtype="datetime64[ns]", min_size=min_size).filter(lambda x: allow_nat_in_index or not pd.NaT in x)
     return draw(hs_pd.data_frames(columns, index=index))
 
 @st.composite
@@ -110,98 +110,126 @@ def segments_have_compatible_schema(segment_list):
                 return False
     return True
 
+def get_append_keys(lib, sym):
+    lib_tool = lib._nvs.library_tool()
+    keys = lib_tool.find_keys_for_symbol(KeyType.APPEND_DATA, sym)
+    return keys
+
 @use_of_function_scoped_fixtures_in_hypothesis_checked
+@settings(deadline=None)
 @given(df_list=generate_dataframes(COLUMN_DESCRIPTIONS))
 def test_sort_merge_static_schema_write(lmdb_library, df_list):
     lib = lmdb_library
+    sym = "test_sort_merge_static_schema_write"
     for df in df_list:
-        lib.write("sym", df, staged=True, validate_index=False)
+        lib.write(sym, df, staged=True, validate_index=False)
     if len(df_list) == 0:
-        assert_cannot_finalize_without_staged_data(lib, "sym", StagedDataFinalizeMethod.WRITE)
+        assert_cannot_finalize_without_staged_data(lib, sym, StagedDataFinalizeMethod.WRITE)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     if not all(df_list[0].dtypes.equals(segment.dtypes) for segment in df_list):
-        assert_staged_columns_are_compatible(lib, "sym", StagedDataFinalizeMethod.WRITE)
+        assert_staged_columns_are_compatible(lib, sym, StagedDataFinalizeMethod.WRITE)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     if has_nat_in_index(df_list):
-        assert_nat_is_not_supported(lib, "sym", StagedDataFinalizeMethod.WRITE)
+        assert_nat_is_not_supported(lib, sym, StagedDataFinalizeMethod.WRITE)
+        assert len(get_append_keys(lib, sym)) == 0
         return
+    lib.sort_and_finalize_staged_data(sym, mode=StagedDataFinalizeMethod.WRITE)
+    assert len(get_append_keys(lib, sym)) == 0
     expected = merge_and_sort_segment_list(df_list)
-    lib.sort_and_finalize_staged_data("sym", mode=StagedDataFinalizeMethod.WRITE)
-    data = lib.read("sym").data
+    data = lib.read(sym).data
     assert_equal(expected, data)
 
 @use_of_function_scoped_fixtures_in_hypothesis_checked
-@given(df_list=generate_dataframes(COLUMN_DESCRIPTIONS), initial_df=generate_single_dataframe(COLUMN_DESCRIPTIONS))
+@settings(deadline=None)
+@given(df_list=generate_dataframes(COLUMN_DESCRIPTIONS), initial_df=generate_single_dataframe(COLUMN_DESCRIPTIONS, min_size=1, allow_nat_in_index=False))
 def test_sort_merge_static_schema_append(lmdb_library, df_list, initial_df):
     lib = lmdb_library
-    assume(len(initial_df) > 0 and not has_nat_in_index([initial_df]))
+    sym = "test_sort_merge_static_schema_append"
     initial_df.sort_index(inplace=True)
-    lib.write("sym", initial_df)
+    lib.write(sym, initial_df)
     for df in df_list:
-        lib.write("sym", df, staged=True, validate_index=False)
+        lib.write(sym, df, staged=True, validate_index=False)
     if len(df_list) == 0:
-        assert_cannot_finalize_without_staged_data(lib, "sym", StagedDataFinalizeMethod.APPEND)
+        assert_cannot_finalize_without_staged_data(lib, sym, StagedDataFinalizeMethod.APPEND)
         return
     if not all(initial_df.dtypes.equals(segment.dtypes) for segment in df_list):
-        assert_staged_columns_are_compatible(lib, "sym", StagedDataFinalizeMethod.APPEND)
+        assert_staged_columns_are_compatible(lib, sym, StagedDataFinalizeMethod.APPEND)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     if has_nat_in_index(df_list):
-        assert_nat_is_not_supported(lib, "sym", StagedDataFinalizeMethod.APPEND)
+        assert_nat_is_not_supported(lib, sym, StagedDataFinalizeMethod.APPEND)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     merged_staging = merge_and_sort_segment_list(df_list)
-    if initial_df.index[-1] > merged_staging.index[-1]:
-        assert_appended_data_does_not_overlap_with_storage(lib, "sym")
+    if len(merged_staging) > 0 and initial_df.index[-1] > merged_staging.index[0]:
+        assert_appended_data_does_not_overlap_with_storage(lib, sym)
+        assert len(get_append_keys(lib, sym)) == 0
         return
+    lib.sort_and_finalize_staged_data(sym, mode=StagedDataFinalizeMethod.APPEND)
+    assert len(get_append_keys(lib, sym)) == 0
     expected = pd.concat([initial_df, merged_staging])
-    lib.sort_and_finalize_staged_data("sym", mode=StagedDataFinalizeMethod.APPEND)
-    data = lib.read("sym").data
+    data = lib.read(sym).data
     assert_equal(expected, data)
 
 @use_of_function_scoped_fixtures_in_hypothesis_checked
+@settings(deadline=None)
 @given(df_list=generate_dataframes(COLUMN_DESCRIPTIONS))
 def test_sort_merge_dynamic_schema_write(lmdb_library_dynamic_schema, df_list):
     lib = lmdb_library_dynamic_schema
+    sym = "test_sort_merge_dynamic_schema_write"
     for df in df_list:
-        lib.write("sym", df, staged=True, validate_index=False)
+        lib.write(sym, df, staged=True, validate_index=False)
     if len(df_list) == 0:
-        assert_cannot_finalize_without_staged_data(lib, "sym", StagedDataFinalizeMethod.WRITE)
+        assert_cannot_finalize_without_staged_data(lib, sym, StagedDataFinalizeMethod.WRITE)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     if not segments_have_compatible_schema(df_list):
-        assert_staged_columns_are_compatible(lib, "sym", StagedDataFinalizeMethod.WRITE)
+        assert_staged_columns_are_compatible(lib, sym, StagedDataFinalizeMethod.WRITE)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     if has_nat_in_index(df_list):
-        assert_nat_is_not_supported(lib, "sym", StagedDataFinalizeMethod.WRITE)
+        assert_nat_is_not_supported(lib, sym, StagedDataFinalizeMethod.WRITE)
+        assert len(get_append_keys(lib, sym)) == 0
         return
-    lib.sort_and_finalize_staged_data("sym", mode=StagedDataFinalizeMethod.WRITE)
-    data = lib.read("sym").data
+    lib.sort_and_finalize_staged_data(sym, mode=StagedDataFinalizeMethod.WRITE)
+    assert len(get_append_keys(lib, sym)) == 0
+    data = lib.read(sym).data
     int_columns_in_df = [col_name for col_name in data if is_integer_dtype(data.dtypes[col_name])]
     expected = merge_and_sort_segment_list(df_list, int_columns_in_df=int_columns_in_df)
     assert_equal(expected, data)
 
 @use_of_function_scoped_fixtures_in_hypothesis_checked
-@given(df_list=generate_dataframes(COLUMN_DESCRIPTIONS), initial_df=generate_single_dataframe(COLUMN_DESCRIPTIONS))
-def test_sort_merge_dynamic_schema_append(lmdb_library_dynamic_schema, df_list, initial_df):
+@settings(deadline=None)
+@given(df_list=generate_dataframes(COLUMN_DESCRIPTIONS), initial_df=generate_single_dataframe(COLUMN_DESCRIPTIONS, min_size=1, allow_nat_in_index=False))
+def test_sort_merge_dynamic_schema_append(lmdb_library_dynamic_schema, df_list, initial_df):    
     lib = lmdb_library_dynamic_schema
-    assume(len(initial_df) > 0 and not has_nat_in_index([initial_df]))
+    sym = "test_sort_merge_dynamic_schema_append"
     initial_df.sort_index(inplace=True)
-    lib.write("sym", initial_df)
+    lib.write(sym, initial_df)
     for df in df_list:
-        lib.write("sym", df, staged=True, validate_index=False)
+        lib.write(sym, df, staged=True, validate_index=False)
     if len(df_list) == 0:
-        assert_cannot_finalize_without_staged_data(lib, "sym", StagedDataFinalizeMethod.APPEND)
+        assert_cannot_finalize_without_staged_data(lib, sym, StagedDataFinalizeMethod.APPEND)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     if not segments_have_compatible_schema([initial_df, *df_list]):
-        assert_staged_columns_are_compatible(lib, "sym", StagedDataFinalizeMethod.APPEND)
+        assert_staged_columns_are_compatible(lib, sym, StagedDataFinalizeMethod.APPEND)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     if has_nat_in_index(df_list):
-        assert_nat_is_not_supported(lib, "sym", StagedDataFinalizeMethod.APPEND)
+        assert_nat_is_not_supported(lib, sym, StagedDataFinalizeMethod.APPEND)
+        assert len(get_append_keys(lib, sym)) == 0
         return
     merged_staging = merge_and_sort_segment_list(df_list)
-    if initial_df.index[-1] > merged_staging.index[-1]:
-        assert_appended_data_does_not_overlap_with_storage(lib, "sym")
+    if len(merged_staging) > 0 and initial_df.index[-1] > merged_staging.index[0]:
+        assert_appended_data_does_not_overlap_with_storage(lib, sym)
+        assert len(get_append_keys(lib, sym)) == 0
         return
-    lib.sort_and_finalize_staged_data("sym", mode=StagedDataFinalizeMethod.APPEND)
-    data = lib.read("sym").data
+    lib.sort_and_finalize_staged_data(sym, mode=StagedDataFinalizeMethod.APPEND)
+    assert len(get_append_keys(lib, sym)) == 0
+    data = lib.read(sym).data
     int_columns_in_df = [col_name for col_name in data if is_integer_dtype(data.dtypes[col_name])]
-    expected = merge_and_sort_segment_list([initial_df, *df_list], int_columns_in_df=int_columns_in_df)
+    expected = merge_and_sort_segment_list([initial_df, merged_staging], int_columns_in_df=int_columns_in_df)
     assert_equal(expected, data)
