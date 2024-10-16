5d6227f9dcb0abfd0ff1d86d3cde6a3479005f73
Vasil Pashov <vasil.pashov1@gmail.com>
2024-10-16 16:09:47 +0300

    Remove vcpkg man nuget

--- build_tooling/vcpkg_caching.sh
+++ build_tooling/vcpkg_caching.sh
@@ -11,12 +11,14 @@ echo "Using nuget=$nuget"
 
 VCPKG_BINARY_SOURCES="clear;nuget,github,readwrite"
 url="https://nuget.pkg.github.com/$VCPKG_NUGET_USER/index.json"
+echo "Nuget user is $VCPKG_NUGET_USER url is $url"
 $nuget sources add -source $url -storepasswordincleartext -name github \
     -username "${VCPKG_NUGET_USER:?environment variable is not set}" \
     -password "${VCPKG_NUGET_TOKEN:?environment variable is not set}"
 $nuget setapikey "$VCPKG_NUGET_TOKEN" -source $url
 
 if [[ -n "$VCPKG_MAN_NUGET_TOKEN" ]] ; then
+    echo "$VCPKG_MAN_NUGET_TOKEN is setup proceeding with man-group nuget"
     $nuget sources add -source "https://nuget.pkg.github.com/man-group/index.json" -storepasswordincleartext -name man \
     -username "${VCPKG_MAN_NUGET_USER:-$VCPKG_NUGET_USER}" \
     -password "$VCPKG_MAN_NUGET_TOKEN"
