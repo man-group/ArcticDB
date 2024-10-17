ae0c4ce7d3bca952729e3c0910fa0a5608208b73
William Dealtry <william.dealtry@gmail.com>
2024-10-17 12:02:15 +0100

    Correct header for shared_mutex

--- cpp/arcticdb/util/lru_cache.hpp
+++ cpp/arcticdb/util/lru_cache.hpp
@@ -4,7 +4,7 @@
  *
  * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
  */
-#include <mutex>
+#include <shared_mutex>
 #include <memory>
 
 #include <ankerl/unordered_dense.h>
