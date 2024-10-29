8de497c80c5fff154682642e8b296b983a9de8a5
Georgi Rusev <george_rusev@yahoo.com>
2024-10-29 13:50:50 +0200

    missed file

--- python/arcticdb/util/test.py
+++ python/arcticdb/util/test.py
@@ -11,6 +11,7 @@ from contextlib import contextmanager
 from typing import Mapping, Any, Optional, Iterable, NamedTuple, List, AnyStr
 import numpy as np
 import pandas as pd
+import datetime as dt
 import string
 import random
 import time
@@ -43,6 +44,109 @@ def create_df(start=0, columns=1) -> pd.DataFrame:
 
     index = np.arange(start, start + 10, dtype=np.int64)
     return pd.DataFrame(data, index=index)
+
+def create_df_index_rownum(num_columns: int, start_index: int, end_index : int) -> pd.DataFrame:
+    """
+        Creates a data frame with (integer index) specified number of columns with integer index starting
+        from specified and ending in specified position. The content of the dataframe is 
+        integer random numbers ['start_index', 'end_index')
+
+        Why this is useful? Consider this example:
+
+          df1 = create_df_index_rownum(2, 0, 2)
+          df2 = create_df_index_rownum(2, 2, 4)
+          df_full = dataframe_update_full(df1, df2)
+        
+        This will produce 3 dataframes and the result of the update full operation would be 
+        visibly correct, even without other methods of comparison:
+
+            df1:
+            COL_0  COL_1
+            0      1      1
+            1      1      0
+            --------------------
+            df2:
+            COL_0  COL_1
+            2      3      2
+            3      2      2
+            --------------------
+            df_full:
+            COL_0  COL_1
+            0      1      1
+            1      1      0
+            2      3      2
+            3      2      2
+
+    """
+    rows = end_index - start_index
+    cols = ['COL_%d' % i for i in range(num_columns)]
+    df = pd.DataFrame(np.random.randint(start_index, 
+                                        end_index, 
+                                        size=(rows, num_columns)), 
+                                        columns=cols)
+    df.index = np.arange(start_index, end_index, 1).tolist()
+    return df
+
+def create_df_index_datetime(num_columns: int, start_hour: int, end_hour : int) -> pd.DataFrame:
+    """
+        Creates data frame with specified number of solumns 
+        with datetime index sorted, starting from start_hour till
+        end hour (you can use thousands of hours if needed). 
+        The data is random integer data again with min 'start_hour' and
+        max 'end_hour'. The goal is to achieve same visually understandable 
+        correctness validation of append/update operations (see 'create_df_index_rownum()' doc)
+    """
+    assert start_hour >= 0 , "start index must be positive"
+    assert end_hour >= 0 , "end index must be positive"
+
+    time_start = dt.datetime(2000, 1, 1, 0)
+    rows = end_hour - start_hour
+    cols = ['COL_%d' % i for i in range(num_columns)]
+    df = pd.DataFrame(np.random.randint(start_hour, 
+                                        end_hour, 
+                                        size=(rows, num_columns)), 
+                                        columns=cols)
+    start_date = time_start + dt.timedelta(hours=start_hour)
+    dr = pd.date_range(start_date, periods=rows, freq='h').astype("datetime64[ns]")
+    df.index = dr
+    return df
+
+def dataframe_update(update_to : pd.DataFrame, update_from: pd.DataFrame) -> pd.DataFrame:
+    """
+        Creates a new dataframe where you have a result of update and append (head/tail) operations
+        applied over the content of 'update_to' dataframe with content from 'update_from' dataframe
+    """
+    df_updated = update_to.copy(deep=True)
+    df_updated.update(update_from)
+    return df_updated
+
+def dataframe_update_full(update_to : pd.DataFrame, update_from: pd.DataFrame) -> pd.DataFrame:
+    '''
+        Updates the existing rows of an indexed DataFrame and then adds missing rows
+        to it. The result is a new Dataframe that contains the result of operation, leaving
+        the imput dataframes untouched
+    '''
+    update_to = dataframe_update(update_to, update_from)
+
+    df1 = update_to.copy(deep=True)
+    df2 = update_from.copy(deep=True)
+
+    # Reset index to merge on datetime
+    df1_reset = df1.reset_index()
+    df2_reset = df2.reset_index()
+    columns = list(df1_reset.columns)
+    # Merge DataFrames with indicator
+    merged_df = df1_reset.merge(df2_reset, on=columns, how='right', indicator=True)
+    # Filter rows that are only in df2
+    missing_rows = merged_df[merged_df['_merge'] == 'right_only'].drop(columns=['_merge'])
+    # Set the index back to datetime
+    missing_rows = missing_rows.set_index('index')
+    # Append missing rows to df1
+    df1 = pd.concat([df1, missing_rows], ignore_index=False)
+
+    df1.sort_index(inplace=True) # We need to sort it at the end
+
+    return df1
 
 
 def maybe_not_check_freq(f):
