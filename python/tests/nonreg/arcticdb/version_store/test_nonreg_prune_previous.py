b06d6185ffe768d701f78093fbd6979ab29ba67a
Vasil Danielov Pashov <vasil.pashov1@gmail.com>
2024-11-05 11:31:05 +0200

    Fix delete snapshot so that it doesn't orphan data keys or delete the wrong key (#1973)
    #### Reference Issues/PRs
    Fixes: #1958
    Fixes: #1863
    #### What does this implement or fix?
    
    * Fix orphaned keys. recurse_index_keys does not assume that the all
      atom keys are for the same stream id anymore
    * Fix AggregationClause::process vector iterator invalidation. Not
      related to the issue but C++ were crashing at runtime because of this.
      Calling erase invalidates the iterator in a loop.
    
    #### Any other comments?
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>
    
    <!--
    Thanks for contributing a Pull Request to ArcticDB! Please ensure you
    have taken a look at:
    - ArcticDB's Code of Conduct:
    https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
    - ArcticDB's Contribution Licensing:
    https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
    -->
    
    ---------
    
    Co-authored-by: Alex Seaton <alexbseaton@gmail.com>
    Co-authored-by: Vasil Pashov <vasil.pashov@man.com>

---
+++ python/tests/nonreg/arcticdb/version_store/test_nonreg_prune_previous.py
@@ -0,0 +1,16 @@
+import gc, time
+import pytest
+
+@pytest.mark.skip(reason="Takes too much time. Unskip to test if the memory usage and speed of prunung are affected.")
+def test_prune_previous_memory_usage(lmdb_version_store_very_big_map):
+    lib = lmdb_version_store_very_big_map
+    sym = "test_prune_previous_memory_usage"
+    num_versions = 8000
+    for idx in range(num_versions):
+        lib.append(sym, pd.DataFrame({"col": np.arange(idx, idx+1)}), write_if_missing=True)
+    assert len(lib.list_versions(sym)) == num_versions
+    gc.collect()
+    start = time.time()
+    lib.prune_previous_versions(sym)
+    print(f"Prune took {time.time() - start}s")
+    assert len(lib.list_versions(sym)) == 1
