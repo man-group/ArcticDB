873a994d02381aebb4e017333cbf9ec518b7fd27
Georgi Rusev <Georgi Rusev>
2024-12-10 11:41:36 +0200

    memray tests for QB OOM+mem_limit

--- python/tests/stress/arcticdb/version_store/test_mem_leaks.py
+++ python/tests/stress/arcticdb/version_store/test_mem_leaks.py
@@ -16,8 +16,16 @@ import numpy as np
 import pandas as pd
 import pytest
 import arcticdb as adb
+import gc
+import random
+import time
+from typing import Generator, Tuple
+import pandas as pd
+from pytest_memray import Stack
 
-from arcticdb.util.test import get_sample_dataframe
+from arcticdb.util.test import get_sample_dataframe, random_string
+from arcticdb.version_store.library import Library
+from arcticdb.version_store.processing import QueryBuilder
 from arcticdb.version_store._store import NativeVersionStore
 
 
@@ -25,7 +33,7 @@ def nice_bytes_str(bytes):
     return f" {bytes / (1024 * 1024):.2f}MB/[{bytes}] "
 
 
-def lets_collect_some_garbage(time_sec: int = 9):
+def lets_collect_some_garbage(time_sec: int = 7):
     """
     Do a garbage collection
     """
@@ -101,7 +109,7 @@ def check_process_memory_leaks(
 
         print("Starting watched code ...........")
         process_func()
-        lets_collect_some_garbage()
+        lets_collect_some_garbage(9)
 
         p_iter_mem_end: np.int64 = p.memory_info().rss
         process_growth: np.int64 = p.memory_info().rss - process_initial_memory
@@ -145,13 +153,148 @@ def grow_exp(df_to_grow: pd.DataFrame, num_times_xx2: int):
         df_to_grow = pd.concat([df_to_grow, df_prev])
     return df_to_grow
 
+def generate_big_dataframe(rows:int=1000000):
+    print("Generating big dataframe")
+    st = time.time()
+    df = get_sample_dataframe(rows)
+    df = grow_exp(df, 5)
+    print("Generation took :", time.time() - st)
+    return df
+
+def is_relevant(stack: Stack) -> bool:
+    """
+    function to decide what to filter out and not to count specific stackframes
+
+    Stack class variables:
+
+    filename: str
+        The source file being executed, or "???" if unknown.
+
+    function: str
+        The function being executed, or "???" if unknown.
+
+    lineno: int
+        The line number of the executing line, or 0 if unknown.
+    """
+    for frame in stack.frames:
+        # do something to check if we need this to be added
+        # as mem leak
+        # print(f"SAMPLE >>> {frame.filename}:{frame.function}[{frame.lineno}]")
+        pass
+    return True
+
+
+def construct_df_querybuilder_tests(size: int) -> pd.DataFrame:
+    df = get_sample_dataframe(size)
+    date_range = pd.date_range(start="2000-1-1", periods=size, freq="s")
+    df.index = pd.Index(date_range)
+    df.index.name = "timestamp"
+    return df
+
+
+def query_group() -> QueryBuilder:
+    """
+        groupby composite aggregation query for QueryBuilder memory tests.
+        The query basically will do aggregation of half of dataframe
+    """
+    q = QueryBuilder()
+    return (
+        q[q["bool"] == True]
+        .groupby("uint8")
+        .agg({"uint32": "mean", "int32": "sum", "strings": "count", "float64": "sum", "float32": "min", "int16": "max"})
+    )
+
+
+def query_group_bigger_result() -> QueryBuilder:
+    """
+        groupby composite aggregation query for QueryBuilder memory tests.
+        The query basically will do aggregation of whole dataframe
+    """
+    q = QueryBuilder()
+    return (
+        q[q["strings"] != "QASDFGH"]
+        .groupby("int16")
+        .agg({"uint32": "mean", 
+              "int32": "sum", 
+              "strings": "count", 
+              "float64": "sum", 
+              "float32": "min", 
+              "int16": "max"})
+    )
+
+
+def query_apply(str:str) -> QueryBuilder:
+    """
+        Apply query for QueryBuilder memory tests.
+        This version of apply does couple of nested operations
+        with columns that were just added
+    """
+    q = QueryBuilder()
+    q = q[q["strings"] != str].apply("NEW 1", q["uint32"] * q["int32"] / q["float32"])
+    q = q.apply("NEW 2", q["float32"] + q["float64"] + q["NEW 1"])
+    q = q.apply("NEW 3", q["NEW 2"] - q["NEW 1"] + q["timestamp"])
+    return q
+
+
+def query_resample() -> QueryBuilder:
+    """
+        Resample query for QueryBuilder memory tests
+    """
+    q = QueryBuilder()
+    return q.resample("min").agg(
+        {"int8" : "min",
+        "int16" : "max",
+        "int32" : "first",
+        "int64" : "last",
+        "uint64" : "sum",
+        "float32" : "mean",
+        "float64" : "sum",
+        "strings" : "count",
+        "bool" : "sum"}
+        )
+
+def query_row_range(size:int) -> QueryBuilder:
+    """
+        Row range query for QueryBuilder memory tests
+        Pass size of dataframe and it will generate random row range
+    """
+    q = QueryBuilder()
+    a = random.randint(0,size-1)
+    b = random.randint(0,size-1)
+    return q.row_range( (min(a,b), max(a,b)) )
+
+
+def query_date_range(start:pd.Timestamp, end: pd.Timestamp) -> QueryBuilder:
+    """
+        Date range query for QueryBuilder memory tests
+        Will generate random date range query based on given
+        start and end timestamps/dates
+    """
+    q = QueryBuilder()
+    a = gen_random_date(start,end)
+    b = gen_random_date(start,end)
+    return q.date_range( (min(a,b), max(a,b)) )
+
+
+def print_info(data: pd.DataFrame, q: QueryBuilder):
+    print("Query", q)
+    print("Size of DF returned by arctic:", data.shape[0], " columns:", data.shape[1])
+
+
+def gen_random_date(start:pd.Timestamp, end: pd.Timestamp):
+    """
+        Returns random timestamp from specified period
+    """
+    date_range = pd.date_range(start=start, end=end, freq='s') 
+    return random.choice(date_range)
+
+
 @pytest.mark.skipif(sys.platform == "win32", reason="Not enough storage on Windows runners")
 @pytest.mark.skipif(sys.platform == "darwin", reason="Problem on MacOs")
 def test_mem_leak_read_all_arctic_lib(arctic_library_lmdb):
     lib: adb.Library = arctic_library_lmdb
 
-    df = get_sample_dataframe(size=1000000)
-    df = grow_exp(df, 5)
+    df = generate_big_dataframe()
 
     symbol = "test"
     lib.write(symbol, df)
@@ -161,6 +304,7 @@ def test_mem_leak_read_all_arctic_lib(arctic_library_lmdb):
         del data
 
     df = lib.read(symbol).data
+    print(df)
     del df
 
     """
@@ -181,16 +325,58 @@ def test_mem_leak_read_all_arctic_lib(arctic_library_lmdb):
          run the test from command line again to assure it runs ok before commit 
 
     """
-    max_mem_bytes = 450_623_040
+    max_mem_bytes = 175_623_040
 
-    check_process_memory_leaks(proc_to_examine, 25, max_mem_bytes, 80.0)
+    check_process_memory_leaks(proc_to_examine, 20, max_mem_bytes, 80.0)
+
+def test_mem_leak_query_standard(arctic_library_lmdb):
+    """
+        This test uses old approach with iterations.
+        It is created for comparison with the new approach
+        with memray
+        (If memray is good in future we could drop the old approach)
+    """
+    lib: Library = arctic_library_lmdb
+
+    df = construct_df_querybuilder_tests(size=2000000)
+    size = df.shape[0]
+    start_date = df.iloc[0].name
+    end_date = df.iloc[0].name
+
+    symbol = "test"
+    lib.write(symbol, df)
+    del df
+    gc.collect()
+
+    queries = [query_group(), query_group_bigger_result(), query_apply(random_string(10)), query_resample()]
+
+    def proc_to_examine():
+        queries = [query_group(), 
+                    query_group_bigger_result(), 
+                    query_apply(random_string(10)), 
+                    query_resample(),
+                    query_row_range(size),
+                    query_date_range(start_date, end_date)]
+        for q in queries:
+            data: pd.DataFrame = lib.read(symbol, query_builder=q).data
+            print_info(data, q)
+            del data
+            gc.collect()
+
+    max_mem_bytes = 550_623_040
+
+    check_process_memory_leaks(proc_to_examine, 10, max_mem_bytes, 80.0)
+
+    del lib, queries
+    gc.collect()
+    time.sleep(10)
+
 
 @pytest.mark.skipif(sys.platform == "darwin", reason="Problem on MacOs")
 def test_mem_leak_read_all_native_store(lmdb_version_store_very_big_map):
     lib: NativeVersionStore = lmdb_version_store_very_big_map
 
-    df = get_sample_dataframe(size=1000000)
-    df = grow_exp(df, 5)
+    df = generate_big_dataframe()
 
     symbol = "test"
     lib.write(symbol, df)
@@ -200,6 +386,7 @@ def test_mem_leak_read_all_native_store(lmdb_version_store_very_big_map):
         del data
 
     df = lib.read(symbol).data
+    print(df)
     del df
 
     """ 
@@ -208,3 +395,115 @@ def test_mem_leak_read_all_native_store(lmdb_version_store_very_big_map):
     max_mem_bytes = 608_662_528
 
     check_process_memory_leaks(proc_to_examine, 20, max_mem_bytes, 80.0)
+
+
+symbol = "test"
+
+
+@pytest.fixture
+def library_with_symbol(arctic_library_lmdb) -> Generator[Tuple[Library, pd.DataFrame], None, None]:
+    """
+        As memray instruments memory, we need to take out 
+        everything not relevant from mem leak measurement out of 
+        test, so it works as less as possible
+    """
+    lib: Library = arctic_library_lmdb
+    df = construct_df_querybuilder_tests(size=2000000)
+    lib.write(symbol, df)
+    yield (lib, df)
+
+def mem_query(lib: Library, df: pd.DataFrame, num_repetitions:int=1):
+    """
+        This is the function where we test different types
+        of queries against a large dataframe. Later this
+        function will be used for memory limit and memory leaks 
+        tests
+    """
+    size = df.shape[0]
+    start_date = df.iloc[0].name
+    end_date = df.iloc[0].name
+
+    symbol = "test"
+    lib.write(symbol, df)
+    del df
+    gc.collect()
+
+    queries = []
+
+    queries = [query_group(), 
+                query_group_bigger_result(), 
+                query_apply(random_string(10)), 
+                query_resample(),
+                query_row_range(size),
+                query_date_range(start_date, end_date)]
+    
+    for rep in range(num_repetitions):
+        for q in queries:
+            data: pd.DataFrame = lib.read(symbol, query_builder=q).data
+            lib.read_batch
+            print_info(data, q)
+            del data
+            gc.collect()
+
+    del lib, queries
+    gc.collect()
+    time.sleep(10)
+
+
+@pytest.mark.limit_leaks(location_limit="25 KB", filter_fn=is_relevant)
+def test_mem_leak_querybuilder_memray(library_with_symbol):
+    """
+        Test to capture memory leaks >= of specified number
+
+        NOTE: wec could filter out not meaningful for us stackframes
+        in future if something outside of us start to leak using
+        the argument "filter_fn" - just add to the filter function
+        what we must exclude from calculation
+    """
+    (lib, df) = library_with_symbol
+    mem_query(lib, df)
+
+
+@pytest.mark.limit_memory("490 MB")
+def test_mem_limit_querybuilder_memray(library_with_symbol):
+    """
+        The fact that we do not leak memory does not mean that we
+        are efficient on memory usage. This test captures the memory usage
+        and limits it, so that we do not go over it (unless fro good reason)
+        Thus if the test fails then perhaps we are using now more memory than
+        in the past
+    """
+    (lib, df) = library_with_symbol
+    mem_query(lib, df)
+
+@pytest.fixture
+def library_with_big_symbol(arctic_library_lmdb) -> Generator[Library, None, None]:
+    """
+        As memray instruments memory, we need to take out 
+        everything not relevant from mem leak measurement out of 
+        test, so it works as less as possible
+    """
+    lib: Library = arctic_library_lmdb
+    df = generate_big_dataframe(300000)
+    lib.write(symbol, df)
+    del df
+    yield lib
+
+
+@pytest.mark.limit_leaks(location_limit="30 KB", filter_fn=is_relevant)
+def test_mem_leak_read_all_arctic_lib_memray(library_with_big_symbol):
+    """
+        This is a new version of the initial test that reads the whole
+        big dataframe in memory
+    """
+
+    print("Test starting")
+    st = time.time()
+    lib: Library = library_with_big_symbol
+    data = lib.read(symbol).data
+    del data
+    print("Test took :", time.time() - st)
+
+    print("Sleeping for 10 secs")
+    gc.collect()
+    time.sleep(10)
