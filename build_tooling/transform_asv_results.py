dd09029241e38600de6a538339a388132ebf0def
G-D-Petrov <petrov.d.george@gmail.com>
2024-12-12 17:53:35 +0200

    Fix ASV problems

--- build_tooling/transform_asv_results.py
+++ build_tooling/transform_asv_results.py
@@ -5,7 +5,9 @@ Use of this software is governed by the Business Source License 1.1 included in 
 
 As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
 """
+
 import pandas as pd
+from numpy import inf
 from arcticdb.storage_fixtures.s3 import real_s3_from_environment_variables
 import json
 from pathlib import Path
