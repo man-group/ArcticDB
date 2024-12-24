5beee7c611d21ad9c65f52918f03fb2cfe962449
Alex Seaton <alexbseaton@gmail.com>
2024-12-24 12:43:19 +0000

    Fix race between list_versions and delete_snapshot on NFS 8104588520 (#2092)
    See arcticc PR #1170 for more context and discussion.
    
    The keys in the `KeysNotFoundException` were not having
    `unencode_object_id` applied to them, so in the snapshot iteration we
    were incorrectly thinking they were different to the snapshot key being
    iterated over (which has had the unencoding applied).

--- python/tests/integration/arcticdb/test_s3.py
+++ python/tests/integration/arcticdb/test_s3.py
@@ -5,14 +5,17 @@ Use of this software is governed by the Business Source License 1.1 included in 
 
 As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
 """
+import re
+import time
+from multiprocessing import Queue, Process
 
 import pytest
 import pandas as pd
+from arcticdb.util.test import create_df
 
-from arcticdb import Arctic
 from arcticdb_ext.exceptions import StorageException
 from arcticdb_ext import set_config_string
-from arcticdb.storage_fixtures.s3 import MotoS3StorageFixtureFactory
+from arcticdb.storage_fixtures.s3 import MotoS3StorageFixtureFactory, MotoNfsBackedS3StorageFixtureFactory
 
 
 def test_s3_storage_failures(mock_s3_store_with_error_simulation):
@@ -48,3 +51,94 @@ def test_s3_running_on_aws_fast_check(lib_name, s3_storage_factory, run_on_aws):
         assert lib_tool.inspect_env_variable("AWS_EC2_METADATA_DISABLED") == None
     else:
         assert lib_tool.inspect_env_variable("AWS_EC2_METADATA_DISABLED") == "true"
+
+
+def test_nfs_backed_s3_storage(lib_name, nfs_backed_s3_storage):
+    # Given
+    lib = nfs_backed_s3_storage.create_version_store_factory(lib_name)()
+
+    # When
+    lib.write("s", data=create_df())
+
+    # Then - should be written in "bucketized" structure
+    bucket = nfs_backed_s3_storage.get_boto_bucket()
+    objects = bucket.objects.all()
+
+    # Expect one or two repetitions of 3 digit "buckets" in the object names
+    bucketized_pattern = r".*/(sl|tdata|tindex|ver|vref)/([0-9]{1,3}/){1,2}.*"
+
+    for o in objects:
+        assert re.match(bucketized_pattern, o.key), f"Object {o.key} does not match pattern {bucketized_pattern}"
+
+
+def read_repeatedly(version_store, queue: Queue):
+    while True:
+        try:
+            version_store.list_versions("tst")
+        except Exception as e:
+            queue.put(e)
+            raise
+        time.sleep(0.1)
+
+
+def write_repeatedly(version_store):
+    while True:
+        for i in range(10):
+            version_store.snapshot(f"snap-{i}")
+        for i in range(10):
+            version_store.delete_snapshot(f"snap-{i}")
+        time.sleep(0.1)
+
+
+def test_racing_list_and_delete_nfs(nfs_backed_s3_storage, lib_name):
+    """This test is for a regression with NFS where iterating snapshots raced with
+    deleting them, due to a bug in our logic to suppress the KeyNotFoundException."""
+    lib = nfs_backed_s3_storage.create_version_store_factory(lib_name)()
+    lib.write("tst", [1, 2, 3])
+
+    exceptions_in_reader = Queue()
+    reader = Process(target=read_repeatedly, args=(lib, exceptions_in_reader))
+    writer = Process(target=write_repeatedly, args=(lib,))
+
+    try:
+        reader.start()
+        writer.start()
+
+        # Run test for 2 seconds - this was enough for this regression test to reliably fail
+        # 10 times in a row.
+        reader.join(2)
+        writer.join(0.001)
+    finally:
+        writer.terminate()
+        reader.terminate()
+
+    assert exceptions_in_reader.empty()
+
+
+@pytest.fixture(scope="function", params=[MotoNfsBackedS3StorageFixtureFactory, MotoS3StorageFixtureFactory])
+def s3_storage_dots_in_path(request):
+    prefix = "some_path/.thing_with_a_dot/even.more.dots/end"
+
+    with request.param(
+            use_ssl=False,
+            ssl_test_support=False,
+            bucket_versioning=False,
+            default_prefix=prefix,
+            use_raw_prefix=True
+    ) as f:
+        with f.create_fixture() as g:
+            yield g
+
+
+def test_read_path_with_dot(lib_name, s3_storage_dots_in_path):
+    # Given
+    factory = s3_storage_dots_in_path.create_version_store_factory(lib_name)
+    lib = factory()
+
+    # When
+    expected = create_df()
+    lib.write("s", data=expected)
+
+    # Then - should be readable
+    res = lib.read("s").data
+    pd.testing.assert_frame_equal(res, expected)
