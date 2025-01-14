110adac1b8cc3622a0aef34e276fa525dd9f6e1c
Georgi Rusev <Georgi Rusev>
2025-01-14 18:23:03 +0200

    fixes for code review

--- python/utils/asv_checks.py
+++ python/utils/asv_checks.py
@@ -39,9 +39,10 @@ def run_command(command: List[str], cwd: str, ok_errors_list: List[str] = None) 
         if not ok_errors_list is None:
             for ok_error in ok_errors_list:
                 err_output.replace(ok_error, "")
-                err_output = re.sub(r'\s+', '', err_output)
-            if err_output == "":
-                error_code = 0
+                err_output = err_output.strip()
+                if err_output == "":
+                    error_code = 0
+                    break
             else:
                 error(f"Unknown errors: {err_output}" )
 
@@ -66,7 +67,7 @@ def file_unchanged(filepath, last_check_time):
 
 def get_project_root():
     file_location = os.path.abspath(__file__)
-    return file_location.split("/python/")[0]
+    return file_location.split("/python/benchmarks")[0]
 
 
 def perform_asv_checks() -> int:
@@ -81,8 +82,8 @@ def perform_asv_checks() -> int:
     sys.path.insert(0,f"{path}/python")
     sys.path.insert(0,f"{path}/python/tests")
 
-    bencmark_config = f"{path}/python/.asv/results/benchmarks.json"
-    orig_hash = compute_file_hash(bencmark_config)
+    benchmark_config = f"{path}/python/.asv/results/benchmarks.json"
+    orig_hash = compute_file_hash(benchmark_config)
 
     print("_" * 80)
     print("""IMPORTANT: The tool checks CURRENT versions of asv tests along with asv.conf.json")
@@ -90,7 +91,7 @@ def perform_asv_checks() -> int:
             they would need to be in order for completion of current PR""")
     print("_" * 80)
 
-    print("\n\nCheck 1: Executing check for python cod of asv tests")
+    print("\n\nCheck 1: Executing check for python code of asv tests")
     if run_command(["asv", "check", "--python=same"], path) != 0:
         error("Please address all reported errors and submit code in the PR.")
         err = 1
@@ -105,11 +106,11 @@ def perform_asv_checks() -> int:
         error("There was error getting latest benchmarks. See log")        
         err = 1
     else: 
-        if compute_file_hash(bencmark_config) == orig_hash:
+        if compute_file_hash(benchmark_config) == orig_hash:
             print("Great, there are no new versions of asv test either!")
         else:
             error(f"""\n\n There are changes in asv test versions. 
-Open file {bencmark_config} compare with previous version and             
+Open file {benchmark_config} compare with previous version and             
 make sure you submit the file  in git repo""")
             err = 1
 
