8502cbd849a95c2d342c519e931a41c211e34c51
grusev <george_rusev@yahoo.com>
2025-01-15 19:05:25 +0200

    Prechecks for asv (#2107)
    #### Reference Issues/PRs
    <!--Example: Fixes #1234. See also #3456.-->
    
    #### What does this implement or fix?
    
    There are couple of checks that need to be done before making sure asv
    benchmark tests are ok to be merged:
    
    Check that code of tests is ok and can run
    > asv check --python=same
    
    Checks that versions of benchmark tests are up to date in asv,conf.json
    > asv run --bench just-discover --python=same
    
    this PR will prepare a script which can be run by anyone before making
    PR for review as well as github action that will execute it to confirm
    all is ok
    
    A new python utility is added to do the required checks. Usage:
    > python python/utils/asv_checks.py 
    
    This tool now can be used in a github action to do automatic check
    
    Successfull check of Job:
    https://github.com/man-group/ArcticDB/actions/runs/12713349006/job/35441035657
    
    A job failed because benchmark.json not up to date:
    https://github.com/man-group/ArcticDB/actions/runs/12711972129/job/35436505808
    
    NOTE: 
    
    1. The most efficien way to do ASV check is on your machine wither by
    executing the both above mentioned commands or the script python
    python/utils/asv_checks.py (better)
    
    2. On github currently the workflow is not efficient as it does approx
    35min build and the actual check is just 5 secs afterwards. Why it is
    not efficient?
    - it needs build. and as there is no build repository of previous jobs
    like build job the build has to be repeated here
    - this cannot be combines with ASV tests. The asv tests will do specific
    build for their own purpose and run the tests. The build AND the test
    are one atomic process thus we cannot plugin there and even if we plugin
    the inefficiency remains in terms of time
     
     
    
    #### Any other comments?
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>
    
    <!--
    Thanks for contributing a Pull Request to ArcticDB! Please ensure you
    have taken a look at:
    - ArcticDB's Code of Conduct:
    https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
    - ArcticDB's Contribution Licensing:
    https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
    -->
    
    ---------
    
    Co-authored-by: Georgi Rusev <Georgi Rusev>

---
+++ python/utils/asv_checks.py
@@ -0,0 +1,133 @@
+import hashlib
+import logging
+import os
+import re
+import subprocess
+import sys
+from typing import List
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("ASV Linter")
+
+def error(mes):
+    logger.error("-" * 80)
+    logger.error(f"ERROR :{mes}")
+    logger.error("-" * 80)
+
+
+def run_command(command: List[str], cwd: str, ok_errors_list: List[str] = None) -> int:
+    """
+        executes a command in specified directory.
+        if 'ok_error' passed, the string will be searched in stderr
+        and if found will not mark execution as error but count as ok
+    """
+    result = subprocess.run(command, capture_output=True, text=True, cwd=cwd)
+
+    output = result.stdout
+    err_output = result.stderr
+    error_code = result.returncode
+
+    if err_output is not None:
+        error(err_output)
+        if error_code == 0:
+            logger.info("ABOVE ERRORS DO NOT AFFECT FINAL ERROR CODE = 0")
+
+    if output is not None:
+        logger.info("Standard Output:")
+        logger.info(output)
+
+    if error_code != 0:
+        logger.error(f"Error Code Returned: {error_code}") 
+        if ok_errors_list is not None:
+            for ok_error in ok_errors_list:
+                err_output.replace(ok_error, "")
+                err_output = err_output.strip()
+                if err_output == "":
+                    error_code = 0
+                    break
+            else:
+                error(f"Unknown errors: {err_output}" )
+
+    return error_code
+
+def compute_file_hash(file_path): 
+    """Compute the SHA-256 hash of the given file.""" 
+    sha256_hash = hashlib.sha256() 
+    with open(file_path, "rb") as f: 
+        for byte_block in iter(lambda: f.read(4096), b""): 
+            sha256_hash.update(byte_block) 
+    
+    return sha256_hash.hexdigest()
+
+def file_unchanged(filepath, last_check_time):
+  try:
+    modified_time = os.path.getmtime(filepath)
+    return modified_time == last_check_time
+  except FileNotFoundError:
+    return False
+
+
+def get_project_root():
+    file_location = os.path.abspath(__file__)
+    return file_location.split("/python/utils")[0]
+
+
+def perform_asv_checks() -> int:
+    """
+    Performs several checks and returns result 0 if no problem or 1 if there are problems
+    """
+
+    err = 0
+
+    path = get_project_root()
+
+    sys.path.insert(0,f"{path}/python")
+    sys.path.insert(0,f"{path}/python/tests")
+
+    benchmark_config = f"{path}/python/.asv/results/benchmarks.json"
+    orig_hash = compute_file_hash(benchmark_config)
+
+    logger.info("_" * 80)
+    logger.info("""IMPORTANT: The tool checks CURRENT ACTUAL versions of asv benchmark tests along with the one in benchmarks.json file.
+            That means that if there are files that are not submitted yet (tests and benchmark.json),
+            they would need to be in order for completion of current PR.
+            benchmarks.json is updated with a version number calculated as a hash
+            of the python test method. Thus any change of this method triggers different
+            version. Hence you would need to update json file also.
+            It happens automatically if you run following commandline:
+             > asv run --bench just-discover --python=same  """)
+    logger.info("_" * 80)
+
+    logger.info("\n\nCheck 1: Executing check for python code of asv tests")
+    if run_command(["asv", "check", "--python=same"], path) != 0:
+        error("Please address all reported errors and submit code in the PR.")
+        err = 1
+    else:
+        logger.info("Relax, no worries. Code is fine!")
+
+
+    logger.info("\n\nCheck 2: Check that benchmarks.json has up to date latest versions of tests.")
+    if run_command(command = ["asv", "run", "--bench", "just-discover", "--python=same"], 
+                   cwd = path, 
+                   ok_errors_list = ["Couldn't load asv.plugins._mamba_helpers"]) != 0:
+        error("There was error getting latest benchmarks. See log")        
+        err = 1
+    else: 
+        new_hash = compute_file_hash(benchmark_config)
+        if new_hash == orig_hash:
+            logger.info("Great, there are no new versions of asv test either!")
+        else:
+            error(f"""\n\n There are changes in asv test versions. 
+Open file {benchmark_config} compare with previous version and             
+make sure you submit the file  in git repo""")
+            err = 1
+
+    return err
+
+res = perform_asv_checks()
+if res != 0:
+    error("Errors detected - check output above")
+    sys.exit(res)
+else: 
+    logger.info("SUCCESS! All checks pass")
