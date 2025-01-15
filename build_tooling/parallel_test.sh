630d16e65b0eef9bbdbda02efe3ef33c4a9942ad
phoebusm <phoebusmak@gmail.com>
2025-01-15 10:47:46 +0000

    Test

--- build_tooling/parallel_test.sh
+++ build_tooling/parallel_test.sh
@@ -18,7 +18,7 @@ cd $PARALLEL_TEST_ROOT
 
 export ARCTICDB_RAND_SEED=$RANDOM
 
-$catch python -m pytest --timeout=3600 $PYTEST_XDIST_MODE -v --log-file="$TEST_OUTPUT_DIR/pytest-logger.$group.log" \
+$catch python -m pytest --timeout=3600 $PYTEST_XDIST_MODE -vs --log-file="$TEST_OUTPUT_DIR/pytest-logger.$group.log" \
     --junitxml="$TEST_OUTPUT_DIR/pytest.$group.xml" \
     --basetemp="$PARALLEL_TEST_ROOT/temp-pytest-output" \
     "$@" 2>&1 | sed -ur "s#^(tests/.*/([^/]+\.py))?#\2#"
