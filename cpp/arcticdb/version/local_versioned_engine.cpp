837299eb6ac4feeabae7854cd8885de74a93c333
Vasil Pashov <vasil.pashov1@gmail.com>
2025-01-23 15:38:31 +0200

    Improve exception message for missing symbol on update. Mention the upsert parameter

--- cpp/arcticdb/version/local_versioned_engine.cpp
+++ cpp/arcticdb/version/local_versioned_engine.cpp
@@ -1447,7 +1447,11 @@ std::vector<std::variant<VersionedItem, DataError>> LocalVersionedEngine::batch_
                         dynamic_schema,
                         empty_types);
                 } else {
-                    missing_data::check<ErrorCode::E_NO_SUCH_VERSION>(upsert, "Cannot update non-existent symbol {}", stream_id);
+                    missing_data::check<ErrorCode::E_NO_SUCH_VERSION>(
+                        upsert,
+                        "Cannot update non-existent symbol {}."
+                        "Using \"upsert=True\" will create create the symbol instead of throwing this exception.",
+                        stream_id);
                     index_key_fut = async_write_dataframe_impl(
                         store(),
                         0,
