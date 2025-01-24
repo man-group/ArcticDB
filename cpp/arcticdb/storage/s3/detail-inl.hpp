059477404ce08d74492c10393db9e850ba6ad154
Ivo Dilov <iddilov@gmail.com>
2025-01-24 18:28:18 +0200

    Adds a BAD_REQUEST exception type
    Bad requests like `XAmzContentSHA256Mismatch` can happen from the sdk
    and it usually means we are not using the aws-sdk correctly. One recent
    such example was forking an s3 library and using the same s3 client from
    multiple processes which resulted in sending invalid requests to S3.

--- cpp/arcticdb/storage/s3/detail-inl.hpp
+++ cpp/arcticdb/storage/s3/detail-inl.hpp
@@ -79,6 +79,13 @@ using Range = folly::Range<It>;
             raise<ErrorCode::E_NOT_IMPLEMENTED_BY_STORAGE>(
                     fmt::format("Operation is not implemented for storage: {}", error_message_suffix));
         }
+
+        if (err.GetResponseCode() == Aws::Http::HttpResponseCode::BAD_REQUEST) {
+            raise<ErrorCode::E_BAD_REQUEST>(
+                    fmt::format("Aws-sdk sent a bad request to S3. This could be due to improper use of the sdk or due "
+                                "to using the S3Client in parallel from forked processes. Error message: {}",
+                                error_message_suffix));
+        }
     }
 
     if (err.ShouldRetry()) {
