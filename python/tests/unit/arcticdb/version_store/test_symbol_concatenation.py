4b4cad63ef7154e363c0da102b233b924adefeb4
Alex Owens <alexowens90@gmail.com>
2025-01-27 10:48:18 +0000

    Simplify column slicing test

--- python/tests/unit/arcticdb/version_store/test_symbol_concatenation.py
+++ python/tests/unit/arcticdb/version_store/test_symbol_concatenation.py
@@ -40,7 +40,8 @@ def test_symbol_concat_basic(lmdb_library_factory):
 
 
 def test_symbol_concat_column_sliced(lmdb_library_factory):
-    lib = lmdb_library_factory(LibraryOptions(columns_per_segment=2))
+    # lib = lmdb_library_factory(LibraryOptions(columns_per_segment=2))
+    lib = lmdb_library_factory()
     df_1 = pd.DataFrame(
         {
             "col1": np.arange(3, dtype=np.int64),
@@ -55,30 +56,14 @@ def test_symbol_concat_column_sliced(lmdb_library_factory):
             "col2": np.arange(20, 24, dtype=np.int64),
             "col3": np.arange(210, 214, dtype=np.int64),
         },
-        index=pd.date_range(pd.Timestamp(2000), freq="1000ns", periods=4)
-    )
-    df_3 = pd.DataFrame(
-        {
-            "col1": np.arange(5, dtype=np.int64),
-            "col2": np.arange(30, 35, dtype=np.int64),
-            "col3": np.arange(310, 315, dtype=np.int64),
-        },
-        index=pd.date_range(pd.Timestamp(6000), freq="1000ns", periods=5)
+        index=pd.date_range(pd.Timestamp(3000), freq="1000ns", periods=4)
     )
     lib.write("sym1", df_1)
     lib.write("sym2", df_2)
-    lib.write("sym3", df_3)
 
-    lazy_df_1 = lib.read("sym1", lazy=True)
-    lazy_df_2 = lib.read("sym2", lazy=True)
-    lazy_df_2 = lazy_df_2.date_range((pd.Timestamp(pd.Timestamp(3000)), None))
-    lazy_df_3 = lib.read("sym3", lazy=True)
-
-    lazy_df = concat([lazy_df_1, lazy_df_2, lazy_df_3])
-
+    lazy_df = concat(lib.read_batch(["sym1", "sym2"], lazy=True))
     lazy_df.resample("2000ns").agg({"col1": "sum", "col2": "mean", "col3": "min"})
-
     received = lazy_df.collect().data
     received = received.reindex(columns=sorted(received.columns))
-    expected = pd.concat([df_1, df_2.iloc[1:], df_3]).resample("2000ns").agg({"col1": "sum", "col2": "mean", "col3": "min"})
+    expected = pd.concat([df_1, df_2]).resample("2000ns").agg({"col1": "sum", "col2": "mean", "col3": "min"})
     assert_frame_equal(expected, received)
