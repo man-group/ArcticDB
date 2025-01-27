096ff095b8e868b0dbe5c90cb2ffbb1d580b953f
Alex Owens <alexowens90@gmail.com>
2025-01-27 10:40:38 +0000

    Added failing test with column slicing

--- python/tests/unit/arcticdb/version_store/test_symbol_concatenation.py
+++ python/tests/unit/arcticdb/version_store/test_symbol_concatenation.py
@@ -10,14 +10,14 @@ import pandas as pd
 import pytest
 
 from arcticdb import col, concat, LazyDataFrame, LazyDataFrameCollection, QueryBuilder, ReadRequest
+from arcticdb.options import LibraryOptions
 from arcticdb.util.test import assert_frame_equal
-
 
 pytestmark = pytest.mark.pipeline
 
 
-def test_symbol_concat_basic(lmdb_library):
-    lib = lmdb_library
+def test_symbol_concat_basic(lmdb_library_factory):
+    lib = lmdb_library_factory()
     df_1 = pd.DataFrame({"col": np.arange(3, dtype=np.int64)}, index=pd.date_range(pd.Timestamp(0), freq="1000ns", periods=3))
     df_2 = pd.DataFrame({"col": np.arange(4, dtype=np.int64)}, index=pd.date_range(pd.Timestamp(2000), freq="1000ns", periods=4))
     df_3 = pd.DataFrame({"col": np.arange(5, dtype=np.int64)}, index=pd.date_range(pd.Timestamp(6000), freq="1000ns", periods=5))
@@ -38,3 +38,47 @@ def test_symbol_concat_basic(lmdb_library):
     expected = pd.concat([df_1, df_2.iloc[1:], df_3]).resample("2000ns").agg({"col": "sum"})
     assert_frame_equal(expected, received)
 
+
+def test_symbol_concat_column_sliced(lmdb_library_factory):
+    lib = lmdb_library_factory(LibraryOptions(columns_per_segment=2))
+    df_1 = pd.DataFrame(
+        {
+            "col1": np.arange(3, dtype=np.int64),
+            "col2": np.arange(10, 13, dtype=np.int64),
+            "col3": np.arange(110, 113, dtype=np.int64),
+        },
+        index=pd.date_range(pd.Timestamp(0), freq="1000ns", periods=3)
+    )
+    df_2 = pd.DataFrame(
+        {
+            "col1": np.arange(4, dtype=np.int64),
+            "col2": np.arange(20, 24, dtype=np.int64),
+            "col3": np.arange(210, 214, dtype=np.int64),
+        },
+        index=pd.date_range(pd.Timestamp(2000), freq="1000ns", periods=4)
+    )
+    df_3 = pd.DataFrame(
+        {
+            "col1": np.arange(5, dtype=np.int64),
+            "col2": np.arange(30, 35, dtype=np.int64),
+            "col3": np.arange(310, 315, dtype=np.int64),
+        },
+        index=pd.date_range(pd.Timestamp(6000), freq="1000ns", periods=5)
+    )
+    lib.write("sym1", df_1)
+    lib.write("sym2", df_2)
+    lib.write("sym3", df_3)
+
+    lazy_df_1 = lib.read("sym1", lazy=True)
+    lazy_df_2 = lib.read("sym2", lazy=True)
+    lazy_df_2 = lazy_df_2.date_range((pd.Timestamp(pd.Timestamp(3000)), None))
+    lazy_df_3 = lib.read("sym3", lazy=True)
+
+    lazy_df = concat([lazy_df_1, lazy_df_2, lazy_df_3])
+
+    lazy_df.resample("2000ns").agg({"col1": "sum", "col2": "mean", "col3": "min"})
+
+    received = lazy_df.collect().data
+    received = received.reindex(columns=sorted(received.columns))
+    expected = pd.concat([df_1, df_2.iloc[1:], df_3]).resample("2000ns").agg({"col1": "sum", "col2": "mean", "col3": "min"})
+    assert_frame_equal(expected, received)
