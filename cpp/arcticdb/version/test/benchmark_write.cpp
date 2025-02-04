5f6d3203c306bfd0f833553900ccf9533cc3010d
Ivo Dilov <iddilov@gmail.com>
2025-02-04 11:41:43 +0200

    Benchmark cloning vs writing a `Segment`
    Adds a new set of benchmarks which compare the cost of cloning a
    `Segment` vs writing it to LMDB (or to memory).
    
    Results from a release build point to not a big impact on performance of
    cloning with respect to writing to local SSD LMDB.
    
    ```
    ---------------------------------------------------------------------------------------
    Benchmark                                             Time             CPU   Iterations
    ---------------------------------------------------------------------------------------
    BM_write_lmdb/mock_with_clone/100000_mean         0.853 ms        0.853 ms           10
    BM_write_lmdb/mock_with_clone/100000_median       0.817 ms        0.817 ms           10
    BM_write_lmdb/mock_with_clone/100000_stddev       0.082 ms        0.082 ms           10
    BM_write_lmdb/mock_with_clone/100000_cv            9.65 %          9.65 %            10
    BM_write_lmdb/mock_with_clone/1000000_mean         5.02 ms         5.02 ms           10
    BM_write_lmdb/mock_with_clone/1000000_median       4.60 ms         4.60 ms           10
    BM_write_lmdb/mock_with_clone/1000000_stddev      0.960 ms        0.960 ms           10
    BM_write_lmdb/mock_with_clone/1000000_cv          19.12 %         19.12 %            10
    BM_write_lmdb/mock_no_clone/100000_mean           0.005 ms        0.005 ms           10
    BM_write_lmdb/mock_no_clone/100000_median         0.005 ms        0.005 ms           10
    BM_write_lmdb/mock_no_clone/100000_stddev         0.001 ms        0.001 ms           10
    BM_write_lmdb/mock_no_clone/100000_cv             16.94 %         17.65 %            10
    BM_write_lmdb/mock_no_clone/1000000_mean          0.016 ms        0.015 ms           10
    BM_write_lmdb/mock_no_clone/1000000_median        0.015 ms        0.014 ms           10
    BM_write_lmdb/mock_no_clone/1000000_stddev        0.002 ms        0.002 ms           10
    BM_write_lmdb/mock_no_clone/1000000_cv            14.69 %         15.76 %            10
    BM_write_lmdb/real_with_clone/100000_mean          5.39 ms         2.01 ms           10
    BM_write_lmdb/real_with_clone/100000_median        5.21 ms         1.98 ms           10
    BM_write_lmdb/real_with_clone/100000_stddev       0.652 ms        0.075 ms           10
    BM_write_lmdb/real_with_clone/100000_cv           12.10 %          3.71 %            10
    BM_write_lmdb/real_with_clone/1000000_mean         35.1 ms         25.1 ms           10
    BM_write_lmdb/real_with_clone/1000000_median       34.6 ms         24.6 ms           10
    BM_write_lmdb/real_with_clone/1000000_stddev      0.978 ms        0.958 ms           10
    BM_write_lmdb/real_with_clone/1000000_cv           2.79 %          3.81 %            10
    BM_write_lmdb/real_no_clone/100000_mean            6.83 ms         2.45 ms           10
    BM_write_lmdb/real_no_clone/100000_median          6.47 ms         2.30 ms           10
    BM_write_lmdb/real_no_clone/100000_stddev          1.03 ms        0.401 ms           10
    BM_write_lmdb/real_no_clone/100000_cv             15.02 %         16.37 %            10
    BM_write_lmdb/real_no_clone/1000000_mean           36.9 ms         22.1 ms           10
    BM_write_lmdb/real_no_clone/1000000_median         35.6 ms         20.8 ms           10
    BM_write_lmdb/real_no_clone/1000000_stddev         3.73 ms         2.43 ms           10
    BM_write_lmdb/real_no_clone/1000000_cv            10.12 %         10.99 %            10
    ```

---
+++ cpp/arcticdb/version/test/benchmark_write.cpp
@@ -0,0 +1,81 @@
+#include <random>
+
+#include <benchmark/benchmark.h>
+
+#include <arcticdb/codec/codec.hpp>
+#include <arcticdb/storage/storage.hpp>
+#include <arcticdb/storage/lmdb/lmdb_storage.hpp>
+#include <arcticdb/storage/test/common.hpp>
+
+using namespace arcticdb;
+
+inline const fs::path TEST_DATABASES_PATH = "./test_databases";
+const std::string TEST_LIB_NAME = "test_lib";
+const uint64_t MAP_SIZE = 128ULL * (1ULL << 20); // 128 MiB
+struct LMDBStore {
+
+    std::unique_ptr<storage::lmdb::LmdbStorage> storage;
+
+    LMDBStore(bool use_mock) {
+        setup();
+
+        arcticdb::proto::lmdb_storage::Config cfg;
+
+        cfg.set_path((TEST_DATABASES_PATH / "test_lmdb").generic_string());
+        cfg.set_map_size(MAP_SIZE);
+        cfg.set_recreate_if_exists(true);
+        cfg.set_use_mock_storage_for_testing(use_mock);
+
+        arcticdb::storage::LibraryPath library_path(TEST_LIB_NAME, '/');
+
+        storage = std::make_unique<arcticdb::storage::lmdb::LmdbStorage>(library_path, arcticdb::storage::OpenMode::DELETE, cfg);
+    }
+
+    ~LMDBStore() {
+        clear();
+    }
+
+    void setup() {
+        if (!fs::exists(TEST_DATABASES_PATH)) {
+            fs::create_directories(TEST_DATABASES_PATH);
+        }
+    }
+
+    void clear() {
+        if (fs::exists(TEST_DATABASES_PATH)) {
+            fs::remove_all(TEST_DATABASES_PATH);
+        }
+    }
+};
+
+static void BM_write_lmdb(benchmark::State& state, bool include_clone_time, bool use_mock) {
+    auto num_rows = state.range(0);
+
+    auto sym = "symbol";
+    auto key = RefKey(sym, KeyType::APPEND_REF);
+    auto segment_in_memory = get_test_timeseries_frame(sym, num_rows, 0).segment_;
+    auto codec_opts = proto::encoding::VariantCodec();
+    auto segment = encode_dispatch(std::move(segment_in_memory), codec_opts, arcticdb::EncodingVersion::V2);
+
+    auto lmdb = LMDBStore(use_mock);
+
+    for (auto _ : state) {
+        if (!include_clone_time) {
+            state.PauseTiming();
+        }
+        auto segment_copy = segment.clone();
+        auto key_segment_pair_copy = storage::KeySegmentPair(key, std::move(segment_copy));
+        if (!include_clone_time) {
+            state.ResumeTiming();
+        }
+        lmdb.storage->write(std::move(key_segment_pair_copy));
+        state.PauseTiming();
+        lmdb.storage->remove(key, storage::RemoveOpts{});
+        state.ResumeTiming();
+    }
+}
+
+BENCHMARK_CAPTURE(BM_write_lmdb, mock_with_clone, true, true)->Arg(100'000)->Arg(1'000'000);
+BENCHMARK_CAPTURE(BM_write_lmdb, mock_no_clone, false, true)->Arg(100'000)->Arg(1'000'000);
+BENCHMARK_CAPTURE(BM_write_lmdb, real_with_clone, true, false)->Arg(100'000)->Arg(1'000'000);
+BENCHMARK_CAPTURE(BM_write_lmdb, real_no_clone, false, false)->Arg(100'000)->Arg(1'000'000);
