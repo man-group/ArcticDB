78733922669706c7335f8b908bda8a0d820c9e03
William Dealtry <william.dealtry@gmail.com>
2025-03-03 10:32:44 +0000

    Fix size issue

--- cpp/arcticdb/util/dump_bytes.hpp
+++ cpp/arcticdb/util/dump_bytes.hpp
@@ -12,23 +12,25 @@
 
 // based on this: https://codereview.stackexchange.com/questions/165120/printing-hex-dumps-for-diagnostics
 namespace arcticdb {
-inline std::ostream &hex_dump(std::ostream &os, const void *buffer,
-                       std::size_t buf_size, bool show_printable_chars = true) {
-    if (buffer == nullptr) {
+inline std::ostream &hex_dump(
+        std::ostream &os, const void *buffer,
+        std::size_t buf_size,
+        bool show_printable_chars = true) {
+    if (buffer == nullptr)
         return os;
-    }
+
     auto old_format = os.flags();
     auto old_fill_char = os.fill();
     constexpr std::size_t max_line{8};
+
     // create a place to store text version of string
     char render_string[max_line + 1];
     char *rsptr{render_string};
+
     // convenience cast
     const unsigned char *buf{reinterpret_cast<const unsigned char *>(buffer)};
-
     for (std::size_t line_count = max_line; buf_size; --buf_size, ++buf) {
-        os << std::setw(2) << std::setfill('0') << std::hex
-           << static_cast<unsigned>(*buf) << ' ';
+        os << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned>(*buf) << ' ';
         *rsptr++ = std::isprint(*buf) ? *buf : '.';
         if (--line_count == 0) {
             *rsptr++ = '\0';  // terminate string
@@ -40,6 +42,7 @@ inline std::ostream &hex_dump(std::ostream &os, const void *buffer,
             line_count = std::min(max_line, buf_size);
         }
     }
+
     // emit newline if we haven't already
     if (rsptr != render_string) {
         if (show_printable_chars) {
@@ -56,7 +59,7 @@ inline std::ostream &hex_dump(std::ostream &os, const void *buffer,
     return os;
 }
 
-inline std::string dump_bytes(const void* data, size_t size, size_t max_size = 20u) {
+[[nodiscard]] inline std::string dump_bytes(const void* data, size_t size, size_t max_size = 20u) {
     const auto sz = std::min(max_size, size);
     std::ostringstream strm;
     hex_dump(strm, data, sz);
