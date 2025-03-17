b83ab4b1cbebffbe5fd5981a5c009c6f31c67c4e
Georgi Rusev <Georgi Rusev>
2025-03-17 15:17:00 +0200

    fixed regression

--- python/benchmarks/real_list_operations.py
+++ python/benchmarks/real_list_operations.py
@@ -7,7 +7,7 @@ As of the Change Date specified in that file, in accordance with the Business So
 """
 
 import time
-from arcticdb.util.environment_setup import LibraryManager, LibraryPopulationPolicy, LibraryType, Storage, populate_library_if_missing
+from arcticdb.util.environment_setup import LibraryManager, LibraryPopulationPolicy, LibraryType, Storage, populate_library, populate_library_if_missing
 from benchmarks.common import AsvBase
 
 
@@ -48,16 +48,20 @@ class AWSListSymbols(AsvBase):
         policy = self.get_population_policy()
         for number_symbols in AWSListSymbols.params:
             start = time.time()
-            policy.set_parameters(num_rows* number_symbols)
-            populate_library_if_missing(manager, policy, AWSListSymbols.library_type, number_symbols)
-            self.get_logger().info(f"Generated {number_symbols} with {num_rows} each for {time.time()- start}")
+            policy.set_parameters([num_rows] * number_symbols)
+            if not manager.has_library(AWSListSymbols.library_type, number_symbols):
+                populate_library(manager, policy, AWSListSymbols.library_type, number_symbols)
+                self.get_logger().info(f"Generated {number_symbols} with {num_rows} each for {time.time()- start}")
+            else:
+                self.get_logger().info(f"Library already exists, population skipped")
         manager.log_info() # Always log the ArcticURIs 
     
     def setup(self, num_syms):
         self.population_policy = self.get_population_policy()
         self.lib = self.get_library_manager().get_library(AWSListSymbols.library_type, num_syms)
         self.test_counter = 1
-        assert num_syms == len(self.lib.list_symbols()), "The library contains expected number of symbols"
+        symbols_list = self.lib.list_symbols()
+        assert num_syms == len(symbols_list), f"The library contains expected number of symbols {symbols_list}"
         self.lib._nvs.version_store._clear_symbol_list_keys() # clear cache
 
     def time_list_symbols(self, num_syms):
@@ -119,11 +123,15 @@ class AWSVersionSymbols(AsvBase):
         for number_symbols in AWSVersionSymbols.params:
             start = time.time()
             policy.set_parameters([num_rows] * number_symbols)
-            populate_library_if_missing(manager, policy, AWSVersionSymbols.library_type, number_symbols)
+            if not manager.has_library(AWSListSymbols.library_type, number_symbols):
+                populate_library(manager, policy, AWSVersionSymbols.library_type, number_symbols)
+                self.get_logger().info(f"Generated {number_symbols} with {num_rows} each for {time.time()- start}")
+            else:
+                self.get_logger().info(f"Library already exists, population skipped")
+            # Getting one snapshot - the last
             lib = self.get_library_manager().get_library(AWSVersionSymbols.library_type, number_symbols)
             snapshot_name = lib.list_snapshots(load_metadata=False)[-1]
             last_snapshot_names_dict[number_symbols] = snapshot_name
-            self.get_logger().info(f"Generated {number_symbols} with {num_rows} each for {time.time()- start}")
         manager.log_info() # Always log the ArcticURIs 
         return last_snapshot_names_dict
     
@@ -132,9 +140,11 @@ class AWSVersionSymbols(AsvBase):
         self.lib = self.get_library_manager().get_library(AWSVersionSymbols.library_type, num_syms)
         self.test_counter = 1
         expected_num_versions = AWSVersionSymbols.mean_number_versions_per_symbol * num_syms
-        assert num_syms == len(self.lib.list_symbols()), "The library contains expected number of symbols"
-        assert expected_num_versions - 1 <= len(self.lib.list_versions()), "There are sufficient versions"
-        assert expected_num_versions - 1 <= len(self.lib.list_snapshots()), "There are sufficient snapshots"
+        self.get_logger().info(f"Library {str(self.lib)}")
+        symbols_list = self.lib.list_symbols()
+        assert num_syms == len(symbols_list), f"The library contains expected number of symbols {symbols_list}"
+        mes = f"There are sufficient versions (at least {expected_num_versions - 1}, num symbols {len(symbols_list)})"
+        assert (expected_num_versions - 1) >= len(symbols_list), mes
         assert last_snapshot_names_dict[num_syms] is not None
 
     def time_list_versions(self, last_snapshot_names_dict, num_syms):
