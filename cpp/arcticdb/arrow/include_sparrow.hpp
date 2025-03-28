6c01be96a5da8d691aee55cf3069d43b04fb3161
Ivo Dilov <iddilov@gmail.com>
2025-03-28 16:00:40 +0200

    Add sparrow to vcpkg
    This requires updating the vcpkg baseline to pick up the new sparrow
    package.
    
    The new baseline requires extra package version pinning to make sure
    everything is compatible with eachother. I've made sure to not upgrade
    any major sdks which already had pins. This commit only adds extra
    pinning to make sure everything compiles.

--- cpp/arcticdb/arrow/include_sparrow.hpp
+++ cpp/arcticdb/arrow/include_sparrow.hpp
@@ -6,19 +6,5 @@
  */
 
 #pragma once
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic warning "-Wall"
-#elif defined(_MSC_VER)
-#pragma warning(push)
-#pragma warning(disable: 4996)
-#endif
-
 #include <sparrow/sparrow.hpp>
 #include <sparrow/record_batch.hpp>
-
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic pop
-#elif defined(_MSC_VER)
-#pragma warning(pop)
-#endif
