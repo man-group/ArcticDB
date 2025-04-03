5ff516c1365b4a9a727aa3002b6c6a022a5dc9e2
Ivo Dilov <iddilov@gmail.com>
2025-04-03 14:50:56 +0300

    Upgrade manylinux to g++-11
    We were actually using g++-10 for the manylinux2014 image. This commit
    upgrades the CI image to use the already available g++-11

--- build_tooling/build_many_linux_image.sh
+++ build_tooling/build_many_linux_image.sh
@@ -44,10 +44,11 @@ RUN rpmkeys --import 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3F
             s/#?exclude.*/exclude=.edu/' /etc/yum/pluginconf.d/fastestmirror.conf
 ADD sccache /usr/local/bin/
 RUN yum update -y && \
-    yum install -y zip jq less devtoolset-11-gdb perl-IPC-Cmd \
-      openssl-devel cyrus-sasl-devel devtoolset-10-libatomic-devel libcurl-devel python3-devel flex && \
+    yum install -y zip jq less devtoolset-11 perl-IPC-Cmd \
+      openssl-devel cyrus-sasl-devel devtoolset-11-libatomic-devel libcurl-devel python3-devel flex && \
     rpm -Uvh --nodeps \$(repoquery --location mono-{core,web,devel,data,wcf,winfx}) && \
     yum clean all && touch /etc/arcticdb_deps_installed
+ENV PATH=\"/opt/rh/devtoolset-11/root/usr/bin/:$PATH\"
 LABEL io.arcticdb.cibw_ver=\"${cibuildwheel_ver}\" io.arcticdb.base=\"${manylinux_image}\"
 " > Dockerfile
 
