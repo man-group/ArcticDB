8529cf9a4bf74faaab808dcd287748da5c23b5f6
grusev <george_rusev@yahoo.com>
2025-04-09 10:15:06 +0300

    More uniqueness in library names

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -100,7 +100,7 @@ def lib_name(request: "pytest.FixtureRequest") -> str:
     name = re.sub(r"[^\w]", "_", request.node.name)[:30]
     pid = os.getpid()
     thread_id = threading.get_ident()
-    return f"{name}.{random.randint(0, 999)}_{pid}_{thread_id}_{datetime.utcnow().strftime('%Y-%m-%dT%H_%M_%S_%f')}"
+    return f"{name}.{random.randint(0, 9999999)}_{pid}_{thread_id}_{datetime.utcnow().strftime('%Y-%m-%dT%H_%M_%S_%f')}"
 
 
 @pytest.fixture
