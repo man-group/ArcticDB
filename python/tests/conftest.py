man-group/ArcticDB

commit 59587817f3052b4464b4151f86efa35a30954624

GCP Support over the S3 XML API (#2176)
This adds support for GCP over the S3-compatible XML API.

GCP support did not work out of the box because Google does not support
any of the S3 "batch" APIs. We used batch delete for all our deletion
operations, which broke. Instead I use the unary delete operation here,
using the AWS SDK's async API to reduce the performance impact of
sending single object deletes.

Users connect to GCP like:

```
ac = Arctic("gcpxml://storage.googleapis.com:aseaton_bucket?access=...&secret=...")
ac = Arctic("gcpxmls://storage.googleapis.com:aseaton_bucket?access=...&secret=...")
# Kept aws_auth naming as this will use ~/.aws/credentials
ac = Arctic("gcpxml://storage.googleapis.com:aseaton_bucket?aws_auth=true&aws_profile=blah")
ac = Arctic("gcpxml://storage.googleapis.com:aseaton_bucket?aws_auth=true&prefix=my_prefix")
```

We have a URL of `gcpxml` so that if we do a full GCP implementation
later, users can get it with `gcp://`. I've added a GCP storage proto
object so that users on the `gcpxml` API will later be able to move to a
`gcp://` API without updating any library configs. The proto is tiny,
just storing the library's prefix, which is the only information we need
to serialize.

This is implemented as a small sublass of our existing S3
implementation, with its deletion functionality overridden. The Python
library adapter creates the `NativeVersionStore` with a `GCPXMLSettings`
in memory object (in the `NativeVariantStorage`) which makes us create a
`GCPXMLStorage` C++ adapter rather than the normal `S3Storage`.

The interesting part of this PR is the threading model in
`do_remove_no_batching_impl`. This is called from an IO executor as part
of the `RemoveTask` so it is not safe (due to the risk of deadlocks) to
submit any work to the IO executor from it. Instead:
- The storage client `s3_client_impl.cpp` submits delete operations with
the S3 API. The S3 SDK has its own event loop to handle these. When they

cpp/arcticdb/storage/s3/nfs_backed_storage.cpp
@@ -184,7 +184,8 @@ KeySegmentPair NfsBackedStorage::do_read(VariantKey&& variant_key, ReadKeyOpts o
 
 void NfsBackedStorage::do_remove(VariantKey&& variant_key, RemoveOpts) {
     auto enc = encode_object_id(variant_key);
-    s3::detail::do_remove_impl(std::move(enc), root_folder_, bucket_name_, *s3_client_, NfsBucketizer{});
+    std::array<VariantKey, 1> arr{std::move(variant_key)};
+    s3::detail::do_remove_impl(std::span(arr), root_folder_, bucket_name_, *s3_client_, NfsBucketizer{});
 }
 
 void NfsBackedStorage::do_remove(std::span<VariantKey> variant_keys, RemoveOpts) {

python/arcticdb/adapters/s3_library_adapter.py
@@ -12,7 +12,6 @@ from typing import Optional
 import ssl
 import platform
 
-from arcticdb.options import LibraryOptions
 from arcticc.pb2.storage_pb2 import EnvironmentConfigsMap, LibraryDescriptor
 from arcticdb.version_store.helper import add_s3_library_to_env
 from arcticdb.config import _DEFAULT_ENV
@@ -111,7 +110,8 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
 
         super().__init__(uri, self._encoding_version)
 
-        self._native_cfg = NativeVariantStorage(NativeS3Settings(AWSAuthMethod.DISABLED, "", False))
+    def native_config(self):
+        return NativeVariantStorage(NativeS3Settings(AWSAuthMethod.DISABLED, "", False))
 
     def __repr__(self):
         return "S3(endpoint=%s, bucket=%s)" % (self._endpoint, self._bucket)
@@ -150,11 +150,11 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
             ssl=self._ssl,
             aws_auth=self._query_params.aws_auth,
             aws_profile=self._query_params.aws_profile,
-            native_cfg=self._native_cfg,
+            native_cfg=self.native_config(),
         )
 
         lib = NativeVersionStore.create_store_from_config(
-            (env_cfg, self._native_cfg), _DEFAULT_ENV, CONFIG_LIBRARY_NAME, encoding_version=self._encoding_version
+            (env_cfg, self.native_config()), _DEFAULT_ENV, CONFIG_LIBRARY_NAME, encoding_version=self._encoding_version
         )
 
         return lib._library
@@ -198,7 +198,7 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
         _kwargs = {k: v for k, v in parsed_query.items()}
         return ParsedQuery(**_kwargs)
 
-    def get_storage_override(self) -> StorageOverride:
+    def _get_s3_override(self) -> S3Override:
         s3_override = S3Override()
         # storage_override will overwrite access and key while reading config from storage
         # access and secret whether equals to _RBAC_ are used for determining aws_auth is true on C++ layer
@@ -226,10 +226,11 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
             s3_override.ssl = self._ssl
 
         s3_override.use_virtual_addressing = self._query_params.use_virtual_addressing
+        return s3_override
 
+    def get_storage_override(self) -> StorageOverride:
         storage_override = StorageOverride()
-        storage_override.set_s3_override(s3_override)
-
+        storage_override.set_s3_override(self._get_s3_override())
         return storage_override
 
     def get_masking_override(self) -> StorageOverride:
@@ -273,7 +274,7 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
             ssl=self._ssl,
             aws_auth=self._query_params.aws_auth,
             aws_profile=self._query_params.aws_profile,
-            native_cfg=self._native_cfg,
+            native_cfg=self.native_config(),
         )
 
     def _configure_aws(self):

commit 04b1d8b4c7bf3a68358ba096947c038bf1f5fbbe

Fix nfs storage tests and bugs in nfs storage (#2298)
#### Reference Issues/PRs
Fixes 8873431775

#### What does this implement or fix?
A regression was introduced in
https://github.com/man-group/ArcticDB/pull/2176, specifically the change
in nfs_backed_storage.cpp, that resulted in the shard directory being
calculated incorrectly for NFS-backed storages when deleting objects.
This manifested as failing to delete snapshots, which is particularly
noticeable because they then still appear in the output of
list_snapshots. This would likely have resulted in no data being deleted
correctly from NFS-backed storages, but for other key types this would
present as orphaned data rather than errors, as data being logically
deleted is represented by a write operation to the version chain.
#### Any other comments?

#### Checklist

<details>
  <summary>
   Checklist for code changes...
  </summary>
 
- [ ] Have you updated the relevant docstrings, documentation and
copyright notice?
- [ ] Is this contribution tested against [all ArcticDB's
features](../docs/mkdocs/docs/technical/contributing.md)?
- [ ] Do all exceptions introduced raise appropriate [error
messages](https://docs.arcticdb.io/error_messages/)?
 - [ ] Are API changes highlighted in the PR description?
- [ ] Is the PR labelled as enhancement or bug so it appears in
autogenerated release notes?
</details>

<!--
Thanks for contributing a Pull Request to ArcticDB! Please ensure you
have taken a look at:
- ArcticDB's Code of Conduct:
https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
- ArcticDB's Contribution Licensing:
https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
-->

cpp/arcticdb/storage/s3/nfs_backed_storage.cpp
@@ -207,17 +207,28 @@ static std::string prefix_handler(const std::string& prefix, const std::string& 
     return !prefix.empty() ? fmt::format("{}/{}", key_type_dir, new_prefix) : key_type_dir;
 }
 
+// signature needs to match PrefixHandler in s3_storage.hpp
+static std::string iter_prefix_handler(const std::string&, const std::string& key_type_dir, const KeyDescriptor&, KeyType) {
+    // The prefix handler is not used for filtering (done in func below)
+    // so we just return the key type dir
+    return key_type_dir;
+}
+
 bool NfsBackedStorage::do_iterate_type_until_match(KeyType key_type, const IterateTypePredicate& visitor, const std::string& prefix) {
+    // We need this filtering here instead of a regex like in s3/azure
+    // because we are doing sharding through subdirectories
+    // and the prefix might be partial(e.g. "sym_" instead of "sym_123")
+    // so it cannot be hashed to the correct shard
     const IterateTypePredicate func = [&v = visitor, prefix=prefix] (VariantKey&& k) {
         auto key = unencode_object_id(k);
-        if(prefix.empty() || variant_key_id(key) == StreamId{prefix}) {
+        if(prefix.empty() || variant_key_view(key).find(prefix) != std::string::npos) {
           return v(std::move(key));
         } else {
           return false;
         }
     };
 
-    return s3::detail::do_iterate_type_impl(key_type, func, root_folder_, bucket_name_, *s3_client_, NfsBucketizer{}, prefix_handler, prefix);
+    return s3::detail::do_iterate_type_impl(key_type, func, root_folder_, bucket_name_, *s3_client_, NfsBucketizer{}, iter_prefix_handler, prefix);
 }
 
 bool NfsBackedStorage::do_key_exists(const VariantKey& key) {

python/arcticdb/adapters/s3_library_adapter.py
@@ -62,6 +62,8 @@ class ParsedQuery:
 
     ssl: Optional[bool] = False
 
+    _test_only_is_nfs_layout: bool = False
+
 
 class S3LibraryAdapter(ArcticLibraryAdapter):
     REGEX = r"s3(s)?://(?P<endpoint>.*):(?P<bucket>[-_a-zA-Z0-9.]+)(?P<query>\?.*)?"
@@ -105,13 +107,20 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
 
         self._ssl = self._query_params.ssl
 
+        self._test_only_is_nfs_layout = self._query_params._test_only_is_nfs_layout
+
         if "amazonaws" in self._endpoint:
             self._configure_aws()
 
         super().__init__(uri, self._encoding_version)
 
     def native_config(self):
-        return NativeVariantStorage(NativeS3Settings(self._query_params.aws_auth, self._query_params.aws_profile, False))
+        if self._test_only_is_nfs_layout:
+            return None
+        else:
+            return NativeVariantStorage(
+                NativeS3Settings(self._query_params.aws_auth, self._query_params.aws_profile, False)
+            )
 
     def __repr__(self):
         return "S3(endpoint=%s, bucket=%s)" % (self._endpoint, self._bucket)
@@ -148,14 +157,22 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
             ca_cert_path=self._ca_cert_path,
             ca_cert_dir=self._ca_cert_dir,
             ssl=self._ssl,
+            is_nfs_layout=self._test_only_is_nfs_layout,
             aws_auth=self._query_params.aws_auth,
             aws_profile=self._query_params.aws_profile,
             native_cfg=self.native_config(),
         )
-
-        lib = NativeVersionStore.create_store_from_config(
-            (env_cfg, self.native_config()), _DEFAULT_ENV, CONFIG_LIBRARY_NAME, encoding_version=self._encoding_version
-        )
+        if self._test_only_is_nfs_layout:
+            lib = NativeVersionStore.create_store_from_config(
+                (env_cfg), _DEFAULT_ENV, CONFIG_LIBRARY_NAME, encoding_version=self._encoding_version
+            )
+        else:
+            lib = NativeVersionStore.create_store_from_config(
+                (env_cfg, self.native_config()),
+                _DEFAULT_ENV,
+                CONFIG_LIBRARY_NAME,
+                encoding_version=self._encoding_version,
+            )
 
         return lib._library
 
@@ -230,7 +247,8 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
 
     def get_storage_override(self) -> StorageOverride:
         storage_override = StorageOverride()
-        storage_override.set_s3_override(self._get_s3_override())
+        if not self._test_only_is_nfs_layout:
+            storage_override.set_s3_override(self._get_s3_override())
         return storage_override
 
     def get_masking_override(self) -> StorageOverride:
@@ -272,6 +290,7 @@ class S3LibraryAdapter(ArcticLibraryAdapter):
             ca_cert_path=self._ca_cert_path,
             ca_cert_dir=self._ca_cert_dir,
             ssl=self._ssl,
+            is_nfs_layout=self._test_only_is_nfs_layout,
             aws_auth=self._query_params.aws_auth,
             aws_profile=self._query_params.aws_profile,
             native_cfg=self.native_config(),

python/arcticdb/storage_fixtures/s3.py
@@ -103,6 +103,8 @@ class S3Bucket(StorageFixture):
             self.arctic_uri += f"&path_prefix={factory.default_prefix}"
         if factory.ssl:
             self.arctic_uri += "&ssl=True"
+        if factory._test_only_is_nfs_layout:
+            self.arctic_uri += "&_test_only_is_nfs_layout=True"
         if platform.system() == "Linux":
             if factory.client_cert_file:
                 self.arctic_uri += f"&CA_cert_path={self.factory.client_cert_file}"
@@ -202,17 +204,16 @@ class NfsS3Bucket(S3Bucket):
 
 
 class GcpS3Bucket(S3Bucket):
-
     def __init__(
         self,
         factory: "BaseGCPStorageFixtureFactory",
         bucket: str,
         native_config: Optional[NativeVariantStorage] = None,
     ):
-        if any(sub in factory.endpoint for sub in ["http:", "https:"])  :
+        if any(sub in factory.endpoint for sub in ["http:", "https:"]):
             super().__init__(factory, bucket, native_config=native_config)
             self.arctic_uri = self.arctic_uri.replace("s3", "gcpxml", 1)
-        else: 
+        else:
             StorageFixture.__init__(self)
             self.factory = factory
             self.bucket = bucket
@@ -246,10 +247,7 @@ class GcpS3Bucket(S3Bucket):
             with_prefix = False
 
         add_gcp_library_to_env(
-            cfg=cfg,
-            lib_name=lib_name,
-            env_name=Defaults.ENV,
-            with_prefix=with_prefix
+            cfg=cfg, lib_name=lib_name, env_name=Defaults.ENV, with_prefix=with_prefix
         )  # client_cert_dir is skipped on purpose; It will be tested manually in other tests
         return cfg, self.native_config
 
@@ -266,6 +264,7 @@ class BaseS3StorageFixtureFactory(StorageFixtureFactory):
     clean_bucket_on_fixture_exit = True
     use_mock_storage_for_testing = None  # If set to true allows error simulation
     use_internal_client_wrapper_for_testing = None  # If set to true uses the internal client wrapper for testing
+    _test_only_is_nfs_layout: bool = False
 
     def __init__(self, native_config: Optional[dict] = None):
         self.client_cert_file = None
@@ -355,6 +354,7 @@ def real_s3_from_environment_variables(
     else:
         out.default_prefix = os.getenv("ARCTICDB_PERSISTENT_STORAGE_UNIQUE_PATH_PREFIX", "") + additional_suffix
     return out
+
 
 def real_gcp_from_environment_variables(
     shared_path: bool, native_config: Optional[NativeVariantStorage] = None, additional_suffix: str = ""
@@ -718,6 +718,7 @@ class MotoS3StorageFixtureFactory(BaseS3StorageFixtureFactory):
         use_mock_storage_for_testing: bool = False,
         use_internal_client_wrapper_for_testing: bool = False,
         native_config: Optional[NativeVariantStorage] = None,
+        _test_only_is_nfs_layout: bool = False,
     ):
         super().__init__(native_config)
         self.http_protocol = "https" if use_ssl else "http"
@@ -727,6 +728,7 @@ class MotoS3StorageFixtureFactory(BaseS3StorageFixtureFactory):
         self.use_raw_prefix = use_raw_prefix
         self.use_mock_storage_for_testing = use_mock_storage_for_testing
         self.use_internal_client_wrapper_for_testing = use_internal_client_wrapper_for_testing
+        self._test_only_is_nfs_layout = _test_only_is_nfs_layout
         # This is needed because we might have multiple factory instances in the same test run
         # and we need to make sure the bucket names are unique
         # set the unique_id to the current UNIX timestamp to avoid conflicts

python/tests/conftest.py
@@ -210,7 +210,9 @@ def s3_ssl_disabled_storage_factory() -> Generator[MotoS3StorageFixtureFactory, 
 
 @pytest.fixture(scope="session")
 def nfs_backed_s3_storage_factory() -> Generator[MotoNfsBackedS3StorageFixtureFactory, None, None]:
-    with MotoNfsBackedS3StorageFixtureFactory(use_ssl=False, ssl_test_support=False, bucket_versioning=False) as f:
+    with MotoNfsBackedS3StorageFixtureFactory(
+        use_ssl=False, ssl_test_support=False, bucket_versioning=False, _test_only_is_nfs_layout=True
+    ) as f:
         yield f
 
 
@@ -310,9 +312,10 @@ def real_gcp_storage_factory() -> BaseGCPStorageFixtureFactory:
     )
 
 
-@pytest.fixture(scope="session",
-                params=[pytest.param("real_s3", marks=REAL_S3_TESTS_MARK),
-                        pytest.param("real_gcp", marks=REAL_GCP_TESTS_MARK)])
+@pytest.fixture(
+    scope="session",
+    params=[pytest.param("real_s3", marks=REAL_S3_TESTS_MARK), pytest.param("real_gcp", marks=REAL_GCP_TESTS_MARK)],
+)
 def real_storage_factory(request) -> Union[BaseGCPStorageFixtureFactory, BaseGCPStorageFixtureFactory]:
     storage_fixture: StorageFixture = request.getfixturevalue(request.param + "_storage_factory")
     return storage_fixture
@@ -465,29 +468,31 @@ def mem_storage() -> Generator[InMemoryStorageFixture, None, None]:
 # endregion
 # region ==================================== `Arctic` API Fixtures ====================================
 
+
 def filter_out_unwanted_mark(request, current_param):
-    """ Exclude specific fixture parameters or include only certain
+    """Exclude specific fixture parameters or include only certain
 
     Provides ability tp filter out unwanted fixture parameter
     To do that add to test as many of the marks you want to stay using:
-    
+
       @pytest.mark.only_fixture_params([<fixture_param_names_list>], )
 
     Or alternatively you can exclude as many as wanted:
       @pytest.mark.skip_fixture_params([<fixture_param_names_list>], )
     """
     only_fixtures = request.node.get_closest_marker("only_fixture_params")
-    if only_fixtures: 
+    if only_fixtures:
         # Only when defined at least one only_ mark evaluate what should stay
         values_to_include = only_fixtures.args[0]
         if not current_param in values_to_include:
-            pytest.skip(reason = f"Skipping {current_param} param as not included in test 'only_fixture_params' mark.")
+            pytest.skip(reason=f"Skipping {current_param} param as not included in test 'only_fixture_params' mark.")
 
     skip_fixture_value_mark = request.node.get_closest_marker("skip_fixture_params")
     if skip_fixture_value_mark:
         values_to_skip = skip_fixture_value_mark.args[0]
         if current_param in values_to_skip:
-            pytest.skip(reason = f"Skipping param: {current_param} as per 'skip_fixture_params' mark")
+            pytest.skip(reason=f"Skipping param: {current_param} as per 'skip_fixture_params' mark")
+
 
 @pytest.fixture(
     scope="function",
@@ -508,6 +513,7 @@ def arctic_client(request, encoding_version) -> Arctic:
     storage_fixture: StorageFixture = request.getfixturevalue(request.param + "_storage")
     ac = storage_fixture.create_arctic(encoding_version=encoding_version)
     return ac
+
 
 @pytest.fixture(
     scope="function",
@@ -534,6 +540,8 @@ def arctic_client_v1(request) -> Arctic:
     scope="function",
     params=[
         pytest.param("s3", marks=SIM_S3_TESTS_MARK),
+        pytest.param("nfs_backed_s3", marks=SIM_NFS_TESTS_MARK),
+        pytest.param("gcp", marks=SIM_GCP_TESTS_MARK),
         pytest.param("mem", marks=MEM_TESTS_MARK),
         pytest.param("azurite", marks=AZURE_TESTS_MARK),
         pytest.param("mongo", marks=MONGO_TESTS_MARK),
@@ -824,9 +832,13 @@ def local_object_version_store_prune_previous(local_object_store_factory):
     return local_object_store_factory(prune_previous_version=True)
 
 
-@pytest.fixture(params=[pytest.param("version_store_factory", marks=LMDB_TESTS_MARK),
-                        pytest.param("real_gcp_store_factory", marks=REAL_GCP_TESTS_MARK), 
-                        pytest.param("real_s3_store_factory", marks=REAL_S3_TESTS_MARK)])
+@pytest.fixture(
+    params=[
+        pytest.param("version_store_factory", marks=LMDB_TESTS_MARK),
+        pytest.param("real_gcp_store_factory", marks=REAL_GCP_TESTS_MARK),
+        pytest.param("real_s3_store_factory", marks=REAL_S3_TESTS_MARK),
+    ]
+)
 def version_store_and_real_s3_basic_store_factory(request):
     """
     Just the version_store and real_s3 specifically for the test test_interleaved_store_read
@@ -839,7 +851,7 @@ def version_store_and_real_s3_basic_store_factory(request):
 @pytest.fixture(
     params=[
         pytest.param("version_store_factory", marks=LMDB_TESTS_MARK),
-        pytest.param("in_memory_store_factory",marks=MEM_TESTS_MARK),
+        pytest.param("in_memory_store_factory", marks=MEM_TESTS_MARK),
         pytest.param("real_s3_store_factory", marks=REAL_S3_TESTS_MARK),
         pytest.param("real_gcp_store_factory", marks=REAL_GCP_TESTS_MARK),
     ]