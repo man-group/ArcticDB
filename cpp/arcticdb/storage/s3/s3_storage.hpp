2f13689502b19e5351ba6df289132719021d0657
Phoebus Mak <61957902+phoebusm@users.noreply.github.com>
2025-04-25 14:15:25 +0100

    Move env var setup to c++

--- cpp/arcticdb/storage/s3/s3_storage.hpp
+++ cpp/arcticdb/storage/s3/s3_storage.hpp
@@ -249,9 +249,32 @@ inline Aws::Client::ClientConfiguration get_proxy_config(Aws::Http::Scheme endpo
         return Aws::Client::ClientConfiguration();
     }
 }
+// Since aws-sdk-cpp >= 1.11.486, it has turned on checksum integrity check `x-amz-checksum-mode: enabled`
+// This feature is not supported in many s3 implementation and causes error
+// Update environment variable before import arcticdb_ext to disable checksum validation
+inline void configure_s3_checksum_validation() {
+    const char* response_checksum = std::getenv("AWS_RESPONSE_CHECKSUM_VALIDATION");
+    const char* request_checksum = std::getenv("AWS_REQUEST_CHECKSUM_CALCULATION");
+    
+    if ((response_checksum && std::string(response_checksum) == "when_supported") || 
+        (request_checksum && std::string(request_checksum) == "when_supported")) {
+        log::storage().warn("Checksum validation has been specifically enabled by user, which the endpoint may not support and causes error.");
+    }
+    else {
+#ifdef _WIN32
+        _putenv_s("AWS_RESPONSE_CHECKSUM_VALIDATION", "when_required");
+        _putenv_s("AWS_REQUEST_CHECKSUM_CALCULATION", "when_required");
+#else
+        setenv("AWS_RESPONSE_CHECKSUM_VALIDATION", "when_required", 1);
+        setenv("AWS_REQUEST_CHECKSUM_CALCULATION", "when_required", 1);
+#endif
+    }
+}
 
 template<typename ConfigType>
 auto get_s3_config(const ConfigType& conf) {
+    configure_s3_checksum_validation();
+
     auto endpoint_scheme = conf.https() ? Aws::Http::Scheme::HTTPS : Aws::Http::Scheme::HTTP;
     Aws::Client::ClientConfiguration client_configuration = get_proxy_config(endpoint_scheme);
     client_configuration.scheme = endpoint_scheme;
