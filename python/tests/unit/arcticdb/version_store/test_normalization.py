man-group/ArcticDB

commit 67d2bbe530f96a0aa5412f479e123da480ba2d99

Enhancement 8277989680: symbol concatenation poc (#2142)
#### Reference Issues/PRs
8277989680

#### What does this implement or fix?
Implements symbol concatenation. Inner and outer joins over columns both
supported. Expected usage:
```
# Read requests can contain usual as_of, date_range, columns, etc arguments
lazy_dfs = lib.read_batch([read_request_1, read_request_2, ...])
# Potentially apply some processing to all or individual constituent lazy dataframes here, that will be applied before the join
lazy_dfs = lazy_dfs[lazy_dfs["col"].notnull()]
# Join here
lazy_df = adb.concat(lazy_dfs)
# Perform more processing if desired
lazy_df = lazy_df.resample("15min").agg({"col": "mean"})
# Collect result
res = lazy_df.collect()
# res contains a list of VersionedItems from the consituent symbols that went into the join with data=None, and a data member with the joined Series/DataFrame
```
See `test_symbol_concatenation.py` for thorough examples of how the API
works.
For outer joins, if a column is not present in one of the input symbols,
then the same type-specific behaviour as used for dynamic schema is used
to backfill the missing values.
Not all symbols can be concatenated together. The following will throw
exceptions if attempted to be concatenated:

- a Series with a DataFrame
- Different index types, including multiindexes with different numbers
of levels
- Incompatible column types. e.g. if `col` has type `INT64` in one
symbol, and is a string column in another symbol. this only applies if
the column would be in the result, which is always the case for all
columns with an outer join, but may not always be for inner joins.

Where possible, the implementation is permissive with what can be joined
with an output as sensible as possible:

- Joining two or more Series with different names that are otherwise
compatible will produce a Series with no name
- Joining two or more timeseries where the indexes have different names
will produce a timeseries with an unnamed index

cpp/arcticdb/pipeline/frame_slice_map.hpp
@@ -22,7 +22,10 @@ struct FrameSliceMap {
 
     FrameSliceMap(std::shared_ptr<PipelineContext> context, bool dynamic_schema) :
         context_(std::move(context)) {
-
+        const entity::StreamDescriptor& descriptor = context_->descriptor();
+        const auto required_fields_count = static_cast<bool>(context_->norm_meta_) ?
+                                           index::required_fields_count(descriptor, *context_->norm_meta_) :
+                                           index::required_fields_count(descriptor);
         for (const auto &context_row: *context_) {
             const auto& row_range = context_row.slice_and_key().slice_.row_range;
 
@@ -42,7 +45,6 @@ struct FrameSliceMap {
                     // run over them.
                     // TODO: This logic won't be needed when we move string handling into separate type handler
                     if(is_empty_type(row_range_type)) {
-                        const entity::StreamDescriptor& descriptor = context_->descriptor();
                         const size_t global_field_idx = descriptor.find_field(field->name()).value();
                         const Field& global_field = descriptor.field(global_field_idx);
                         const entity::DataType global_field_type = global_field.type().data_type();
@@ -55,10 +57,21 @@ struct FrameSliceMap {
                         continue;
                     }
                 }
-
-                auto& column = columns_[field->name()];
-                ContextData data{context_row.index_, field.index};
-                column.insert(std::make_pair(row_range, data));
+                const size_t first_col = context_row.slice_and_key().slice_.columns().first;
+                const bool first_col_slice = first_col == 0;
+                const bool true_index_required_field = first_col_slice && field.index < descriptor.index().field_count();
+                // Second condition required to avoid underflow when subtracting one unsigned value from another
+                const bool other_required_field = descriptor.index().field_count() <= field.index &&
+                                                  required_fields_count >= first_col &&
+                                                  field.index < required_fields_count - first_col;
+                const bool required_field = true_index_required_field || other_required_field;
+                // If required_field is true, this is a required column in the output. The name in slice stream
+                // descriptor may not match that in the global stream descriptor, so use the global name here
+                // e.g. If 2 timeseries are joined that had differently named indexes
+                // All other columns use names to match the source with the destination
+                const auto& field_name = required_field ? descriptor.field(field.index + first_col).name() : field->name();
+                auto &column = columns_[field_name];
+                column.emplace(row_range, ContextData{context_row.index_, field.index});
             }
         }
     }

cpp/arcticdb/CMakeLists.txt
@@ -939,6 +939,7 @@ if(${TEST})
             processing/test/test_component_manager.cpp
             processing/test/test_expression.cpp
             processing/test/test_filter_and_project_sparse.cpp
+            processing/test/test_join_schemas.cpp
             processing/test/test_type_promotion.cpp
             processing/test/test_operation_dispatch.cpp
             processing/test/test_output_schema_aggregator_types.cpp

cpp/arcticdb/column_store/test/test_index_filtering.cpp
@@ -21,7 +21,7 @@ using namespace arcticdb::pipelines;
 std::pair<TimeseriesDescriptor , std::vector<SliceAndKey>> get_sample_slice_and_key(StreamId stream_id, VersionId version_id, size_t col_slices = 1, size_t row_slices = 10) {
     StreamDescriptor stream_desc{
         stream_id,
-        IndexDescriptorImpl{1, IndexDescriptorImpl::Type::TIMESTAMP}
+        IndexDescriptorImpl{IndexDescriptorImpl::Type::TIMESTAMP, 1}
     };
 
     stream_desc.add_field(scalar_field(DataType::NANOSECONDS_UTC64, "time"));

cpp/arcticdb/entity/read_result.hpp
@@ -21,10 +21,10 @@ namespace arcticdb {
 
 struct ARCTICDB_VISIBILITY_HIDDEN ReadResult {
     ReadResult(
-            const VersionedItem& versioned_item,
+            const std::variant<VersionedItem, std::vector<VersionedItem>>& versioned_item,
             pipelines::PythonOutputFrame&& frame_data,
             const arcticdb::proto::descriptors::NormalizationMetadata& norm_meta,
-            const arcticdb::proto::descriptors::UserDefinedMetadata& user_meta,
+            const std::variant<arcticdb::proto::descriptors::UserDefinedMetadata, std::vector<arcticdb::proto::descriptors::UserDefinedMetadata>>& user_meta,
             const arcticdb::proto::descriptors::UserDefinedMetadata& multi_key_meta,
             std::vector<entity::AtomKey>&& multi_keys) :
             item(versioned_item),
@@ -35,10 +35,10 @@ struct ARCTICDB_VISIBILITY_HIDDEN ReadResult {
             multi_keys(std::move(multi_keys)) {
 
     }
-    VersionedItem item;
+    std::variant<VersionedItem, std::vector<VersionedItem>> item;
     pipelines::PythonOutputFrame frame_data;
     arcticdb::proto::descriptors::NormalizationMetadata norm_meta;
-    arcticdb::proto::descriptors::UserDefinedMetadata user_meta;
+    std::variant<arcticdb::proto::descriptors::UserDefinedMetadata, std::vector<arcticdb::proto::descriptors::UserDefinedMetadata>> user_meta;
     arcticdb::proto::descriptors::UserDefinedMetadata multi_key_meta;
     std::vector <entity::AtomKey> multi_keys;
 
@@ -46,10 +46,17 @@ struct ARCTICDB_VISIBILITY_HIDDEN ReadResult {
 };
 
 inline ReadResult create_python_read_result(
-    const VersionedItem& version,
+    const std::variant<VersionedItem, std::vector<VersionedItem>>& version,
     OutputFormat output_format,
-    FrameAndDescriptor&& fd) {
+    FrameAndDescriptor&& fd,
+    std::optional<std::vector<arcticdb::proto::descriptors::UserDefinedMetadata>>&& user_meta = std::nullopt) {
     auto result = std::move(fd);
+
+    // If version is a vector then this was a multi-symbol join, so the user_meta vector should have a value
+    // Otherwise, there is a single piece of metadata on the frame descriptor
+    util::check(
+            std::holds_alternative<VersionedItem>(version) ^ user_meta.has_value(),
+            "Unexpected argument combination to create_python_read_result");
 
     // Very old (pre Nov-2020) PandasIndex protobuf messages had no "start" or "step" fields. If is_physically_stored
     // (renamed from is_not_range_index) was false, the index was always RangeIndex(num_rows, 1)
@@ -76,8 +83,14 @@ inline ReadResult create_python_read_result(
     util::print_total_mem_usage(__FILE__, __LINE__, __FUNCTION__);
 
     const auto& desc_proto = result.desc_.proto();
+    std::variant<arcticdb::proto::descriptors::UserDefinedMetadata, std::vector<arcticdb::proto::descriptors::UserDefinedMetadata>> metadata;
+    if (user_meta.has_value()) {
+        metadata = std::move(*user_meta);
+    } else {
+        metadata = std::move(desc_proto.user_meta());
+    }
     return {version, std::move(python_frame), desc_proto.normalization(),
-            desc_proto.user_meta(), desc_proto.multi_key_meta(), std::move(result.keys_)};
+            metadata, desc_proto.multi_key_meta(), std::move(result.keys_)};
 }
 
 } //namespace arcticdb

cpp/arcticdb/entity/stream_descriptor.hpp
@@ -124,7 +124,7 @@ struct StreamDescriptor {
 
     explicit StreamDescriptor(const StreamId& id) {
         set_id(id);
-        set_index({0, IndexDescriptor::Type::ROWCOUNT});
+        set_index({IndexDescriptor::Type::ROWCOUNT, 0});
     }
 
     void add_scalar_field(DataType data_type, std::string_view name) {
@@ -274,10 +274,12 @@ struct StreamDescriptor {
 };
 
 struct OutputSchema {
-    arcticdb::proto::descriptors::NormalizationMetadata norm_metadata_;
+    proto::descriptors::NormalizationMetadata norm_metadata_;
+
+    OutputSchema() = default;
 
     OutputSchema(StreamDescriptor stream_descriptor,
-                 arcticdb::proto::descriptors::NormalizationMetadata norm_metadata):
+                 proto::descriptors::NormalizationMetadata norm_metadata):
             norm_metadata_(std::move(norm_metadata)),
             stream_descriptor_(std::move(stream_descriptor)) {};
 

cpp/arcticdb/entity/types.hpp
@@ -548,8 +548,14 @@ struct IndexDescriptorImpl : public IndexDescriptor {
 
     IndexDescriptorImpl() = default;
 
-    IndexDescriptorImpl(uint32_t field_count, Type type) :
+    IndexDescriptorImpl(Type type, uint32_t field_count) :
         IndexDescriptor(type, field_count) {
+    }
+
+    // Maintained as this is the constructor the Python interface uses
+    // Prefer using the constructor above internally as the argument order matches that of IndexDescriptor
+    IndexDescriptorImpl(uint32_t field_count, Type type) :
+            IndexDescriptor(type, field_count) {
     }
 
     IndexDescriptorImpl(const IndexDescriptor& idx) :

cpp/arcticdb/pipeline/column_stats.cpp
@@ -14,7 +14,7 @@ namespace arcticdb {
 SegmentInMemory merge_column_stats_segments(const std::vector<SegmentInMemory>& segments) {
     SegmentInMemory merged;
     merged.init_column_map();
-    merged.descriptor().set_index(IndexDescriptorImpl{0, IndexDescriptor::Type::ROWCOUNT});
+    merged.descriptor().set_index(IndexDescriptorImpl{IndexDescriptor::Type::ROWCOUNT, 0});
 
     // Maintain the order of the columns in the input segments
     ankerl::unordered_dense::map<std::string, size_t> field_name_to_index;

cpp/arcticdb/pipeline/frame_utils.cpp
@@ -72,20 +72,65 @@ TimeseriesDescriptor index_descriptor_from_frame(
         frame->bucketize_dynamic);
 }
 
-void adjust_slice_rowcounts(const std::shared_ptr<pipelines::PipelineContext>& pipeline_context) {
-    if(pipeline_context->slice_and_keys_.empty())
+void adjust_slice_ranges(const std::shared_ptr<pipelines::PipelineContext>& pipeline_context) {
+    using namespace arcticdb::pipelines;
+    auto& slice_and_keys = pipeline_context->slice_and_keys_;
+    if(slice_and_keys.empty())
         return;
+    // Row and Col ranges input can be disjoint, "compress" them into the top left corner
+    // e.g.
+    //    1 3  6 9
+    //    ---  ---
+    // 3 |   ||   |
+    // 5 |   ||   |
+    //    1 3  4 7
+    //    ---  ---
+    // 7 |   ||   |
+    // 8 |   ||   |
+    //    ---  ---
+    // becomes
+    //    0 2  2 5
+    //    ---  ---
+    // 0 |   ||   |
+    // 2 |   ||   |
+    //    0 2  2 5
+    //    ---  ---
+    // 2 |   ||   |
+    // 3 |   ||   |
+    //    ---  ---
+    bool increment_row_slice{false};
+    size_t row_offset{0};
+    size_t col_offset{0};
+    for (auto it = slice_and_keys.begin(); it != slice_and_keys.end(); ++it) {
+        auto& slice = it->slice();
+        if (std::next(it) != slice_and_keys.end()) {
+            auto& next_slice = std::next(it)->slice();
+            // Hash groupings produce slices that all have row ranges starting at 0 and the same col ranges, hence the
+            // second condition
+            increment_row_slice = next_slice.row_range.first != slice.row_range.first ||
+                                  next_slice.col_range.first == slice.col_range.first;
+        } else {
+            increment_row_slice = true;
+        }
+        slice.row_range = RowRange(row_offset, row_offset + slice.rows().diff());
+        slice.col_range = ColRange(col_offset, col_offset + slice.columns().diff());
+        col_offset += slice.columns().diff();
+        if (increment_row_slice) {
+            row_offset += slice.rows().diff();
+            col_offset = 0;
+        }
+    }
 
-    pipeline_context->total_rows_ = adjust_slice_rowcounts(pipeline_context->slice_and_keys_);
+    pipeline_context->total_rows_ = row_offset;
 }
 
-size_t adjust_slice_rowcounts(std::vector<pipelines::SliceAndKey> & slice_and_keys, const std::optional<size_t>& first_row) {
+size_t adjust_slice_rowcounts(std::vector<pipelines::SliceAndKey> & slice_and_keys) {
     using namespace arcticdb::pipelines;
     if(slice_and_keys.empty())
 		return 0u;
 
-    auto offset = first_row.value_or(slice_and_keys[0].slice_.row_range.first);
-	auto diff = slice_and_keys[0].slice_.row_range.diff();
+    auto offset = 0;
+    auto diff = slice_and_keys[0].slice_.row_range.diff();
     auto col_begin = slice_and_keys[0].slice_.col_range.first;
 	
 	for(auto it = slice_and_keys.begin(); it != slice_and_keys.end(); ++it) {
@@ -95,7 +140,6 @@ size_t adjust_slice_rowcounts(std::vector<pipelines::SliceAndKey> & slice_and_ke
 		}
 		it->slice_.row_range = RowRange{offset, offset + diff};
 	}
-	
 	return offset + diff;
 }
 

commit 1242994d1d5a18f745b9ca4ab0846e19b8e22e2a

Fix multiindex series (#2363)
#### What does this implement or fix?
Fixes roundtripping of multiindexed Series with timestamps as the first
level and strings as the second level.
Broken by #2142

---------

Co-authored-by: Alex Owens <alex.owens@man.com>

cpp/arcticdb/pipeline/frame_slice_map.hpp
@@ -23,11 +23,19 @@ struct FrameSliceMap {
     FrameSliceMap(std::shared_ptr<PipelineContext> context, bool dynamic_schema) :
         context_(std::move(context)) {
         const entity::StreamDescriptor& descriptor = context_->descriptor();
+        const auto true_index_field_count = descriptor.index().field_count();
         const auto required_fields_count = static_cast<bool>(context_->norm_meta_) ?
                                            index::required_fields_count(descriptor, *context_->norm_meta_) :
                                            index::required_fields_count(descriptor);
+        std::optional<size_t> min_col_index;
         for (const auto &context_row: *context_) {
             const auto& row_range = context_row.slice_and_key().slice_.row_range;
+            const auto& col_range = context_row.slice_and_key().slice_.col_range;
+            if (!min_col_index.has_value()) {
+                min_col_index.emplace(col_range.first);
+            }
+            const size_t first_col = col_range.first;
+            const bool first_col_slice = first_col == *min_col_index;
 
             const auto& fields = context_row.descriptor().fields();
             for(const auto& field : folly::enumerate(fields)) {
@@ -57,19 +65,20 @@ struct FrameSliceMap {
                         continue;
                     }
                 }
-                const size_t first_col = context_row.slice_and_key().slice_.columns().first;
-                const bool first_col_slice = first_col == 0;
-                const bool true_index_required_field = first_col_slice && field.index < descriptor.index().field_count();
-                // Second condition required to avoid underflow when subtracting one unsigned value from another
-                const bool other_required_field = descriptor.index().field_count() <= field.index &&
-                                                  required_fields_count >= first_col &&
-                                                  field.index < required_fields_count - first_col;
-                const bool required_field = true_index_required_field || other_required_field;
+                // If we are in the first col slice, then we just need to check if the field index is less than the
+                // required fields count
+                // Otherwise, we need to ignore true index fields (as they are grabbed from the first column slice), and
+                // offset by first_col - true_index_field_count
+                const bool required_field = first_col_slice ?
+                                            field.index < required_fields_count :
+                                            true_index_field_count <= field.index &&
+                                            required_fields_count >= first_col - true_index_field_count &&
+                                            field.index < required_fields_count - (first_col - true_index_field_count);
                 // If required_field is true, this is a required column in the output. The name in slice stream
                 // descriptor may not match that in the global stream descriptor, so use the global name here
                 // e.g. If 2 timeseries are joined that had differently named indexes
                 // All other columns use names to match the source with the destination
-                const auto& field_name = required_field ? descriptor.field(field.index + first_col).name() : field->name();
+                const auto& field_name = required_field ? descriptor.field(field.index + (first_col_slice ? 0 : first_col)).name() : field->name();
                 auto &column = columns_[field_name];
                 column.emplace(row_range, ContextData{context_row.index_, field.index});
             }

python/tests/unit/arcticdb/version_store/test_normalization.py
@@ -23,6 +23,7 @@ if sys.version_info >= (3, 9):
 from numpy.testing import assert_equal, assert_array_equal
 from arcticdb_ext.version_store import SortedValue as _SortedValue
 
+from arcticdb import QueryBuilder
 from arcticdb.exceptions import ArcticDbNotYetImplemented, ArcticException
 from arcticdb.version_store._custom_normalizers import (
     register_normalizer,
@@ -1018,3 +1019,44 @@ def test_empty_dimension(lmdb_version_store):
     
     result = lib.read("df_zero_dim").data
     pd.testing.assert_frame_equal(result, df)
+
+
+@pytest.mark.parametrize("dynamic_schema", [True, False])
+@pytest.mark.parametrize("segment_row_size", [2, 100_000])
+@pytest.mark.parametrize("column_group_size", [2, 127])
+@pytest.mark.parametrize("data_type", ["dataframe", "series"])
+@pytest.mark.parametrize(
+    "index",
+    [
+        None,
+        pd.date_range("2025-01-01", periods=12),
+        pd.MultiIndex.from_product([pd.date_range("2025-01-01", periods=6), ["hello", "goodbye"]]),
+        pd.MultiIndex.from_product([pd.date_range("2025-01-01", periods=3), ["hello", "goodbye"], ["bonjour", "au revoir"]]),
+    ]
+)
+def test_required_field_inclusion(version_store_factory, dynamic_schema, segment_row_size, column_group_size, data_type, index):
+    lib = version_store_factory(dynamic_schema=dynamic_schema, column_group_size=column_group_size, segment_row_size=segment_row_size)
+    sym = "test_required_field_inclusion"
+    num_rows = len(index) if index is not None else 12
+    original_data = pd.Series(np.arange(num_rows), index=index) if data_type == "series" else \
+    pd.DataFrame({"col1": np.arange(num_rows), "col2": np.arange(num_rows), "col3": np.arange(num_rows)}, index=index)
+    lib.write(sym, original_data)
+    received_data = lib.read(sym).data
+    if data_type == "series":
+        assert_series_equal(original_data, received_data)
+    else:
+        assert_frame_equal(original_data, received_data)
+        # Also test with column selection and processing
+        received_data = lib.read(sym, columns=["col3"]).data
+        expected_df = original_data.drop(columns=["col1", "col2"])
+        assert_frame_equal(expected_df, received_data)
+        q = QueryBuilder()
+        q = q[q["col1"] > 5]
+        received_data = lib.read(sym, query_builder=q).data
+        expected_df = original_data[original_data["col1"] > 5]
+        if index is None:
+            expected_df.index = pd.RangeIndex(0, 6)
+        assert_frame_equal(expected_df, received_data)
+        received_data = lib.read(sym, columns=["col3"], query_builder=q).data
+        expected_df = expected_df.drop(columns=["col1", "col2"])
+        assert_frame_equal(expected_df, received_data)