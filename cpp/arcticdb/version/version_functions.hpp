2bfb0aa13ebb78d15c65d97d1b4b92b06f47dbd8
Georgi Petrov <petrov.d.george@gmail.com>
2025-05-27 13:28:31 +0300

    Remove unneeded check

--- cpp/arcticdb/version/version_functions.hpp
+++ cpp/arcticdb/version/version_functions.hpp
@@ -182,8 +182,6 @@ inline version_store::TombstoneVersionResult tombstone_versions(
         }
     }
 
-    util::check(res.keys_to_delete.size() == version_ids.size(), "Expected {} tombstone keys, got {}", version_ids.size(), res.keys_to_delete.size());
-
     version_map->write_tombstones(store, res.keys_to_delete, stream_id, entry, creation_ts);
 
     if (version_map->validate())
