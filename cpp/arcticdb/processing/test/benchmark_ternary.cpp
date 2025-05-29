6387379b7e501a0e906338b0a703bbe6c988873f
phoebusm <phoebusmak@gmail.com>
2025-05-29 16:17:15 +0100

    Query builder regex filter support

--- cpp/arcticdb/processing/test/benchmark_ternary.cpp
+++ cpp/arcticdb/processing/test/benchmark_ternary.cpp
@@ -5,124 +5,12 @@
  * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
  */
 
-#include <memory>
-#include <random>
-
-#include <benchmark/benchmark.h>
-
-#include <arcticdb/column_store/column.hpp>
-#include <arcticdb/column_store/string_pool.hpp>
 #include <arcticdb/processing/operation_dispatch_ternary.hpp>
+#include <arcticdb/processing/test/benchmark_common.hpp>
 
 using namespace arcticdb;
 
 // run like: --benchmark_time_unit=ms --benchmark_filter=.* --benchmark_min_time=5x
-
-static std::random_device rd;
-static std::mt19937 gen(rd());
-
-util::BitSet generate_bitset(const size_t num_rows) {
-    util::BitSet bitset;
-    bitset.resize(num_rows);
-    util::BitSet::bulk_insert_iterator inserter(bitset);
-    std::uniform_int_distribution<> dis(0, 1);
-    for (size_t idx = 0; idx < num_rows; ++idx) {
-        if (dis(gen) == 0) {
-            inserter = idx;
-        }
-    }
-    inserter.flush();
-    return bitset;
-}
-
-Value generate_numeric_value() {
-    std::uniform_int_distribution<int64_t> dis(std::numeric_limits<int64_t>::lowest(), std::numeric_limits<int64_t>::max());
-    return construct_value<int64_t>(dis(gen));
-}
-
-std::string generate_string() {
-    static const std::string characters =
-            "0123456789"
-            "abcdefghijklmnopqrstuvwxyz"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    std::uniform_int_distribution<std::string::size_type > dis(0, characters.size() - 1);
-    std::string res;
-    for (size_t idx = 0; idx < 10; ++idx) {
-        res += characters.at(dis(gen));
-    }
-    return res;
-}
-
-Value generate_string_value() {
-    return construct_string_value(generate_string());
-}
-
-
-ColumnWithStrings generate_numeric_dense_column(const size_t num_rows) {
-    std::vector<int64_t> data;
-    data.reserve(num_rows);
-    std::uniform_int_distribution<int64_t> dis(std::numeric_limits<int64_t>::lowest(), std::numeric_limits<int64_t>::max());
-    for (size_t idx = 0; idx < num_rows; ++idx) {
-        data.emplace_back(dis(gen));
-    }
-    Column col(make_scalar_type(DataType::INT64), num_rows, AllocationType::PRESIZED, Sparsity::NOT_PERMITTED);
-    memcpy(col.ptr(), data.data(), num_rows * sizeof(int64_t));
-    col.set_row_data(num_rows - 1);
-    return {std::move(col), {}, ""};
-}
-
-ColumnWithStrings generate_string_dense_column(const size_t num_rows, const size_t unique_strings) {
-    auto string_pool = std::make_shared<StringPool>();
-    std::vector<entity::position_t> offsets;
-    offsets.reserve(unique_strings);
-    for (size_t _ = 0; _ < unique_strings; _++) {
-        auto str = generate_string();
-        offsets.emplace_back(string_pool->get(str, false).offset());
-    }
-
-    std::vector<int64_t> data;
-    data.reserve(num_rows);
-    std::uniform_int_distribution<uint64_t> dis(0, unique_strings - 1);
-    for (size_t idx = 0; idx < num_rows; ++idx) {
-        data.emplace_back(offsets.at(dis(gen)));
-    }
-    Column col(make_scalar_type(DataType::UTF_DYNAMIC64), num_rows, AllocationType::PRESIZED, Sparsity::NOT_PERMITTED);
-    memcpy(col.ptr(), data.data(), num_rows * sizeof(int64_t));
-    col.set_row_data(num_rows - 1);
-    return {std::move(col), string_pool, ""};
-}
-
-ColumnWithStrings generate_numeric_sparse_column(const size_t num_rows) {
-    Column col(make_scalar_type(DataType::INT64), 0, AllocationType::DYNAMIC, Sparsity::PERMITTED);
-    std::uniform_int_distribution<int64_t> dis(std::numeric_limits<int64_t>::lowest(), std::numeric_limits<int64_t>::max());
-    for (size_t idx = 0; idx < num_rows; ++idx) {
-        if (dis(gen) < 0) {
-            col.set_scalar<int64_t>(static_cast<ssize_t>(idx), dis(gen));
-        }
-    }
-    col.set_row_data(num_rows - 1);
-    return {std::move(col), {}, ""};
-}
-
-ColumnWithStrings generate_string_sparse_column(const size_t num_rows, const size_t unique_strings) {
-    Column col(make_scalar_type(DataType::UTF_DYNAMIC64), 0, AllocationType::DYNAMIC, Sparsity::PERMITTED);
-    auto string_pool = std::make_shared<StringPool>();
-    std::vector<entity::position_t> offsets;
-    offsets.reserve(unique_strings);
-    for (size_t _ = 0; _ < unique_strings; _++) {
-        auto str = generate_string();
-        offsets.emplace_back(string_pool->get(str, false).offset());
-    }
-    std::uniform_int_distribution<uint64_t> dis(0, unique_strings - 1);
-    for (size_t idx = 0; idx < num_rows; ++idx) {
-        if (dis(gen) < unique_strings / 2) {
-            col.set_scalar<int64_t>(static_cast<ssize_t>(idx), offsets.at(dis(gen)));
-        }
-    }
-    col.set_row_data(num_rows - 1);
-    return {std::move(col), string_pool, ""};
-}
-
 static void BM_ternary_bitset_bitset(benchmark::State& state) {
     const auto num_rows = static_cast<size_t>(state.range(0));
     const auto condition = generate_bitset(num_rows);
---
+++ cpp/arcticdb/util/python_bindings.hpp
@@ -0,0 +1,20 @@
+/* Copyright 2023 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+ */
+
+#pragma once
+
+#include <pybind11/pybind11.h>
+
+namespace arcticdb::util {
+
+namespace py = pybind11;
+
+void register_bindings(py::module &m);
+
+} // namespace arcticdb::util
+
+
