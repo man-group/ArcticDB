9417ba71b6242f936afcce64ab562be458573e01
Alex Seaton <alexbseaton@gmail.com>
2025-05-30 18:40:50 +0100

    Recursive normalizers: Support wider range of dict keys - that would not be supported as symbol names

--- cpp/arcticdb/stream/python_bindings.hpp
+++ cpp/arcticdb/stream/python_bindings.hpp
@@ -19,20 +19,20 @@ namespace stream {
 void register_stream_bindings(py::module &m);
 
 inline void register_bindings(py::module &m) {
-    auto arcticxx_types = m.def_submodule("types", R"pydoc(
+    auto arcticdb_ext_types = m.def_submodule("types", R"pydoc(
     Fundamental types
     -----------------
     Contains definition of the types used to define the descriptors)pydoc");
 
-    arcticdb::register_types(arcticxx_types);
+    arcticdb::register_types(arcticdb_ext_types);
 
-    auto arcticxx_stream = m.def_submodule("stream", R"pydoc(
+    auto arcticdb_ext_stream = m.def_submodule("stream", R"pydoc(
     arcticdb Streams
     -----------------
     Contains the stream api classes used to write/read streams of values
     )pydoc");
 
-    arcticdb::stream::register_stream_bindings(arcticxx_stream);
+    arcticdb::stream::register_stream_bindings(arcticdb_ext_stream);
 }
 
 } // namespace arcticdb::stream
