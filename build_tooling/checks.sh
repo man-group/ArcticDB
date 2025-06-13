5b69b97cfe4e71f0504a647e2e24e45b66dec50b
Georgi Rusev <Georgi Rusev>
2025-06-13 16:00:25 +0300

    more flexible RE

--- build_tooling/checks.sh
+++ build_tooling/checks.sh
@@ -5,12 +5,13 @@ pushd "$(pwd)" >/dev/null
 tooling_dir="$(dirname $BASH_SOURCE)"
 cd $tooling_dir/../python
 
-FORBIDDEN_CONTENT=("import\s+test" "from\s+test")
+RE='(?:[\s]+|[\n]+)' 
+FORBIDDEN_CONTENT=("import${RE}test" "from${RE}test")
 SEARCH_DIR="arcticdb/"
 ERROR_FOUND=0
 
 for CONTENT in "${FORBIDDEN_CONTENT[@]}"; do
-    MATCHES=$(grep -rP "$CONTENT" "$SEARCH_DIR") 
+    MATCHES=$(grep -rlPz "$CONTENT" "$SEARCH_DIR") 
     if [ -n "$MATCHES" ]; then
         echo "ERROR: Forbidden package '$CONTENT' is imported!"
         echo "$MATCHES"  
