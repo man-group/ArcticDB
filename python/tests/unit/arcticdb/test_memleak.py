11ca2258b5ed947d462bc984887560a89c6dbc20
Alex Owens <alexowens90@gmail.com>
2025-06-16 13:54:06 +0100

    Minimal repro

---
+++ python/tests/unit/arcticdb/test_memleak.py
@@ -0,0 +1,74 @@
+"""
+Copyright 2025 Man Group Operations Limited
+
+Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+
+As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
+"""
+import gc
+import numpy as np
+import pandas as pd
+import os
+import psutil
+import pytest
+
+from arcticdb import Arctic, LibraryOptions, QueryBuilder
+
+
+ac = Arctic("lmdb:///tmp/memleak_test")
+lib = ac.get_library("memleak_test", create_if_missing=True, library_options=LibraryOptions(columns_per_segment=1_000_000))
+num_rows = 1
+num_columns = 100_000
+columns=[f"col_{idx}" for idx in range(num_columns)]
+
+
+def test_write_data():
+    lib._nvs.version_store.clear()
+    df = pd.DataFrame(
+        np.random.randint(0, 100, size=(num_rows, num_columns)),
+        columns=columns,
+    )
+    lib.write("sym", df)
+
+
+@pytest.mark.parametrize("read_from_arctic", [True, False])
+def test_read_data_and_drop_columns(read_from_arctic):
+    print(f"read_from_arctic {read_from_arctic}")
+    get_rss = lambda: psutil.Process(os.getpid()).memory_info().rss / 1e6
+    if read_from_arctic:
+        df = lib.read("sym").data
+    else:
+        df = pd.DataFrame(
+            np.random.randint(0, 100, size=(num_rows, num_columns)),
+            columns=columns,
+            index=pd.date_range("2000-01-01", periods=num_rows),
+        )
+    print(f"RSS: {get_rss():.2f} MB")
+    chunks = 10
+    columns_per_chunk = num_columns // chunks
+    for idx in range(chunks):
+        df = df.drop(columns=columns[idx * columns_per_chunk: (idx + 1) * columns_per_chunk])
+        gc.collect()
+        print(f"RSS: {get_rss():.2f} MB")
+    del df
+    gc.collect()
+    print(f"RSS: {get_rss():.2f} MB")
+
+
+@pytest.mark.parametrize("read_from_arctic", [True, False])
+def test_read_data(read_from_arctic):
+    print(f"read_from_arctic {read_from_arctic}")
+    retrieved = []
+    get_rss = lambda: psutil.Process(os.getpid()).memory_info().rss / 1e6
+
+    for i in range(10):
+        if read_from_arctic:
+            small = lib.read("sym").data
+        else:
+            small = pd.DataFrame(
+                np.random.randint(0, 100, size=(num_rows, num_columns)),
+                columns=columns,
+                index=pd.date_range("2000-01-01", periods=num_rows),
+            )
+        retrieved.append(small)
+        print(f"RSS: {get_rss():.2f} MB")
