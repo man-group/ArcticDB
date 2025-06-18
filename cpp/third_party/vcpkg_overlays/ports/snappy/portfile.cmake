963fcbdfbcf43e2b9594de59c82ccbdf2e52717c
Julien Jerphanion <git@jjerphan.xyz>
2025-06-18 11:28:52 +0200

    Remove `snappy-disable-bmi.patch`
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- cpp/third_party/vcpkg_overlays/ports/snappy/portfile.cmake
+++ cpp/third_party/vcpkg_overlays/ports/snappy/portfile.cmake
@@ -2,7 +2,6 @@ vcpkg_from_github(
     fix_clang-cl_build.patch
     no-werror.patch
     pkgconfig.diff
-    "snappy-disable-bmi.patch"
 )
 
 file(COPY "${CURRENT_PORT_DIR}/snappy.pc.in" DESTINATION "${SOURCE_PATH}")
