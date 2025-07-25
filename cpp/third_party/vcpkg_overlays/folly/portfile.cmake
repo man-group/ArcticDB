ad6fc356491e9a5842f5763c0847a9c0b4667e6f
Johan Mabille <johan.mabille@gmail.com>
2025-07-25 13:27:04 +0200

    Windows NOMINMAX fix

--- cpp/third_party/vcpkg_overlays/folly/portfile.cmake
+++ cpp/third_party/vcpkg_overlays/folly/portfile.cmake
@@ -15,6 +15,7 @@ vcpkg_from_github(
         0004-fix-absolute-dir.patch
         0005-Deactivate-libunwind-features.patch
         0006-fix-Ignore-features-based-on-demangle.h.patch
+        0007-fix-windows-minmax.patch
 )
 file(REMOVE "${SOURCE_PATH}/CMake/FindFastFloat.cmake")
 file(REMOVE "${SOURCE_PATH}/CMake/FindFmt.cmake")
