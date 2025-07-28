e17e4ed8231188571fbce02eeed579fd67ee9703
Alex Owens <alexowens90@gmail.com>
2025-07-28 15:22:40 +0100

    Revert "Fix memleak"
    This reverts commit 9ce0fc86460349ffe9afeee6f9a6e7b5d3579e31.

--- cpp/arcticdb/column_store/chunked_buffer.hpp
+++ cpp/arcticdb/column_store/chunked_buffer.hpp
@@ -533,9 +533,7 @@ class ChunkedBufferImpl {
 
     MemBlock* create_detachable_block(size_t capacity, size_t offset) const {
         auto [ptr, ts] = Allocator::aligned_alloc(sizeof(MemBlock));
-        // TODO: Use new again when sparrow bug is fixed
-        auto* data = std::allocator<uint8_t>().allocate(capacity);
-//        auto* data = new uint8_t[capacity];
+        auto* data = new uint8_t[capacity];
         new(ptr) MemBlock(data, capacity, offset, ts, true);
         return reinterpret_cast<BlockType*>(ptr);
     }
