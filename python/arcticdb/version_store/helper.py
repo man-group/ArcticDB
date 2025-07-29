7138857887324bcf0599af8e09dd9b0074b168f5
Georgi Rusev <Georgi Rusev>
2025-07-29 08:31:35 +0300

    fiv error in get_azure_proto

--- python/arcticdb/version_store/helper.py
+++ python/arcticdb/version_store/helper.py
@@ -431,7 +431,7 @@ def get_azure_proto(
     else:
         azure.prefix = lib_name
     azure.ca_cert_path = ca_cert_path
-    azure.ca_cert_dir = ca_cert_dir
+    azure.ca_cert_dir = ca_cert_dir if ca_cert_dir else ""
 
     sid, storage = get_storage_for_lib_name(azure.prefix, env)
     storage.config.Pack(azure, type_url_prefix="cxx.arctic.org")
