a077b3b1aa8244cf9c4580e0dba59208cc534df7
Georgi Rusev <Georgi Rusev>
2025-07-30 12:59:03 +0300

    fixed special xfail

--- python/installation_tests/conftest.py
+++ python/installation_tests/conftest.py
@@ -12,6 +12,7 @@ import random
 import re
 import sys
 import threading
+import traceback
 from typing import Generator
 import uuid
 import pytest
@@ -79,26 +80,32 @@ def ac_library(request, ac_client, lib_name) -> Generator[Library, None, None]:
 #region Pytest special xfail handling
 
 MACOS = sys.platform == "darwin"
-# This is due issue 9692682845 - has_library may return error on Mac_OS
-# With thisapproach we will xfail dynamically all tests experiencing this error on MacOS
+XFAILMESSAGE= "This is due issue 9692682845 - has_library may return error on Mac_OS"
 ERROR_MARKER = "arcticdb_ext.exceptions.InternalException: Azure::Storage::StorageException(404 The specified blob does not exist."
 marked_tests = []  # Global list to collect xfailed test IDs
 
 def pytest_runtest_makereport(item, call):
     if MACOS and call.excinfo:
-        err_msg = str(call.excinfo.value)
-        if ERROR_MARKER in err_msg:
-            report = pytest.TestReport.from_item_and_call(item, call)
-            report.outcome = "skipped"
-            report.wasxfail = True
+        if call.excinfo:
+            err_msg = str(call.excinfo.value)
+            full_trace = ''.join(traceback.format_exception(
+                call.excinfo.type,
+                call.excinfo.value,
+                call.excinfo.tb
+            ))
 
-            # Collect the test ID
-            marked_tests.append(item.nodeid)
-            return report
+            if ERROR_MARKER in full_trace:
+                report = pytest.TestReport.from_item_and_call(item, call)
+                report.outcome = "skipped"
+                report.wasxfail = "Skipped due to known macOS issue"
+
+                # Collect the test ID
+                marked_tests.append(item.nodeid)
+                return report
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
     if marked_tests:
-        terminalreporter.write("\n=== MAC-OS XFAIL SUMMARY ===\n", bold=True)
+        terminalreporter.write("\n=== SPECIAL XFAIL SUMMARY ===\n", bold=True)
         for test_id in marked_tests:
             terminalreporter.write(f"• {test_id}\n")
         terminalreporter.write("=============================\n\n")
