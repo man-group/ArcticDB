f341095fa1abd5e679dbed5e61597535708517d6
Deyan <deyan.hadzhi.manich@gmail.com>
2025-08-05 17:49:59 +0300

    Fixed usage of write_versioned_dataframe_internal in a test

--- cpp/arcticdb/version/test/test_sparse.cpp
+++ cpp/arcticdb/version/test/test_sparse.cpp
@@ -419,7 +419,7 @@ TEST_F(SparseTestStore, SegmentWithExistingIndex) {
     bool written = false;
     DynamicAggregator aggregator(std::move(schema), [&](SegmentInMemory &&segment) {
         if(!written) {
-            test_store_->write_versioned_dataframe_internal(stream_id, std::move(segment), false, false, false, true);
+            test_store_->write_versioned_dataframe_internal(stream_id, std::move(segment), false, false, false);
             written = true;
         }
         else {
@@ -479,7 +479,7 @@ TEST_F(SparseTestStore, SegmentAndFilterColumn) {
     bool written = false;
     DynamicAggregator aggregator(std::move(schema), [&](SegmentInMemory &&segment) {
         if(!written) {
-            test_store_->write_versioned_dataframe_internal(stream_id, std::move(segment), false, false, false, true);
+            test_store_->write_versioned_dataframe_internal(stream_id, std::move(segment), false, false, false);
             written = true;
         }
         else {
@@ -536,7 +536,7 @@ TEST_F(SparseTestStore, SegmentWithRangeFilter) {
     bool written = false;
     DynamicAggregator aggregator(std::move(schema), [&](SegmentInMemory &&segment) {
         if(!written) {
-            test_store_->write_versioned_dataframe_internal(stream_id, std::move(segment), false, false, false, true);
+            test_store_->write_versioned_dataframe_internal(stream_id, std::move(segment), false, false, false);
             written = true;
         }
         else {
