c0a27e48183a429da63e0b7674f02e573d25d851
Vasil Pashov <vasil.pashov1@gmail.com>
2025-08-25 14:35:01 +0300

    Tests for upsert

--- python/tests/unit/arcticdb/version_store/test_merge.py
+++ python/tests/unit/arcticdb/version_store/test_merge.py
@@ -23,7 +23,7 @@ class MergeAction(Enum):
 
 class MergeStrategy(NamedTuple):
     matched: MergeAction = MergeAction.UPDATE
-    not_matched: MergeAction = MergeAction.DO_NOTHING
+    not_matched: MergeAction = MergeAction.INSERT
 
 
 # Note: Maxim had another suggestion for using enum flags instead of a tuple. That would require that the strategies
@@ -43,7 +43,7 @@ class MergeStrategy(NamedTuple):
 #
 # but it's not shorter than
 # lib.merge("sym", source, strategy=MergeStrategy(MergeAction.DO_NOTHING, MergeAction.INSERT))
-# lib.merge("sym", source, strategy=MergeStrategy(matched=MergeAction.DO_NOTHING, not_matched=naMergeAction.INSERT))
+# lib.merge("sym", source, strategy=MergeStrategy(matched=MergeAction.DO_NOTHING, not_matched=MergeAction.INSERT))
 # lib.merge("sym", source, strategy=MergeStrategy(MergeAction.INSERT, MergeAction.INSERT))
 
 
@@ -78,7 +78,7 @@ class TestMergeTimeseries:
             timestamp=write_vit.timestamp + 1,
         ), raising=False)
 
-        merge_vit = lib.merge("sym", source)
+        merge_vit = lib.merge("sym", source, strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
         assert merge_vit.version == 1
         assert merge_vit.symbol == write_vit.symbol
         assert merge_vit.timestamp > write_vit.timestamp
@@ -133,7 +133,7 @@ class TestMergeTimeseries:
             timestamp=write_vit.timestamp + 1,
         ), raising=False)
 
-        merge_vit = lib.merge("sym", source, metadata=metadata)
+        merge_vit = lib.merge("sym", source, metadata=metadata, strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
         assert merge_vit.version == 1
         assert merge_vit.symbol == write_vit.symbol
         assert merge_vit.timestamp > write_vit.timestamp
@@ -206,7 +206,7 @@ class TestMergeTimeseries:
             timestamp=write_vit.timestamp + 1,
         ), raising=False)
         source = pd.DataFrame({"a": [4, 5], "b": [4.0, 5.0]}, index=pd.date_range("2023-01-01", periods=2))
-        merge_vit = lib.merge("sym", source)
+        merge_vit = lib.merge("sym", source, strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
         assert merge_vit.version == 1
         assert merge_vit.timestamp > write_vit.timestamp
 
@@ -237,7 +237,7 @@ class TestMergeTimeseries:
         monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: None, raising=False)
 
         source = pd.DataFrame({"a": [30, 50], "b": [31.0, 51.0]}, index=pd.DatetimeIndex([pd.Timestamp("2024-01-04"), pd.Timestamp("2024-01-06")]))
-        lib.merge("sym", source)
+        lib.merge("sym", source, strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
 
         expected = pd.DataFrame({"a": [0, 1, 2, 30, 4, 50, 6], "b": [0.0, 1.0, 2.0, 31.0, 4.0, 51.0, 6.0]})
         monkeypatch.setattr(lib, "read", lambda *args, **kwargs: VersionedItem("sym", "lib", expected, 2))
@@ -261,7 +261,7 @@ class TestMergeTimeseries:
             {f"col_{i}": [20 + i, 30 + i, 40 + i] for i in range(5)},
             index=pd.DatetimeIndex([pd.Timestamp("2024-01-03"), pd.Timestamp("2024-01-04"), pd.Timestamp("2024-01-07")])
         )
-        lib.merge("sym", source)
+        lib.merge("sym", source, strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
 
         expected = target.copy(deep=True)
         for i in range(len(source)):
@@ -289,7 +289,7 @@ class TestMergeTimeseries:
             expected.loc[source.index[i]] = source.iloc[i]
 
         monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: None, raising=False)
-        lib.merge("sym", source)
+        lib.merge("sym", source, strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
 
         monkeypatch.setattr(lib, "read", lambda *args, **kwargs: VersionedItem("sym", "lib", expected, 2))
         received = lib.read("sym").data
@@ -315,7 +315,7 @@ class TestMergeTimeseries:
                 "2024-01-04" # Does not match either
         ]))
         monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: None, raising=False)
-        lib.merge("sym", source, on=["a"])
+        lib.merge("sym", source, on=["a"], strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
 
         expected = pd.DataFrame({"a": [1, 2, 3], "b": [10.0, 2.0, 3.0]}, index=pd.date_range("2024-01-01", periods=3))
         monkeypatch.setattr(lib, "read", lambda *args, **kwargs: VersionedItem("sym", "lib", expected, 2))
@@ -349,7 +349,7 @@ class TestMergeTimeseries:
         ]))
 
         monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: None, raising=False)
-        lib.merge("sym", source, on=["b", "d", "e"])
+        lib.merge("sym", source, on=["b", "d", "e"], strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
 
         expected = pd.DataFrame({
             "a": [10, 2, 3, 4],
@@ -375,7 +375,7 @@ class TestMergeTimeseries:
             {"a": [5], "b": [20.0]},
             index=pd.DatetimeIndex([pd.Timestamp("2024-01-01")])
         )
-        lib.merge("sym", source)
+        lib.merge("sym", source, strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
         expected = pd.DataFrame(
             {"a": [5, 5, 3], "b": [20.0, 20.0, 3.0]},
             index=pd.DatetimeIndex([pd.Timestamp("2024-01-01"), pd.Timestamp("2024-01-01"), pd.Timestamp("2024-01-02")])
@@ -395,7 +395,15 @@ class TestMergeTimeseries:
         with pytest.raises(SchemaException):
             lib.merge("sym", source)
 
-    def test_static_schema_merge_throws_when_column_names_differ(self, lmdb_library, monkeypatch):
+    @pytest.mark.parametrize("strategy", (
+            None,
+            MergeStrategy(MergeAction.UPDATE, MergeAction.DO_NOTHING),
+            MergeStrategy(MergeAction.INSERT, MergeAction.DO_NOTHING),
+            MergeStrategy(MergeAction.INSERT, MergeAction.INSERT),
+            MergeStrategy(MergeAction.DO_NOTHING, MergeAction.INSERT),
+            MergeStrategy(MergeAction.UPDATE, MergeAction.INSERT)
+    ))
+    def test_static_schema_merge_throws_when_column_names_differ(self, lmdb_library, strategy, monkeypatch):
         lib = lmdb_library
         target = pd.DataFrame({"a": [1, 2, 3], "b": [1.0, 2.0, 3.0]}, index=pd.date_range("2024-01-01", periods=3))
         lib.write("sym", target)
@@ -404,9 +412,18 @@ class TestMergeTimeseries:
         monkeypatch.setattr(lib.__class__, "merge", raise_wrapper(SchemaException), raising=False)
 
         with pytest.raises(SchemaException):
-            lib.merge("sym", source)
+            if strategy is None:
+                lib.merge("sym", source)
+            else:
+                lib.merge("sym", source, strategy=strategy)
 
-    def test_merge_update_throws_when_target_row_is_matched_more_than_once(self, lmdb_library, monkeypatch):
+    @pytest.mark.parametrize("strategy", (
+            MergeStrategy(MergeAction.UPDATE, MergeAction.DO_NOTHING),
+            MergeStrategy(MergeAction.INSERT, MergeAction.DO_NOTHING),
+            MergeStrategy(MergeAction.UPDATE, MergeAction.INSERT),
+            MergeStrategy(MergeAction.INSERT, MergeAction.INSERT)
+    ))
+    def test_merge_update_throws_when_target_row_is_matched_more_than_once(self, lmdb_library, strategy, monkeypatch):
         lib = lmdb_library
         target = pd.DataFrame({"a": [1, 2, 3], "b": [1.0, 2.0, 3.0]}, index=pd.date_range("2024-01-01", periods=3))
         lib.write("sym", target)
@@ -421,7 +438,7 @@ class TestMergeTimeseries:
 
         monkeypatch.setattr(lib.__class__, "merge", raise_wrapper(UserInputException), raising=False)
         with pytest.raises(UserInputException):
-            lib.merge("sym", source)
+            lib.merge("sym", source, strategy=strategy)
 
     def test_merge_insert_not_matched(self, lmdb_library, monkeypatch):
         lib = lmdb_library
@@ -612,6 +629,211 @@ class TestMergeTimeseries:
         monkeypatch.setattr(lib, "read", lambda *args, **kwargs: VersionedItem("sym", "lib", expected, 2))
         received = lib.read("sym").data
         assert_frame_equal(received, expected)
+
+    def test_merge_insert_does_not_throw_when_target_row_is_matched_more_than_once_when_matched_is_do_nothing(self, lmdb_library, monkeypatch):
+        lib = lmdb_library
+        target = pd.DataFrame({"a": [1, 2, 3], "b": [1.0, 2.0, 3.0]}, index=pd.date_range("2024-01-01", periods=3))
+        lib.write("sym", target)
+
+        source = pd.DataFrame(
+            {"a": [1, 2, 3], "b": [1.0, 2.0, 3.0]},
+            index=pd.DatetimeIndex([
+                pd.Timestamp("2024-01-01"), # Matches first row of target
+                pd.Timestamp("2024-01-01"), # Also matches first row of target
+                pd.Timestamp("2024-01-04")
+            ]))
+
+        monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: None, raising=False)
+        lib.merge("sym", source, strategy=MergeStrategy(MergeAction.DO_NOTHING, MergeAction.INSERT))
+
+    def test_upsert(self, lmdb_library, monkeypatch):
+        lib = lmdb_library
+        target = pd.DataFrame({"a": [1, 2, 3], "b": [1.0, 2.0, 3.0], "c": ["a", "b", "c"]}, index=pd.date_range("2024-01-01", periods=3))
+        write_vit = lib.write("sym", target)
+
+        source = pd.DataFrame(
+            {"a": [-1, 20, -2], "b": [-1.1, 20.1, -3.1], "c": ["a", "c", "d"]},
+            index=pd.DatetimeIndex([pd.Timestamp("2024-01-01 15:00:00"), pd.Timestamp("2024-01-02"), pd.Timestamp("2024-01-02 01:00:00")])
+        )
+
+        monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: VersionedItem(
+            symbol=write_vit.symbol,
+            library=write_vit.library,
+            data=None,
+            version=1,
+            metadata=write_vit.metadata,
+            host=write_vit.host,
+            timestamp=write_vit.timestamp + 1,
+        ), raising=False)
+
+        merge_vit = lib.merge("sym", source)
+        assert merge_vit.version == 1
+        assert merge_vit.symbol == write_vit.symbol
+        assert merge_vit.timestamp > write_vit.timestamp
+        assert merge_vit.metadata == write_vit.metadata
+        assert merge_vit.library == write_vit.library
+        assert merge_vit.host == write_vit.host
+        assert merge_vit.data is None
+
+        expected = pd.DataFrame({
+                "a": [1, -1, 20, -2, 3],
+                "b": [1.0, -1.1, 20.1, -3.1, 3.0],
+                "c": ["a", "a", "c", "d", "c"]
+            }, index=pd.DatetimeIndex([
+                pd.Timestamp("2024-01-01"),
+                pd.Timestamp("2024-01-01 15:00:00"),
+                pd.Timestamp("2024-01-02"),
+                pd.Timestamp("2024-01-02 01:00:00"),
+                pd.Timestamp("2024-01-03")
+            ])
+        )
+
+        monkeypatch.setattr(lib, "read", lambda *args, **kwargs: VersionedItem(
+            symbol=merge_vit.symbol,
+            library=merge_vit.library,
+            data=expected,
+            version=merge_vit.version,
+            metadata=merge_vit.metadata,
+            host=merge_vit.host,
+            timestamp=merge_vit.timestamp,
+        ))
+
+        read_vit = lib.read("sym")
+        assert_vit_equals_except_data(read_vit, merge_vit)
+        assert_frame_equal(read_vit.data, expected)
+
+        lt = lib._dev_tools.library_tool()
+        monkeypatch.setattr(lt, "find_keys_for_symbol", mock_find_keys_for_symbol({KeyType.TABLE_DATA: 2, KeyType.TABLE_INDEX: 2, KeyType.VERSION: 2}))
+        assert len(lt.find_keys_for_symbol(KeyType.TABLE_DATA, "sym")) == 2
+        assert len(lt.find_keys_for_symbol(KeyType.TABLE_INDEX, "sym")) == 2
+        assert len(lt.find_keys_for_symbol(KeyType.VERSION, "sym")) == 2
+
+    @pytest.mark.parametrize("slicing_policy", [
+        {"rows_per_segment": 2},
+        {"columns_per_segment": 2},
+        {"rows_per_segment": 2, "columns_per_segment": 2}
+    ])
+    def test_upsert_with_slicing(self, lmdb_library_factory, monkeypatch, slicing_policy):
+        lib = lmdb_library_factory(arcticdb.LibraryOptions(**slicing_policy))
+        target = pd.DataFrame({"a": [1, 2, 3, 4, 5], "b": [1.0, 2.0, 3.0, 4.0, 5.0], "c": ["a", "b", "c", "d", "e"]}, index=pd.date_range("2024-01-01", periods=5))
+        lib.write("sym", target)
+
+        # Insertion pushes the row to be updated into a new segment
+        source = pd.DataFrame({"a": [-1, 40], "b": [-1.1, 40.4], "c": ["a", "f"]}, index=pd.DatetimeIndex([pd.Timestamp("2024-01-03 15:00:00"), pd.Timestamp("2024-01-04")]))
+        monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: None, raising=False)
+        lib.merge("sym", source)
+
+        expected = pd.DataFrame(
+            {"a": [1, 2, 3, -1, 40, 5], "b": [1.0, 2.0, 3.0, -1.1, 40.4, 5.0], "c": ["a", "b", "c", "a", "f", "e"]},
+            index=pd.DatetimeIndex([
+                pd.Timestamp("2024-01-01"),
+                pd.Timestamp("2024-01-02"),
+                pd.Timestamp("2024-01-03"),
+                pd.Timestamp("2024-01-03 15:00:00"),
+                pd.Timestamp("2024-01-04"),
+                pd.Timestamp("2024-01-05")
+            ])
+        )
+        monkeypatch.setattr(lib, "read", lambda *args, **kwargs: VersionedItem("sym", "lib", expected, 2))
+        assert_frame_equal(lib.read("sym").data, expected)
+
+        lt = lib._dev_tools.library_tool()
+        if "rows_per_segment" in slicing_policy and "columns_per_segment" in slicing_policy:
+            expected_data_keys = 10
+        elif "rows_per_segment" in slicing_policy:
+            expected_data_keys = 5
+        elif "columns_per_segment" in slicing_policy:
+            expected_data_keys = 4
+        monkeypatch.setattr(lt, "find_keys_for_symbol", mock_find_keys_for_symbol({KeyType.TABLE_DATA: expected_data_keys, KeyType.TABLE_INDEX: 2, KeyType.VERSION: 2}))
+        assert len(lt.find_keys_for_symbol(KeyType.TABLE_DATA, "sym")) == expected_data_keys
+        assert len(lt.find_keys_for_symbol(KeyType.TABLE_INDEX, "sym")) == 2
+        assert len(lt.find_keys_for_symbol(KeyType.VERSION, "sym")) == 2
+
+    def test_upsert_on_index_and_column(self, lmdb_library, monkeypatch):
+        lib = lmdb_library
+        target = pd.DataFrame(
+            {"a": [1, 2, 3], "b": [1.0, 2.0, 3.0], "c": ["a", "b", "c"]},
+            index=pd.date_range("2024-01-01", periods=3)
+        )
+        lib.write("sym", target)
+
+        source = pd.DataFrame(
+            {"a": [1, 2, 30, 40], "b": [10.0, 20.0, 30.0, 40.0], "c": ["A", "B", "A", "C"]},
+            index=pd.DatetimeIndex([
+                pd.Timestamp("2024-01-01"), # Matches index and column
+                pd.Timestamp("2024-01-02 01:00:00"), # Matches column, but not index
+                pd.Timestamp("2024-01-03"), # Matches index, but not column
+                pd.Timestamp("2024-01-04") # Does not match either
+            ])
+        )
+        monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: None, raising=False)
+        lib.merge("sym", source, on=["a"])
+
+        expected = pd.DataFrame(
+            {"a": [1, 2, 2, 3, 30, 40], "b": [10.0, 2.0, 20.0, 3.0, 30.0, 40.0], "c": ["A", "b", "B", "c", "A", "C"]},
+            index=pd.DatetimeIndex([
+                pd.Timestamp("2024-01-01"),
+                pd.Timestamp("2024-01-02"),
+                pd.Timestamp("2024-01-02 01:00:00"),
+                pd.Timestamp("2024-01-03"),
+                pd.Timestamp("2024-01-03"),
+                pd.Timestamp("2024-01-04")
+            ])
+        )
+        monkeypatch.setattr(lib, "read", lambda *args, **kwargs: VersionedItem("sym", "lib", expected, 2))
+        received = lib.read("sym").data
+
+        assert_frame_equal(received, expected)
+
+    def test_upsert_multiple_columns(self, lmdb_library, monkeypatch):
+        lib = lmdb_library
+        target = pd.DataFrame({
+            "a": [1, 2, 3, 4],
+            "b": ["a", "b", "c", "d"],
+            "c": ["A", "B", "A", "C"],
+            "d": [10.1, 20.2, 30.3, 40.4],
+            "e": [100, 200, 300, 400]
+        }, index=pd.date_range("2024-01-01", periods=4))
+        lib.write("sym", target)
+
+        source = pd.DataFrame({
+            "a": [10, 20, 30, 40, 50],
+            "b": ["a", "b", "c", "d", "e"],
+            "c": ["A", "D", "A", "B", "C"],
+            "d": [10.1, 50.5, 30.3, 40.4, 70.7],
+            "e": [100, 500, 600, 400, 800]
+        }, index=pd.DatetimeIndex([
+            "2024-01-01",      # index + b + d + e match (update)
+            "2024-01-02",      # index + b match, but d + e differ (insert)
+            "2024-01-03",      # index + b + d match, e differs (insert)
+            "2024-01-05",      # new index, b+d+e match (insert)
+            "2024-01-06"       # new index, new b, new d, new e (insert)
+        ]))
+
+        monkeypatch.setattr(lib.__class__, "merge", lambda *args, **kwargs: None, raising=False)
+        lib.merge("sym", source, on=["b", "d", "e"], strategy=MergeStrategy(not_matched=MergeAction.DO_NOTHING))
+        expected = pd.DataFrame({
+                "a": [10, 2, 20, 3, 30, 4, 40, 50],
+                "b": ["a", "b", "b", "c", "c", "d", "d", "e"],
+                "c": ["A", "B", "D", "A", "A", "C", "B", "C"],
+                "d": [10.1, 20.2, 50.5, 30.3, 30.3, 40.4, 40.4, 70.7],
+                "e": [100, 200, 500, 300, 600, 400, 400, 800]
+            }, index=pd.DatetimeIndex([
+                "2024-01-01",      # Updated: matched on b="a", d=10.1, e=100
+                "2024-01-02",      # Original target row
+                "2024-01-02",      # Inserted: no match for b="b", d=50.5, e=500
+                "2024-01-03",      # Original target row
+                "2024-01-03",      # Inserted: no match for b="c", d=30.3, e=600
+                "2024-01-04",      # Original target row
+                "2024-01-05",      # Inserted: no match for b="d", d=40.4, e=400 (different index)
+                "2024-01-06"       # Inserted: completely new b="e", d=70.7, e=800
+        ]))
+
+        monkeypatch.setattr(lib, "read", lambda *args, **kwargs: VersionedItem("sym", "lib", expected, 2))
+        received = lib.read("sym").data
+        assert_frame_equal(received, expected)
+
+
 
     class TestMergeRowRange:
         """Not implemented yet"""
