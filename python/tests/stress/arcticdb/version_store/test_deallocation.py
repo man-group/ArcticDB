06aec2f4e0b8b70d1cdc5a79b97568bc51aa46a6
grusev <george_rusev@yahoo.com>
2025-08-28 10:38:20 +0300

    xfail for now noisy test (#2621)
    #### Reference Issues/PRs
    <!--Example: Fixes #1234. See also #3456.-->
    
    #### What does this implement or fix?
    
    The test fails in almost every run at least once - an issue is created
    and this PR is to silence it until issue is resolved.
    
    #### Any other comments?
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>
    
    <!--
    Thanks for contributing a Pull Request to ArcticDB! Please ensure you
    have taken a look at:
    - ArcticDB's Code of Conduct:
    https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
    - ArcticDB's Contribution Licensing:
    https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
    -->

--- python/tests/stress/arcticdb/version_store/test_deallocation.py
+++ python/tests/stress/arcticdb/version_store/test_deallocation.py
@@ -39,6 +39,7 @@ def killed_worker(lib, io_threads, cpu_threads):
 
 @pytest.mark.parametrize("io_threads_spawned_in_child", [True, False])
 @pytest.mark.parametrize("cpu_threads_spawned_in_child", [True, False])
+@pytest.mark.xfail(reason="Intermittent failure 9917390284", strict=False)
 def test_os_exit_exits_within_timeout(lmdb_storage, lib_name, io_threads_spawned_in_child, cpu_threads_spawned_in_child):
     lib = lmdb_storage.create_arctic().create_library(lib_name)
     df = pd.DataFrame()
