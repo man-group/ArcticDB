man-group/ArcticDB

commit 2cb7ff2d607fa5e73b5f58a73b360102193fdfb2

[18102873455] Handle sparse string columns on arrow read (#2700)
#### Reference Issues/PRs
Monday ref: 18102873455

#### What does this implement or fix?
Change consists of two commits (which can be reviewed separately):
First one (tests and benchmarks):
- Adds a python test via ternary operator which reads sparse data
- Adds a benchmark to verify type handler performance
- Renames `test_arrow.py` to `test_arrow_read.py`

Second one (actual fix):
- Correctly handle sparse input string column and preserve the speed of
dense handling (by filling the inverted bitset)

#### Any other comments?
This change slightly slows down (`503us` -> `594us`) string processing
for arrow (most noticably if very few unique strings).
Experimentation shows that 100% of the slowdown can be attributed to
failure to inline [this lambda
function](https://github.com/man-group/ArcticDB/pull/2700/files#diff-4e399a492a48cda55aa054cc5f6f30a73f35d8ff3aa11dbd32a1f8f2b17598b4R77-R94)
argument to `for_each_enumerated`.

If we unroll `for_each_enumerated` performance goes back to original
level.

This slowdown will be fixed in a follow up PR. (which might positively
impact a lot of processing pipeline affected by a similar issue)


Benchmark results:
```
benchmarks --benchmark_filter="BM_arrow_string.*" --benchmark_min_time=2000x --benchmark_repetitions=5 --benchmark_time_unit=us

After first commit:
-----------------------------------------------------------------------------------------
Benchmark                                               Time             CPU   Iterations
-----------------------------------------------------------------------------------------
BM_arrow_string_handler/10000/1/0_mean               59.7 us         65.1 us            5
BM_arrow_string_handler/10000/1/0_stddev             1.19 us         1.29 us            5

BM_arrow_string_handler/100000/1/0_mean               503 us          544 us            5

cpp/arcticdb/CMakeLists.txt
@@ -1105,6 +1105,7 @@ if(${TEST})
     set(benchmark_srcs
             stream/test/stream_test_common.cpp
             arrow/test/arrow_test_utils.hpp
+            arrow/test/benchmark_arrow_reads.cpp
             arrow/test/benchmark_arrow_writes.cpp
             column_store/test/benchmark_column.cpp
             column_store/test/benchmark_memory_segment.cpp

cpp/arcticdb/arrow/arrow_handlers.cpp
@@ -11,6 +11,7 @@
 #include <arcticdb/util/decode_path_data.hpp>
 #include <arcticdb/pipeline/column_mapping.hpp>
 #include <arcticdb/column_store/string_pool.hpp>
+#include <arcticdb/column_store/column_algorithms.hpp>
 
 namespace arcticdb {
 
@@ -51,7 +52,6 @@ void ArrowStringHandler::convert_type(
         std::any&, const std::shared_ptr<StringPool>& string_pool
 ) const {
     using ArcticStringColumnTag = ScalarTagType<DataTypeTag<DataType::UTF_DYNAMIC64>>;
-    auto input_data = source_column.data();
     struct DictEntry {
         int32_t offset_buffer_pos_;
         int64_t string_buffer_pos_;
@@ -65,49 +65,49 @@ void ArrowStringHandler::convert_type(
     unique_offsets.reserve(source_column.row_count());
     int64_t bytes = 0;
     int32_t unique_offset_count = 0;
-    auto dest_ptr = reinterpret_cast<int32_t*>(
-            dest_column.bytes_at(mapping.offset_bytes_, source_column.row_count() * sizeof(int32_t))
-    );
+    auto dest_ptr = reinterpret_cast<int32_t*>(dest_column.bytes_at(mapping.offset_bytes_, mapping.dest_bytes_));
 
-    util::BitSet bitset;
-    util::BitSet::bulk_insert_iterator inserter(bitset);
-    const auto end = input_data.cend<ArcticStringColumnTag, IteratorType::ENUMERATED>();
-    // First go through the source column once to compute the size of offset and string buffers.
-    // TODO: This can't be right if the column was sparse as it has only been decoded, not expanded
-    for (auto en = input_data.cbegin<ArcticStringColumnTag, IteratorType::ENUMERATED>(); en != end; ++en) {
-        if (is_a_string(en->value())) {
+    util::BitSet dest_bitset;
+    util::BitSet::bulk_insert_iterator inserter(dest_bitset);
+    // For dense columns we populate an inverted bitmap (i.e. set the bits where values are missing)
+    // This makes processing fully dense columns faster because it doesn't need to insert often into the bitmap.
+    // Benchmarks in benchmark_arrow_reads.cpp show a 20% speedup with this approach for a dense string column with few
+    // unique strings.
+    bool populate_inverted_bitset = !source_column.opt_sparse_map().has_value();
+    for_each_enumerated<ArcticStringColumnTag>(source_column, [&](const auto& en) {
+        if (is_a_string(en.value())) {
             auto [entry, is_emplaced] = unique_offsets.try_emplace(
-                    en->value(),
-                    DictEntry{
-                            static_cast<int32_t>(unique_offset_count), bytes, string_pool->get_const_view(en->value())
-                    }
+                    en.value(), DictEntry{unique_offset_count, bytes, string_pool->get_const_view(en.value())}
             );
             if (is_emplaced) {
                 bytes += entry->second.strv.size();
-                unique_offsets_in_order.push_back(en->value());
+                unique_offsets_in_order.push_back(en.value());
                 ++unique_offset_count;
             }
-            *dest_ptr = entry->second.offset_buffer_pos_;
-        } else {
-            inserter = en->idx();
+            dest_ptr[en.idx()] = entry->second.offset_buffer_pos_;
+            if (!populate_inverted_bitset) {
+                inserter = en.idx();
+            }
+        } else if (populate_inverted_bitset) {
+            inserter = en.idx();
         }
-        ++dest_ptr;
-    }
+    });
     inserter.flush();
-    // At this point bitset has ones where the source column contained None or NaN
-    // Inverting and shrinking to the source column size it then makes a sparse map for the input data
-    bitset.invert();
-    // TODO: row_count() here won't be right when the original data was sparse, but we don't support sparse
-    // string columns yet anyway
-    bitset.resize(source_column.row_count());
-    if (bitset.count() != bitset.size()) {
-        handle_truncation(bitset, mapping.truncate_);
-        create_dense_bitmap(mapping.offset_bytes_, bitset, dest_column, AllocationType::DETACHABLE);
-    } // else there weren't any Nones or NaNs
-    // bitset.count() == 0 is the special case where all of the rows contained None or NaN. In this case, do not create
+    if (populate_inverted_bitset) {
+        // For dense columns at this point bitset has ones where the source column is missing values.
+        // We invert it back, so we can use it for the sparse map on the output column.
+        dest_bitset.invert();
+    }
+    dest_bitset.resize(mapping.num_rows_);
+
+    if (dest_bitset.count() != dest_bitset.size()) {
+        handle_truncation(dest_bitset, mapping.truncate_);
+        create_dense_bitmap(mapping.offset_bytes_, dest_bitset, dest_column, AllocationType::DETACHABLE);
+    } // else there weren't any missing values
+    // bitset.count() == 0 is the special case where all the rows were missing. In this case, do not create
     // the extra string and offset buffers. string_dict_from_block will then do the right thing and call
     // minimal_strings_dict
-    if (bitset.count() > 0) {
+    if (dest_bitset.count() > 0) {
         auto& string_buffer = dest_column.create_extra_buffer(
                 mapping.offset_bytes_, ExtraBufferType::STRING, bytes, AllocationType::DETACHABLE
         );

cpp/arcticdb/arrow/test/arrow_test_utils.hpp
@@ -5,10 +5,12 @@
  * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software
  * will be governed by the Apache License, version 2.0.
  */
+#pragma once
 
 #include <sparrow/record_batch.hpp>
 
 #include <arcticdb/util/allocator.hpp>
+#include <arcticdb/column_store/column.hpp>
 
 using namespace arcticdb;
 
@@ -30,10 +32,33 @@ sparrow::array create_array(const std::vector<T>& data) {
     }
 }
 
-sparrow::record_batch create_record_batch(const std::vector<std::pair<std::string, sparrow::array>>& columns) {
+inline sparrow::record_batch create_record_batch(const std::vector<std::pair<std::string, sparrow::array>>& columns) {
     sparrow::record_batch record_batch{};
     for (const auto& column : columns) {
         record_batch.add_column(column.first, column.second);
     }
     return record_batch;
+}
+
+template<typename RawType>
+void allocate_and_fill_chunked_column(
+        Column& column, size_t num_rows, size_t chunk_size, std::optional<std::span<RawType>> values = std::nullopt
+) {
+    // Allocate column in chunks
+    for (size_t row = 0; row < num_rows; row += chunk_size) {
+        auto data_size = data_type_size(column.type(), OutputFormat::ARROW, DataTypeMode::EXTERNAL);
+        auto current_block_size = std::min(chunk_size, num_rows - row);
+        auto bytes = current_block_size * data_size;
+        column.allocate_data(bytes);
+        column.advance_data(bytes);
+    }
+
+    // Actually fill the data
+    for (size_t row = 0; row < num_rows; ++row) {
+        if (values.has_value()) {
+            column.reference_at<RawType>(row) = values.value()[row];
+        } else {
+            column.reference_at<RawType>(row) = static_cast<RawType>(row);
+        }
+    }
 }

cpp/arcticdb/arrow/test/benchmark_arrow_reads.cpp
@@ -1,0 +1,78 @@
+/* Copyright 2025 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software
+ * will be governed by the Apache License, version 2.0.
+ */
+
+#include <benchmark/benchmark.h>
+#include <fmt/format.h>
+
+#include <arcticdb/arrow/test/arrow_test_utils.hpp>
+#include <arcticdb/arrow/arrow_handlers.hpp>
+#include <arcticdb/column_store/string_pool.hpp>
+#include <arcticdb/pipeline/column_mapping.hpp>
+
+using namespace arcticdb;
+
+// run like: --benchmark_time_unit=ms --benchmark_filter=.* --benchmark_min_time=5x
+
+static void BM_arrow_string_handler(benchmark::State& state) {
+    const auto num_rows = state.range(0);
+    const auto num_different_strings = state.range(1);
+    const auto num_sparse = state.range(2);
+
+    const double sparsity_ratio = num_sparse / num_rows;
+    auto source_type_desc = TypeDescriptor{DataType::UTF_DYNAMIC64, Dimension::Dim0};
+    auto dest_type_desc = TypeDescriptor{DataType::UTF_DYNAMIC32, Dimension::Dim0};
+    auto dest_size = data_type_size(dest_type_desc, OutputFormat::ARROW, DataTypeMode::EXTERNAL);
+    auto sparsity = num_sparse == 0 ? Sparsity::NOT_PERMITTED : Sparsity::PERMITTED;
+
+    // Fill up source column
+    auto source_column = Column(source_type_desc, num_rows, AllocationType::DYNAMIC, sparsity);
+    auto string_pool = std::make_shared<StringPool>();
+    for (auto i = 0u, num_set = 0u; i < num_rows; i++) {
+        auto offset = string_pool->get(fmt::format("str_{}", i % num_different_strings)).offset();
+        auto expected_set = std::round((i + 1) * (1 - sparsity_ratio));
+        if (num_set < expected_set) {
+            source_column.set_scalar(i, offset);
+            ++num_set;
+        }
+    }
+    auto mapping = ColumnMapping(
+            source_type_desc,
+            dest_type_desc,
+            FieldWrapper(dest_type_desc, "col").field(),
+            dest_size,
+            num_rows,
+            0,
+            0,
+            dest_size * num_different_strings,
+            0
+    );
+    auto handler = ArrowStringHandler();
+    auto handler_data = std::any{};
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        auto dest_column = Column(dest_type_desc, 0, AllocationType::DETACHABLE, sparsity);
+        allocate_and_fill_chunked_column<int32_t>(dest_column, num_rows, num_rows);
+        state.ResumeTiming();
+        handler.convert_type(source_column, dest_column, mapping, DecodePathData{}, handler_data, string_pool);
+    }
+}
+
+BENCHMARK(BM_arrow_string_handler)
+        // Not sparse, small string and offset buffers
+        ->Args({10'000, 1, 0})
+        ->Args({100'000, 1, 0})
+        // Not sparse, large string and offset buffers
+        ->Args({10'000, 10'000, 0})
+        ->Args({100'000, 100'000, 0})
+        // Half sparse
+        ->Args({10'000, 1, 5'000})
+        ->Args({100'000, 1, 50'000})
+        // Fully sparse
+        ->Args({10'000, 1, 10'000})
+        ->Args({100'000, 1, 100'000});

cpp/arcticdb/arrow/test/test_arrow_read.cpp
@@ -12,33 +12,11 @@
 
 #include <arcticdb/pipeline/column_mapping.hpp>
 #include <arcticdb/stream/test/stream_test_common.hpp>
+#include <arcticdb/arrow/test/arrow_test_utils.hpp>
 #include <arcticdb/arrow/arrow_utils.hpp>
 #include <arcticdb/arrow/arrow_handlers.hpp>
 
 using namespace arcticdb;
-
-template<typename RawType>
-void allocate_and_fill_chunked_column(
-        Column& column, size_t num_rows, size_t chunk_size, std::optional<std::span<RawType>> values = std::nullopt
-) {
-    // Allocate column in chunks
-    for (size_t row = 0; row < num_rows; row += chunk_size) {
-        auto data_size = data_type_size(column.type(), OutputFormat::ARROW, DataTypeMode::EXTERNAL);
-        auto current_block_size = std::min(chunk_size, num_rows - row);
-        auto bytes = current_block_size * data_size;
-        column.allocate_data(bytes);
-        column.advance_data(bytes);
-    }
-
-    // Actually fill the data
-    for (size_t row = 0; row < num_rows; ++row) {
-        if (values.has_value()) {
-            column.reference_at<RawType>(row) = values.value()[row];
-        } else {
-            column.reference_at<RawType>(row) = static_cast<RawType>(row);
-        }
-    }
-}
 
 SegmentInMemory get_detachable_segment(
         StreamId symbol, std::span<const FieldRef> fields, size_t num_rows, size_t chunk_size

python/tests/conftest.py
@@ -1124,6 +1124,14 @@ def lmdb_version_store_dynamic_schema(
 
 
 @pytest.fixture
+def lmdb_version_store_dynamic_schema_arrow(lmdb_version_store_dynamic_schema_v1) -> NativeVersionStore:
+    store = lmdb_version_store_dynamic_schema_v1
+    store.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
+    store._set_allow_arrow_input()
+    return store
+
+
+@pytest.fixture
 def lmdb_version_store_empty_types_v1(version_store_factory, lib_name) -> NativeVersionStore:
     library_name = lib_name + "_v1"
     return version_store_factory(dynamic_strings=True, empty_types=True, name=library_name)

python/tests/unit/arcticdb/version_store/test_arrow_read.py
@@ -8,7 +8,7 @@ from hypothesis.extra.pandas import columns, data_frames
 
 from arcticdb.util.test import assert_frame_equal, assert_frame_equal_with_arrow, stringify_dictionary_encoded_columns
 from arcticdb.exceptions import SchemaException
-from arcticdb.version_store.processing import QueryBuilder
+from arcticdb.version_store.processing import QueryBuilder, where
 from arcticdb.options import OutputFormat
 import pyarrow as pa
 import pyarrow.compute as pc
@@ -524,13 +524,12 @@ def test_arrow_layout(lmdb_version_store_tiny_segment):
         pa.float64(),
     ],
 )
-def test_arrow_dynamic_schema_changing_types(lmdb_version_store_dynamic_schema_v1, first_type, second_type):
+def test_arrow_dynamic_schema_changing_types(lmdb_version_store_dynamic_schema_arrow, first_type, second_type):
     if (pa.types.is_uint64(first_type) and pa.types.is_signed_integer(second_type)) or (
         pa.types.is_uint64(second_type) and pa.types.is_signed_integer(first_type)
     ):
         pytest.skip("Unsupported ArcticDB type combination")
-    lib = lmdb_version_store_dynamic_schema_v1
-    lib.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
+    lib = lmdb_version_store_dynamic_schema_arrow
     sym = "test_arrow_dynamic_schema_changing_types"
     write_table = pa.table({"col": pa.array([0], first_type)})
     append_table = pa.table({"col": pa.array([1], second_type)})
@@ -561,9 +560,8 @@ def test_arrow_dynamic_schema_missing_columns_numeric(version_store_factory, row
 
 
 @pytest.mark.parametrize("rows_per_column", [1, 7, 8, 9, 100_000])
-def test_arrow_dynamic_schema_missing_columns_strings(lmdb_version_store_dynamic_schema_v1, rows_per_column):
-    lib = lmdb_version_store_dynamic_schema_v1
-    lib.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
+def test_arrow_dynamic_schema_missing_columns_strings(lmdb_version_store_dynamic_schema_arrow, rows_per_column):
+    lib = lmdb_version_store_dynamic_schema_arrow
     sym = "test_arrow_dynamic_schema_missing_columns_strings"
     write_table = pa.table({"col1": pa.array(["hello"] * rows_per_column, pa.string())})
     append_table = pa.table({"col2": pa.array(["goodbye"] * rows_per_column, pa.string())})
@@ -629,11 +627,10 @@ def combinable_numeric_dtypes(draw):
     ),
 )
 def test_arrow_dynamic_schema_missing_columns_hypothesis(
-    lmdb_version_store_dynamic_schema_v1, df_0, df_1, df_2, columns_0, columns_1, columns_2
+    lmdb_version_store_dynamic_schema_arrow, df_0, df_1, df_2, columns_0, columns_1, columns_2
 ):
     assume(len(df_0) and len(df_1) and len(df_2))
-    lib = lmdb_version_store_dynamic_schema_v1
-    lib.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
+    lib = lmdb_version_store_dynamic_schema_arrow
     sym = "test_arrow_dynamic_schema_missing_columns_hypothesis"
     df_0 = df_0[columns_0]
     df_1 = df_1[columns_1]
@@ -773,9 +770,8 @@ def test_arrow_sparse_floats_hypothesis(lmdb_version_store_arrow, df, rows_per_s
 
 
 @pytest.mark.parametrize("type_to_drop", [pa.int64(), pa.float64(), pa.large_string()])
-def test_arrow_dynamic_schema_filtered_column(lmdb_version_store_dynamic_schema_v1, type_to_drop):
-    lib = lmdb_version_store_dynamic_schema_v1
-    lib.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
+def test_arrow_dynamic_schema_filtered_column(lmdb_version_store_dynamic_schema_arrow, type_to_drop):
+    lib = lmdb_version_store_dynamic_schema_arrow
     sym = "sym"
     column_to_drop = (
         pa.array(["a", "b"], type_to_drop) if type_to_drop == pa.large_string() else pa.array([1, 2], type_to_drop)
@@ -795,8 +791,8 @@ def test_arrow_dynamic_schema_filtered_column(lmdb_version_store_dynamic_schema_
     assert expected.equals(received)
 
 
-def test_project_dynamic_schema(lmdb_version_store_dynamic_schema_v1):
-    lib = lmdb_version_store_dynamic_schema_v1
+def test_project_dynamic_schema(lmdb_version_store_dynamic_schema_arrow):
+    lib = lmdb_version_store_dynamic_schema_arrow
     lib.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
     sym = "sym"
     table_1 = pa.table({"a": pa.array([1, 2])})
@@ -814,9 +810,8 @@ def test_project_dynamic_schema(lmdb_version_store_dynamic_schema_v1):
     assert expected.equals(received)
 
 
-def test_project_dynamic_schema_complex(lmdb_version_store_dynamic_schema_v1):
-    lib = lmdb_version_store_dynamic_schema_v1
-    lib.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
+def test_project_dynamic_schema_complex(lmdb_version_store_dynamic_schema_arrow):
+    lib = lmdb_version_store_dynamic_schema_arrow
     sym = "sym"
     df = pd.DataFrame(
         {
@@ -838,9 +833,8 @@ def test_project_dynamic_schema_complex(lmdb_version_store_dynamic_schema_v1):
     assert_frame_equal_with_arrow(table, expected)
 
 
-def test_aggregation_empty_slices(lmdb_version_store_dynamic_schema_v1):
-    lib = lmdb_version_store_dynamic_schema_v1
-    lib.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
+def test_aggregation_empty_slices(lmdb_version_store_dynamic_schema_arrow):
+    lib = lmdb_version_store_dynamic_schema_arrow
     sym = "sym"
     df_1 = pd.DataFrame(
         {
@@ -882,9 +876,8 @@ def test_aggregation_empty_slices(lmdb_version_store_dynamic_schema_v1):
     assert_frame_equal_with_arrow(table, expected)
 
 
-def test_resample_empty_slices(lmdb_version_store_dynamic_schema_v1):
-    lib = lmdb_version_store_dynamic_schema_v1
-    lib.set_output_format(OutputFormat.EXPERIMENTAL_ARROW)
+def test_resample_empty_slices(lmdb_version_store_dynamic_schema_arrow):
+    lib = lmdb_version_store_dynamic_schema_arrow
     sym = "sym"
 
     def gen_df(start, num_rows, with_columns=True):
@@ -973,4 +966,40 @@ def test_symbol_concat_empty_intersection(lmdb_version_store_arrow):
     assert table.column_names == []
     assert table.shape == (0, 0)
     expected = pd.DataFrame()
+    assert_frame_equal_with_arrow(table, expected)
+
+
+def test_sparse_strings_from_processing_pipeline(lmdb_version_store_dynamic_schema_arrow):
+    lib = lmdb_version_store_dynamic_schema_arrow
+    sym = "test_sparse_strings_from_processing_pipeline"
+    df_1 = pd.DataFrame(
+        {
+            "condition": [True, False],
+            "col_1": ["a", "b"],
+            "col_2": ["cc", "dd"],
+        }
+    )
+    df_2 = pd.DataFrame(
+        {
+            "condition": [i % 4 == 0 for i in range(10)],
+            "col_2": [chr(i + ord("a")) * (i + 1) if i % 3 > 0 else None for i in range(10)],
+        }
+    )
+    df_3 = pd.DataFrame(
+        {
+            "condition": [i % 3 == 0 for i in range(10)],
+            "col_1": [chr(10 + i + ord("a")) * (i + 1) if i % 2 == 0 else np.nan for i in range(10)],
+        }
+    )
+    lib.write(sym, df_1)
+    lib.append(sym, df_2)
+    lib.append(sym, df_3)
+
+    q = QueryBuilder()
+    q = q.apply("new_col", where(q["condition"], q["col_1"], q["col_2"]))
+    table = lib.read(sym, query_builder=q).data
+
+    expected = pd.concat([df_1, df_2, df_3])
+    expected["new_col"] = np.where(expected["condition"].to_numpy(), expected["col_1"], expected["col_2"])
+    expected.reset_index(drop=True, inplace=True)
     assert_frame_equal_with_arrow(table, expected)

commit 237945a6b68f23b9142036d61f81fc5d5b642a70

Improve lambda inlining when using `for_each_enumerated`
This should fix the performance regression seen in #2700
In a follow up commit we will improve performance in other
pipeline processing which uses `for_each_enumerated`.

cpp/arcticdb/CMakeLists.txt
@@ -532,6 +532,7 @@ set(arcticdb_srcs
         util/decimal.cpp
         util/error_code.cpp
         util/global_lifetimes.cpp
+        util/lambda_inlining.hpp
         util/memory_mapped_file.hpp
         util/name_validation.cpp
         util/offset_string.cpp
@@ -553,7 +554,7 @@ set(arcticdb_srcs
         version/symbol_list.cpp
         version/version_map_batch_methods.cpp
         storage/s3/ec2_utils.cpp
-        )
+)
 
 add_library(arcticdb_core_object OBJECT ${arcticdb_srcs})
 

cpp/arcticdb/arrow/arrow_handlers.cpp
@@ -9,6 +9,7 @@
 #include <arcticdb/codec/encoding_sizes.hpp>
 #include <arcticdb/codec/codec.hpp>
 #include <arcticdb/util/decode_path_data.hpp>
+#include <arcticdb/util/lambda_inlining.hpp>
 #include <arcticdb/pipeline/column_mapping.hpp>
 #include <arcticdb/column_store/string_pool.hpp>
 #include <arcticdb/column_store/column_algorithms.hpp>
@@ -74,24 +75,27 @@ void ArrowStringHandler::convert_type(
     // Benchmarks in benchmark_arrow_reads.cpp show a 20% speedup with this approach for a dense string column with few
     // unique strings.
     bool populate_inverted_bitset = !source_column.opt_sparse_map().has_value();
-    for_each_enumerated<ArcticStringColumnTag>(source_column, [&](const auto& en) {
-        if (is_a_string(en.value())) {
-            auto [entry, is_emplaced] = unique_offsets.try_emplace(
-                    en.value(), DictEntry{unique_offset_count, bytes, string_pool->get_const_view(en.value())}
-            );
-            if (is_emplaced) {
-                bytes += entry->second.strv.size();
-                unique_offsets_in_order.push_back(en.value());
-                ++unique_offset_count;
+    for_each_enumerated<ArcticStringColumnTag>(
+            source_column,
+            [&] ARCTICDB_LAMBDA_INLINE_PRE(const auto& en) ARCTICDB_LAMBDA_INLINE_MID ARCTICDB_LAMBDA_INLINE_POST {
+                if (is_a_string(en.value())) {
+                    auto [entry, is_emplaced] = unique_offsets.try_emplace(
+                            en.value(), DictEntry{unique_offset_count, bytes, string_pool->get_const_view(en.value())}
+                    );
+                    if (is_emplaced) {
+                        bytes += entry->second.strv.size();
+                        unique_offsets_in_order.push_back(en.value());
+                        ++unique_offset_count;
+                    }
+                    dest_ptr[en.idx()] = entry->second.offset_buffer_pos_;
+                    if (!populate_inverted_bitset) {
+                        inserter = en.idx();
+                    }
+                } else if (populate_inverted_bitset) {
+                    inserter = en.idx();
+                }
             }
-            dest_ptr[en.idx()] = entry->second.offset_buffer_pos_;
-            if (!populate_inverted_bitset) {
-                inserter = en.idx();
-            }
-        } else if (populate_inverted_bitset) {
-            inserter = en.idx();
-        }
-    });
+    );
     inserter.flush();
     if (populate_inverted_bitset) {
         // For dense columns at this point bitset has ones where the source column is missing values.

cpp/arcticdb/util/lambda_inlining.hpp
@@ -1,0 +1,27 @@
+#pragma once
+
+#if defined(__clang__)
+// Inlining for clang
+#define ARCTICDB_LAMBDA_INLINE_MID __attribute__((always_inline))
+#elif defined(__GNUC__)
+// Inlining for recent versions of gcc
+#define ARCTICDB_LAMBDA_INLINE_PRE [[gnu::always_inline]]
+#elif defined(_MSC_VER) && _MSC_VER >= 1927 && __cplusplus >= 202002L
+// MSVC 16.7 and newer specific syntax, requires C++20 language standard to be enabled
+#define ARCTICDB_LAMBDA_INLINE_POST [[msvc::forceinline]]
+#endif
+
+#ifndef ARCTICDB_LAMBDA_INLINE_PRE
+#define ARCTICDB_LAMBDA_INLINE_PRE
+#endif
+#ifndef ARCTICDB_LAMBDA_INLINE_MID
+#define ARCTICDB_LAMBDA_INLINE_MID
+#endif
+#ifndef ARCTICDB_LAMBDA_INLINE_POST
+#define ARCTICDB_LAMBDA_INLINE_POST
+#endif
+
+// Example usage of inlining a lambda looks like:
+// [&] ARCTICDB_LAMBDA_INLINE_PRE (int arg) ARCTICDB_LAMBDA_INLINE_MID noexcept ARCTICDB_LAMBDA_INLINE_POST -> int {
+//     return arg+5;
+// }