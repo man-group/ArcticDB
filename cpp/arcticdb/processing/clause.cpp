78861085a7848736c17163f1bd33930d65f9aadd
Vasil Pashov <vasil.pashov1@gmail.com>
2025-10-24 18:26:21 +0300

    Fix compilation errors

--- cpp/arcticdb/processing/clause.cpp
+++ cpp/arcticdb/processing/clause.cpp
@@ -548,8 +548,8 @@ std::vector<EntityId> AggregationClause::process(std::vector<EntityId>&& entity_
 
         auto column_data = index_col->data();
         std::transform(
-                elements.begin(),
-                elements.begin(),
+                elements.cbegin(),
+                elements.cend(),
                 column_data.begin<typename col_type_info::TDT>(),
                 [](const auto& element) { return element.first; }
         );
