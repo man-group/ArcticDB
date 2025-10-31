19284cf8e9545326ded45a4e969f9cfc1a4ba32a
phoebusm <phoebusmak@gmail.com>
2025-10-31 13:54:38 +0000

    Fix seg fault

--- cpp/arcticdb/version/version_core.cpp
+++ cpp/arcticdb/version/version_core.cpp
@@ -2018,7 +2018,7 @@ folly::Future<SegmentInMemory> do_direct_read_or_process(
         ARCTICDB_SAMPLE(RunPipelineAndOutput, 0)
         util::check_rte(!pipeline_context->is_pickled(), "Cannot filter pickled data");
         return read_process_and_collect(store, pipeline_context, read_query, read_options)
-                .thenValue([store, pipeline_context, &read_options, &handler_data](std::vector<SliceAndKey>&& segs) {
+                .thenValue([store, pipeline_context, read_options, &handler_data](std::vector<SliceAndKey>&& segs) {
                     return prepare_output_frame(std::move(segs), pipeline_context, store, read_options, handler_data);
                 });
     } else {
