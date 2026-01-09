282e023d8aea233b1c573495f63de42f876c049a
Alex Seaton <alexbseaton@gmail.com>
2026-01-09 12:19:40 +0000

    Support multiple storages in list_versions.py

--- python/benchmarks/list_versions.py
+++ python/benchmarks/list_versions.py
@@ -5,13 +5,13 @@ Use of this software is governed by the Business Source License 1.1 included in 
 
 As of the Change Date specified in that file, in accordance with the Business Source License, use of this software will be governed by the Apache License, version 2.0.
 """
-
+import itertools
 import random
-import time
-from arcticdb import Arctic, WritePayload
+from arcticdb import WritePayload
 from asv_runner.benchmarks.mark import SkipNotImplemented
 
 from benchmarks.common import *
+from benchmarks.environment_setup import Storage, create_libraries_across_storages, is_storage_enabled, create_libraries
 
 
 class ListVersions:
@@ -21,6 +21,7 @@ class ListVersions:
     warmup_time = 0.1
 
     param_names = [
+        "storage",
         "num_symbols",
         "num_versions",
         "num_snapshots",
@@ -30,6 +31,7 @@ class ListVersions:
         "skip_snapshots",
     ]
     params = [
+        [Storage.LMDB, Storage.AMAZON],
         [10, 100],
         [1, 10],
         [0, 100],
@@ -42,18 +44,15 @@ class ListVersions:
     def __init__(self):
         self.logger = get_logger()
 
-    def setup_cache(self):
-        start = time.time()
-        self._setup_cache()
-        self.logger.info(f"SETUP_CACHE TIME: {time.time() - start}")
-
     def check_if_skipped_parameter(
-        self, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots
+        self, storage, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots
     ):
         """At the time of writing there are two choices for each parameter. This is 128 parameterizations in total if
         we do not skip any.
 
         This function skips less interesting combinations, so that the benchmark executes faster."""
+        if not is_storage_enabled(storage):
+            raise SkipNotImplemented
 
         if symbol and num_symbols != 10:
             # Cost is independent of the number of symbols when symbol is specified
@@ -75,45 +74,56 @@ class ListVersions:
     def _snap_name(self, snap_idx):
         return f"{snap_idx}_snap"
 
-    def _setup_cache(self):
-        self.ac = Arctic("lmdb://list_versions")
+    def setup_cache(self):
+        storages = self.params[0]
+        num_symbols = self.params[1]
+        num_versions = self.params[2]
+        num_snapshots = self.params[3]
 
-        num_symbols = self.params[0]
-        num_versions = self.params[1]
-        num_snapshots = self.params[2]
+        libs_for_storage = dict()
 
-        for syms in num_symbols:
-            write_payloads = [WritePayload(self._sym_name(sym), 0) for sym in range(syms)]
-            for versions in num_versions:
-                for snapshots in num_snapshots:
-                    lib_name = self._lib_name(syms, versions, snapshots)
-                    self.ac.delete_library(lib_name)
-                    lib = self.ac.create_library(lib_name)
-                    for _ in range(versions):
-                        lib.write_pickle_batch(write_payloads)
+        library_names = [self._lib_name(n_symbols, n_versions, n_snaps) for n_symbols, n_versions, n_snaps in
+                         itertools.product(num_symbols, num_versions, num_snapshots)]
 
-                    for snapshot in range(snapshots):
-                        lib.snapshot(
-                            self._snap_name(snapshot),
-                            versions={self._sym_name(sym): random.randint(0, versions - 1) for sym in range(syms)},
-                        )
+        for storage in storages:
+            if not is_storage_enabled(storage):
+                continue
+            libraries = create_libraries(storage, library_names)
+            libs_for_storage[storage] = dict(zip(library_names, libraries))
+            for syms in num_symbols:
+                write_payloads = [WritePayload(self._sym_name(sym), 0) for sym in range(syms)]
+                for versions in num_versions:
+                    for snapshots in num_snapshots:
+                        lib_name = self._lib_name(syms, versions, snapshots)
+                        lib = libs_for_storage[storage][lib_name]
+                        for _ in range(versions):
+                            lib.write_pickle_batch(write_payloads)
 
-    def teardown(self, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots):
+                        for snapshot in range(snapshots):
+                            lib.snapshot(
+                                self._snap_name(snapshot),
+                                versions={self._sym_name(sym): random.randint(0, versions - 1) for sym in range(syms)},
+                            )
+
+        return libs_for_storage
+
+    def teardown(self, *args):
         pass
 
-    def setup(self, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots):
+    def setup(self, lib_for_storage, storage, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots):
         self.check_if_skipped_parameter(
-            num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots
+            storage, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots
         )
-        self.ac = Arctic("lmdb://list_versions")
-        self.lib = self.ac[self._lib_name(num_symbols, num_versions, num_snapshots)]
+        self.lib = lib_for_storage[storage][self._lib_name(num_symbols, num_versions, num_snapshots)]
+        if self.lib is None:
+            raise SkipNotImplemented
 
     def time_list_versions(
-        self, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots
+        self, lib_for_storage, storage, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots
     ):
         self.lib.list_versions(symbol=symbol, snapshot=snapshot, latest_only=latest_only, skip_snapshots=skip_snapshots)
 
     def peakmem_list_versions(
-        self, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots
+        self, lib_for_storage, storage, num_symbols, num_versions, num_snapshots, symbol, snapshot, latest_only, skip_snapshots
     ):
         self.lib.list_versions(symbol=symbol, snapshot=snapshot, latest_only=latest_only, skip_snapshots=skip_snapshots)
