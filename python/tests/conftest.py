cb4a32eddd00f3e167095c62596d93d851a32b42
phoebusm <phoebusmak@gmail.com>
2026-02-02 13:37:45 +0000

    Reset s3 real credentail in sts windows auth

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -41,6 +41,7 @@ from arcticdb.storage_fixtures.s3 import (
     MotoNfsBackedS3StorageFixtureFactory,
     NfsS3Bucket,
     S3Bucket,
+    Key,
     real_gcp_from_environment_variables,
     real_s3_from_environment_variables,
     mock_s3_with_error_simulation,
@@ -510,6 +511,7 @@ def real_s3_sts_storage_factory(monkeypatch_session) -> Generator[BaseS3StorageF
     set_config_int("S3Storage.STSTokenExpiryMin", 15)
     # monkeypatch cannot runtime update environment variables in windows as copy of environment is made at startup
     # Need to manually setup credetial beforehand if run locally
+    # CI run set the config file at .github/actions/set_s3_sts_persistent_storage_env_vars/action.yml
     if WINDOWS:
         config_file_path = os.path.expanduser(os.path.join("~", ".aws", "config"))
         f = real_s3_from_environment_variables(False, NativeVariantStorage(), "")
@@ -522,6 +524,9 @@ def real_s3_sts_storage_factory(monkeypatch_session) -> Generator[BaseS3StorageF
                 use_internal_client_wrapper_for_testing=False,
             )
         )
+        f.default_key = Key(
+            id="", secret="", user_name="unknown user"
+        )  # Reset to ensure client can't fallback to default key+secret auth
         yield f
     else:
         working_dir = mkdtemp(suffix="S3STSStorageFixtureFactory")
