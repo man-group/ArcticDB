74b572645c7441a0a74d2e18a8ad9bb019286e1f
Georgi Petrov <Georgi.Petrov@man.com>
2026-03-11 08:41:14 +0000

    Fix includes

--- cpp/arcticdb/storage/s3/aws_provider_chain.hpp
+++ cpp/arcticdb/storage/s3/aws_provider_chain.hpp
@@ -5,7 +5,7 @@
  * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software
  * will be governed by the Apache License, version 2.0.
  */
-
+#pragma once
 #include <aws/core/auth/AWSCredentialsProviderChain.h>
 
 namespace arcticdb::storage::s3 {
