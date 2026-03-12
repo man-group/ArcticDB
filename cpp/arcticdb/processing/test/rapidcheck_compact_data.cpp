40862852c04b359b93cb03600612a3016d552c05
Alex Owens <alexowens90@gmail.com>
2026-03-12 15:58:52 +0000

    Small test tidy up

--- cpp/arcticdb/processing/test/rapidcheck_compact_data.cpp
+++ cpp/arcticdb/processing/test/rapidcheck_compact_data.cpp
@@ -32,10 +32,11 @@ RC_GTEST_PROP(CompactData, StructureRowRanges, ()) {
         row_ranges.emplace(*row_range_boundary, *std::next(row_range_boundary));
     }
     CompactDataClause clause{rows_per_segment};
-    const auto res = clause.structure_row_ranges(row_ranges);
     // If there are fewer total rows than min_rows_per_segment_ then everything will be combined into one
     const auto min_rows_per_segment = std::min(clause.min_rows_per_segment_, row_range_boundaries.back());
     const auto max_rows_per_segment = clause.max_rows_per_segment_;
+
+    const auto res = clause.structure_row_ranges(row_ranges);
     for (const auto& row_range : res) {
         const auto rows = row_range.diff();
         RC_ASSERT(rows >= min_rows_per_segment);
@@ -54,12 +55,17 @@ RC_GTEST_PROP(CompactData, StructureForProcessing, ()) {
     RC_PRE(row_range_boundaries.size() >= 2);
     std::ranges::sort(row_range_boundaries);
     // Turn this sorted list of row range boundaries into a list of row ranges
-    std::vector<RowRange> row_ranges;
+    std::set<RowRange> row_ranges;
     for (auto row_range_boundary = row_range_boundaries.cbegin();
          row_range_boundary != std::prev(row_range_boundaries.cend());
          ++row_range_boundary) {
-        row_ranges.emplace_back(*row_range_boundary, *std::next(row_range_boundary));
+        row_ranges.emplace(*row_range_boundary, *std::next(row_range_boundary));
     }
+    CompactDataClause clause{rows_per_segment};
+    // If there are fewer total rows than min_rows_per_segment_ then everything will be combined into one
+    const auto min_rows_per_segment = std::min(clause.min_rows_per_segment_, row_range_boundaries.back());
+    const auto max_rows_per_segment = clause.max_rows_per_segment_;
+
     const auto cols_per_segment = *rc::gen::inRange<uint64_t>(1, 1'000);
     const auto num_col_slices = *rc::gen::inRange<uint64_t>(1, 10);
     std::vector<ColRange> col_ranges;
@@ -72,11 +78,7 @@ RC_GTEST_PROP(CompactData, StructureForProcessing, ()) {
             ranges_and_keys.emplace_back(row_range, col_range, AtomKey{});
         }
     }
-    CompactDataClause clause{rows_per_segment};
     auto proc_unit_ids = clause.structure_for_processing(ranges_and_keys);
-    // If there are fewer total rows than min_rows_per_segment_ then everything will be combined into one
-    const auto min_rows_per_segment = std::min(clause.min_rows_per_segment_, row_range_boundaries.back());
-    const auto max_rows_per_segment = clause.max_rows_per_segment_;
     for (const auto& proc_unit : proc_unit_ids) {
         RC_ASSERT_FALSE(proc_unit.empty());
         const auto& col_range = ranges_and_keys.at(proc_unit.front()).col_range();
