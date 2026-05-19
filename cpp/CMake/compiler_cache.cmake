fc2748bba307e7db0f97f60495d3e5ba3603edc1
Vasil Pashov <vasil.pashov@man.com>
2026-05-19 11:11:17 +0300

    CMake cache variables for compiler cache and PDB generation
    ArcticDB used to use /Z7 on windows which generates the debug info inside the .obj files. /Z7 has a limit of 4GB. This commit sets it to \Zi. Which will generate debug symbols in a separate PDB file.
    
    By default PDBs are generated per target. When a translation unit is done compiling it'll try taking a lock and appedning in the PDB. This makes the invocation of .cl nondeterministic which messes up compiler caches as the output of compilation can potentially always be different. To deal with this a new CMake cache variable is added ARCTICDB_PDB_GENERATION_MODE it can be either TARGET or TU, TARGET being the default. If it's TU it will generate a single PDB per TU. In theory not having to take a lock should make parallel builds faster, however MSBuild batches compilation by grouping on compiler flags. All TUs sharing the same flags are compiled in parallel together. Having different PDB means different flags, so ARCTICDB_PDB_GENERATION_MODE=TU forces MSBuild to compile on a single thread. Note this is only MSBuild issues, Ninja does not have this problem and our CI uses Ninja. ARCTICDB_PDB_GENERATION_MODE=TU does not work with PCH.
    
    This PR also adds a CMake cache variable for controlling compiler caches. By default it tries to find eiter sccache or ccache and uses that cache if CMAKE_C(XX)_COMPILER_LAUNCHER is not set.

---
+++ cpp/CMake/compiler_cache.cmake
@@ -0,0 +1,88 @@
+# Sets compiler cache by prepending to CMAKE_CXX_COMPILER_LAUNCHER and CMAKE_C_COMPILER_LAUNCHER based on the value of
+# the ARCTICDB_COMPILER_CACHE cache variable. Allowed values for the variable are AUTO|SCCACHE|CCACHE|OFF|CUSTOM
+# * OFF does noting
+# * SCCACHE|CCACHE the executables for the specified caches are sought. If they are not found the script fails.
+# * CUSOM prepends CMAKE_C(XX)_COMPILER_LAUNCHER with the value of ARCTICDB_COMPILER_CACHE_PATH. If
+#   ARCTICDB_COMPILER_CACHE_PATH is not set the script fails
+# * AUTO tries to find a compiler cache in the order sccache|ccache. If nothing is found the script exits (but does
+#   not fail). If the executable is found but CMAKE_C(XX)_COMPILER_LAUNCHER the script will not change anything.
+function(set_arcticdb_compiler_cache)
+    set(ALLOWED_VALUES "CCACHE" "SCCACHE" "OFF" "CUSTOM" "AUTO")
+    if(NOT ARCTICDB_COMPILER_CACHE IN_LIST ALLOWED_VALUES)
+        message(FATAL_ERROR "Unknown value of the ARCTICDB_COMPILER_CACHE variable: ${ARCTICDB_COMPILER_CACHE}")
+    endif()
+
+    if("${ARCTICDB_COMPILER_CACHE}" STREQUAL "OFF")
+        return()
+    endif()
+
+    if("${ARCTICDB_COMPILER_CACHE}" STREQUAL "CUSTOM")
+        if(NOT ARCTICDB_COMPILER_CACHE_PATH)
+            message(FATAL_ERROR
+                "ARCTICDB_COMPILER_CACHE set to ${ARCTICDB_COMPILER_CACHE} but ARCTICDB_COMPILER_CACHE_PATH was not set"
+            )
+        endif()
+        set(CACHE_PROGRAM_PATH ${ARCTICDB_COMPILER_CACHE_PATH})
+    elseif("${ARCTICDB_COMPILER_CACHE}" STREQUAL "AUTO")
+        set(CACHES_TO_TRY sccache ccache)
+        foreach(CACHE_PROGRAM_NAME ${CACHES_TO_TRY})
+            find_program(CACHE_PROGRAM_PATH ${CACHE_PROGRAM_NAME})
+            if(CACHE_PROGRAM_PATH)
+                break()
+            else()
+                unset(CACHE_PROGRAM_PATH CACHE)
+            endif()
+        endforeach()
+
+        if(NOT CACHE_PROGRAM_PATH)
+            message("ArcticDB could not find any compiler cache as a result of setting ARCTICDB_COMPILER_CACHE to AUTO")
+            return()
+        endif()
+    else()
+        string(TOLOWER ${ARCTICDB_COMPILER_CACHE} CACHE_PROGRAM_NAME)
+        find_program(CACHE_PROGRAM_PATH ${CACHE_PROGRAM_NAME})
+        if(NOT CACHE_PROGRAM_PATH)
+            message(FATAL_ERROR
+                "ARCTICDB_COMPILER_CACHE set to ${ARCTICDB_COMPILER_CACHE} but ${ARCTICDB_COMPILER_CACHE} cannot be
+                found. Consider setting ARCTICDB_COMPILER_CACHE to CUSTOM and using ARCTICDB_COMPILER_CACHE_PATH"
+            )
+        endif()
+    endif()
+
+    if(MSVC AND CACHE_PROGRAM_PATH AND NOT ${ARCTICDB_PDB_GENERATION_MODE} STREQUAL "TU")
+        message(FATAL_ERROR
+            "In order to use a compiler cache with MSVC a .pdb must be generated per each TU. Consider setting
+            ARCTICDB_PDB_GENERATION_MODE to TU."
+        )
+    endif()
+
+    
+    if(NOT "${ARCTICDB_COMPILER_CACHE}" STREQUAL "AUTO" OR NOT CMAKE_C_COMPILER_LAUNCHER)
+        if(CMAKE_CXX_COMPILER_LAUNCHER)
+            message(
+                WARNING
+                "Setting C compiler cache ${CACHE_PROGRAM_PATH} on top of non-empty CMAKE_C_COMPILER_LAUNCHER: \
+                ${CMAKE_CXX_COMPILER_LAUNCHER}"
+            )
+        else()
+            message("ArcticDB using C compiler cache ${CACHE_PROGRAM_PATH}. ARCTICDB_COMPILER_CACHE: ${ARCTICDB_COMPILER_CACHE}")
+        endif()
+        list(PREPEND CMAKE_C_COMPILER_LAUNCHER ${CACHE_PROGRAM_PATH})
+        set(CMAKE_C_COMPILER_LAUNCHER ${CMAKE_C_COMPILER_LAUNCHER} PARENT_SCOPE)
+    endif()
+
+    if(NOT "${ARCTICDB_COMPILER_CACHE}" STREQUAL "AUTO" OR NOT CMAKE_CXX_COMPILER_LAUNCHER)
+        message("ArcticDB using CXX compiler cache ${CACHE_PROGRAM_PATH}. ARCTICDB_COMPILER_CACHE: ${ARCTICDB_COMPILER_CACHE}")
+        if(CMAKE_CXX_COMPILER_LAUNCHER)
+            message(
+                WARNING
+                "Setting CXX compiler cache ${CACHE_PROGRAM_PATH} on top of non-empty CMAKE_CXX_COMPILER_LAUNCHER: \
+                ${CMAKE_CXX_COMPILER_LAUNCHER}"
+            )
+        else()
+            message("ArcticDB using CXX compiler cache ${CACHE_PROGRAM_PATH}. ARCTICDB_COMPILER_CACHE: ${ARCTICDB_COMPILER_CACHE}")
+        endif()
+        list(PREPEND CMAKE_CXX_COMPILER_LAUNCHER ${CACHE_PROGRAM_PATH})
+        set(CMAKE_CXX_COMPILER_LAUNCHER ${CMAKE_CXX_COMPILER_LAUNCHER} PARENT_SCOPE)
+    endif()
+endfunction()
---
+++ cpp/CMake/pdb_generation.cmake
@@ -0,0 +1,63 @@
+set(_ARCTICDB_ARCTICDB_PDB_GENERATION_MODE_WARNED OFF)
+# In case ARCTICDB_PDB_GENERATION_MODE is TU and MSVC is used this will set a custom PDB path for each translation
+# unit. All translation units will produce unique PDB. All PDBs are automatically linked by the linker at the end (no
+# action required). The name of the PDB is the name of the cpp file_<MD5 hash of the path to the file>.pdb. If
+# ARCTICDB_PDB_GENERATION_MODE is TARGET (which is both the default for ArcticDB and MSVC) there will be one PDB per
+# target and in case of parallel builds each compiler invocation will try taking a lock and appending in the PDB file
+# this makes the output of .cl nondeterministic and compiler caches as sccache or ccache will not work.
+#
+# Wile in theory ARCTICDB_PDB_GENERATION_MODE=TU should make parallel builds faster it forces MSBuild to run
+# single-threaded builds even if \MP is set. This is because it batches building of TUs based on compiler parameters
+# as each TU now has unique PDB all TUs will have different flags resulting of no batches. Note: Ninja does not have
+# this problem.
+#
+# Pre-compiled headers cannot work with ARCTICDB_PDB_GENERATION_MODE=TU
+#
+# ARCTICDB_PDB_GENERATION_MODE=TU makes sense only when CMAKE_MSVC_DEBUG_INFORMATION_FORMAT=ProgramDatabase which is
+# the same as setting the /Zi (or /ZI) compiler flag.
+#
+# The function takes the path to each translation unit. The path can be relative or absolute. All paths must be unique.
+function(set_pdb_name_per_translation_unit)
+    if(MSVC AND "${ARCTICDB_PDB_GENERATION_MODE}" STREQUAL "TU")
+        if(NOT "${CMAKE_MSVC_DEBUG_INFORMATION_FORMAT}" STREQUAL "ProgramDatabase")
+            message(WARNING
+                "ARCTICDB_PDB_GENERATION_MODE set to ${ARCTICDB_PDB_GENERATION_MODE}, but \
+                CMAKE_MSVC_DEBUG_INFORMATION_FORMAT is ${CMAKE_MSVC_DEBUG_INFORMATION_FORMAT} (not ProgramDatabase)." \
+                "Generation of PDBs per TU makes sense only if debug format is ProgramDatabase (/Zi or /ZI are set)"
+            )
+            return()
+        endif()
+        message("ArcticDB MSVC build will generate one PDB per translation unit")
+
+        if(ARCTICDB_USE_PCH)
+            message(FATAL_ERROR "Cannot generate .pdb per translation unit and use precompiled headers")
+        endif()
+
+        if(CMAKE_GENERATOR MATCHES "Visual Studio" AND NOT _ARCTICDB_ARCTICDB_PDB_GENERATION_MODE_WARNED)
+            message(AUTHOR_WARNING
+                "Setting ARCTICDB_PDB_GENERATION_MODE to TU will result in single threaded builds with MSBuild even if \
+                /MP is set"
+            )
+            set(_ARCTICDB_ARCTICDB_PDB_GENERATION_MODE_WARNED ON PARENT_SCOPE)
+        endif()
+
+        if(CMAKE_CONFIGURATION_TYPES)
+            foreach(config IN LISTS CMAKE_CONFIGURATION_TYPES)
+                file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${config}")
+            endforeach()
+        else()
+            if(CMAKE_BUILD_TYPE)
+                file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_BUILD_TYPE}")
+            else()
+                message(FATAL_ERROR "CMAKE_BUILD_TYPE not set") 
+            endif()
+        endif()
+
+        foreach(src IN LISTS ARGN)
+            get_filename_component(filename ${src} NAME_WE)
+            string(MD5 src_hash ${src})
+            set(new_pdb_name "${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/${filename}_${src_hash}.pdb")
+            set_source_files_properties(${src} PROPERTIES COMPILE_OPTIONS "/Fd${new_pdb_name}")
+        endforeach()
+    endif()
+endfunction()
