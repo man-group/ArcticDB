4fbdec1e70eda2d12df08572fea02bc360760095
Vasil Pashov <vasil.pashov1@gmail.com>
2026-06-19 03:26:11 +0300

    Fix rapidcheck test for good

--- cpp/arcticdb/processing/test/rapidcheck_merge_update.cpp
+++ cpp/arcticdb/processing/test/rapidcheck_merge_update.cpp
@@ -28,8 +28,8 @@ RC_GTEST_PROP(StructureByTimeSlice, Rapidcheck, ()) {
     constexpr static timestamp ts_max = std::numeric_limits<timestamp>::max();
 
     // Number of column slices per row slice, between 1 and 5.
-    const size_t num_col_slices = *rc::gen::inRange<size_t>(1, 6);
-    const size_t num_row_slices = *rc::gen::inRange<size_t>(1, 100);
+    const int64_t num_col_slices = *rc::gen::inRange<int64_t>(1, 6);
+    const size_t num_row_slices = *rc::gen::inRange<size_t>(1, 1000);
 
     std::vector<TimestampRange> row_slice_time_ranges;
     row_slice_time_ranges.reserve(num_row_slices);
@@ -47,7 +47,7 @@ RC_GTEST_PROP(StructureByTimeSlice, Rapidcheck, ()) {
         const auto& [range_start, range_end] = row_slice_time_ranges[row_slice];
         const size_t range_size = range_end - range_start;
         const size_t num_rows = *rc::gen::inRange<size_t>(1, range_size + 1);
-        for (size_t col = 0; col < num_col_slices; ++col) {
+        for (size_t col = 0; col < static_cast<size_t>(num_col_slices); ++col) {
             ranges.emplace_back(
                     RowRange{row_count, row_count + num_rows},
                     ColRange{1 + col, 2 + col},
@@ -79,12 +79,43 @@ RC_GTEST_PROP(StructureByTimeSlice, Rapidcheck, ()) {
         for (auto range_index = current_group->begin(); range_index != current_group->end(); ++range_index) {
             const TimestampRange& current_time_range = ranges[*range_index].key_.time_range();
             auto intersects_with_current_range = [&](const size_t idx) {
-                return intersects(current_time_range, ranges[idx].key_.time_range());
+                const TimestampRange& range = ranges[idx].key_.time_range();
+                return current_time_range.first < range.second && range.first < current_time_range.second;
             };
-            RC_ASSERT(std::all_of(range_index + 1, current_group->end(), intersects_with_current_range));
-            RC_ASSERT(std::all_of(current_group + 1, groups.end(), [&](const auto& group) {
-                return std::ranges::none_of(group, intersects_with_current_range);
-            }));
+            RC_ASSERT(
+                    current_group->end() - range_index > 1 ||
+                    std::all_of(range_index + 1, current_group->end(), intersects_with_current_range)
+            );
+            if (groups.end() - current_group > 1) {
+                const auto next_group_it = current_group + 1;
+                if (intersects_with_current_range(next_group_it->front())) {
+                    const auto last_row_slice_of_current_group = std::span{*current_group}.last(num_col_slices);
+                    const auto first_row_slice_of_next_group = std::span{*next_group_it}.first(num_col_slices);
+                    RC_ASSERT(std::ranges::equal(last_row_slice_of_current_group, first_row_slice_of_next_group));
+                    if (std::distance(range_index, current_group->end()) > num_col_slices) {
+                        RC_ASSERT(std::ranges::all_of(first_row_slice_of_next_group, intersects_with_current_range));
+                        RC_ASSERT(std::ranges::none_of(
+                                next_group_it->begin() + num_col_slices,
+                                next_group_it->end(),
+                                intersects_with_current_range
+                        ));
+                    } else {
+                        RC_ASSERT(std::ranges::all_of(*next_group_it, intersects_with_current_range));
+                    }
+                } else {
+                    RC_ASSERT(std::ranges::none_of(*next_group_it, intersects_with_current_range));
+                }
+            }
+            RC_ASSERT(
+                    groups.end() - current_group > 2 ||
+                    std::all_of(
+                            current_group + 2,
+                            groups.end(),
+                            [&](const auto& group) {
+                                return std::ranges::none_of(group, intersects_with_current_range);
+                            }
+                    )
+            );
         }
     }
 }
