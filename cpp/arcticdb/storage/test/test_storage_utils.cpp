cd421813a763a03fda582ff4acf69f37068b454c
Ognyan Stoimenov <ostoimenov@icloud.com>
2026-06-25 15:08:31 +0300

    Fix copying index keys to preserve their start/end index (#3192)
    #### Reference Issues/PRs
    <!--Example: Fixes #1234. See also #3456.-->
    
    #### What does this implement or fix?
    Copying index keys via `copy_index_key_recursively` didn't preserve
    their start/end index keys correctly with timeseries-indexed data - the
    index type was lost and the copy was made as if the index is a always a
    row count.
    
    This PR changes the copying to preserve the type so that the resulting
    index key is correct
    
    This method is used in Enterprise's LibraryCopier, which is how the bug
    was caught.
    #### Any other comments?
    
    #### Checklist
    
    <details>
      <summary>
       Checklist for code changes...
      </summary>
     
    - [ ] Have you updated the relevant docstrings, documentation and
    copyright notice?
    - [ ] Is this contribution tested against [all ArcticDB's
    features](../docs/mkdocs/docs/technical/contributing.md)?
    - [ ] Do all exceptions introduced raise appropriate [error
    messages](https://docs.arcticdb.io/error_messages/)?
     - [ ] Are API changes highlighted in the PR description?
    - [ ] Is the PR labelled as enhancement or bug so it appears in
    autogenerated release notes?
    </details>
    
    <!--
    Thanks for contributing a Pull Request to ArcticDB! Please ensure you
    have taken a look at:
    - ArcticDB's Code of Conduct:
    https://github.com/man-group/ArcticDB/blob/master/CODE_OF_CONDUCT.md
    - ArcticDB's Contribution Licensing:
    https://github.com/man-group/ArcticDB/blob/master/docs/mkdocs/docs/technical/contributing.md#contribution-licensing
    -->
    
    ---------
    
    Co-authored-by: Ognyan Stoimenov <ognyan.stoimenov@man.com>

---
+++ cpp/arcticdb/storage/test/test_storage_utils.cpp
@@ -0,0 +1,61 @@
+/* Copyright 2026 Man Group Operations Limited
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License, use of this software
+ * will be governed by the Apache License, version 2.0.
+ */
+
+#include <gtest/gtest.h>
+
+#include <arcticdb/storage/storage_utils.hpp>
+#include <arcticdb/storage/test/in_memory_store.hpp>
+#include <arcticdb/stream/test/stream_test_common.hpp>
+#include <arcticdb/pipeline/frame_slice.hpp>
+#include <arcticdb/pipeline/index_writer.hpp>
+#include <arcticdb/stream/index.hpp>
+
+namespace arcticdb {
+using namespace arcticdb::pipelines;
+
+namespace {
+
+template<typename IndexType>
+AtomKey write_source_index(const std::shared_ptr<Store>& store, const StreamId& stream_id) {
+    const std::array fields{scalar_field(DataType::UINT8, "col")};
+    TimeseriesDescriptor tsd;
+    tsd.set_stream_descriptor(get_test_descriptor<IndexType>(stream_id, fields));
+
+    index::IndexWriter<IndexType> writer(store, IndexPartialKey{stream_id, 0}, std::move(tsd));
+    for (size_t i = 0; i < 3; ++i) {
+        const timestamp start = 100 + static_cast<timestamp>(i) * 10;
+        writer.add(
+                atom_key_builder().start_index(start).end_index(start + 10).build(stream_id, KeyType::TABLE_DATA),
+                FrameSlice{ColRange{1, 2}, RowRange{i, i + 1}}
+        );
+    }
+    return std::move(writer.commit()).get();
+}
+
+} // namespace
+
+template<typename IndexType>
+class CopyIndexKeyTest : public ::testing::Test {};
+
+using IndexTypes = ::testing::Types<stream::TimeseriesIndex, stream::RowCountIndex>;
+TYPED_TEST_SUITE(CopyIndexKeyTest, IndexTypes);
+
+TYPED_TEST(CopyIndexKeyTest, PreservesIndexRange) {
+    using IndexType = TypeParam;
+    const StreamId stream_id{"sym"};
+    auto source = std::make_shared<InMemoryStore>();
+    auto target = std::make_shared<InMemoryStore>();
+
+    const auto source_key = write_source_index<IndexType>(source, stream_id);
+    const auto target_key = copy_index_key_recursively(source, target, source_key, std::nullopt);
+
+    EXPECT_EQ(target_key.start_index(), source_key.start_index());
+    EXPECT_EQ(target_key.end_index(), source_key.end_index());
+}
+
+} // namespace arcticdb
