fcb92ac593815ff9e3827be31ed15b04001a7028
Alex Owens <73388657+alexowens90@users.noreply.github.com>
2026-06-25 10:26:15 +0100

    Make index key reading async in append (#3188)
    #### What does this implement or fix?
    Use `async_get_index_reader` in `async_append_impl` so that the index
    key reading, and therefore the full function, is actually asynchronous.

--- cpp/arcticdb/version/local_versioned_engine.cpp
+++ cpp/arcticdb/version/local_versioned_engine.cpp
@@ -1923,6 +1923,7 @@ std::vector<std::variant<VersionedItem, DataError>> LocalVersionedEngine::batch_
     for (const auto&& [idx, stream_update_info_fut] : folly::enumerate(stream_update_info_futures)) {
         append_versions_futs.push_back(
                 std::move(stream_update_info_fut)
+                        .via(&async::cpu_executor())
                         .thenValue(
                                 [this,
                                  frame = std::move(frames[idx]),
@@ -2006,6 +2007,7 @@ std::vector<std::variant<VersionedItem, DataError>> LocalVersionedEngine::batch_
     for (const auto&& [idx, stream_update_info_fut] : enumerate(stream_update_info_futures)) {
         update_versions_futs.push_back(
                 std::move(stream_update_info_fut)
+                        .via(&async::cpu_executor())
                         .thenValue(
                                 [this,
                                  frame = std::move(frames[idx]),
