f567eb3e02cc3af508c81554b04aa9bec0a9a94a
kigland <shuaizhicheng336@gmail.com>
2026-09-08 13:37:41 +0800

    restore shadowed test cases

--- python/tests/integration/arcticdb/version_store/test_update_with_date_range.py
+++ python/tests/integration/arcticdb/version_store/test_update_with_date_range.py
@@ -107,7 +107,7 @@ def basic_store_custom_norm(basic_store_factory):
 
 @pytest.mark.parametrize("with_timezone_attr,timezone_", [(True, None), (True, timezone.utc), (False, None)])
 @pytest.mark.storage
-def test_update_date_range_non_pandas_dataframe(basic_store_custom_norm, with_timezone_attr, timezone_):
+def test_update_date_range_non_pandas_dataframe_explicit_dates(basic_store_custom_norm, with_timezone_attr, timezone_):
     """Check that updates with a daterange work for a simple non-Pandas timeseries.
 
     This simulates a legacy DataFrame equivalent still used occasionally in Man.
@@ -141,7 +141,7 @@ def test_update_date_range_non_pandas_dataframe(basic_store_custom_norm, with_ti
 
 @pytest.mark.parametrize("with_timezone_attr,timezone_", [(True, None), (True, timezone.utc), (False, None)])
 @pytest.mark.storage
-def test_update_date_range_non_pandas_dataframe(basic_store_custom_norm, with_timezone_attr, timezone_):
+def test_update_date_range_non_pandas_dataframe_with_periods(basic_store_custom_norm, with_timezone_attr, timezone_):
     """Check that updates with a daterange work for a simple non-Pandas timeseries.
 
     This simulates a legacy DataFrame equivalent still used occasionally in Man.
